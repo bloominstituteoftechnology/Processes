d82900e05899882641fed099dad3583a03e75cbd
Sean Chen <skypemaster007@gmail.com>
2017-10-08 09:05:36 -0700

    Add needed header files to ex4.c

--- ex4/ex4.c
+++ ex4/ex4.c
@@ -3,6 +3,9 @@
 // `execl()`, `execle()`, `execv()`, and others. Why do you think there 
 // are so many variants of the same basic call?
 
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
 #include <sys/wait.h>
 
 int main(int argc, char* argv[])
@@ -11,7 +14,7 @@ int main(int argc, char* argv[])
     int rc = fork();
 
     if (rc < 0) {
-        fprintf("fork failed\n");
+        fprintf(stderr, "fork failed\n");
         exit(1);
     } else if (rc == 0) {
         printf("Child process here\n");
