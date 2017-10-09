47581fdd5270e2a628994bb3256ff2e3cf201e1d
Chooster <jcooleycoder@gmail.com>
2017-10-09 14:45:41 -0700

    Finished ex1

--- ex1/ex1.c
+++ ex1/ex1.c
@@ -9,6 +9,22 @@
 int main(int argc, char *argv[])
 {
     // Your code here
+    int test = 0;
+    test = 100;
+    printf("Test variable: %d (pid: %d)\n\n", test, (int) getpid());
+
+    int rc = fork();
+    if (rc == 0) {
+      printf("Child Test Variable: %d (pid: %d)\n", test, (int) getpid());
+      test = 111;
+      printf("Changed Child Test Variable: %d (pid: %d)\n\n", test, (int) getpid());
+    } else {
+      printf("Parent Test Variable: %d (pid: %d)\n", test, (int) getpid());
+      test = 222;
+      printf("Changed Parent Test Variable: %d (pid: %d)\n\n", test, (int) getpid());
+    }
+
+    printf("Re-test of Test Variable: %d (pid: %d)\n\n",test, (int) getpid());
 
     return 0;
 }
