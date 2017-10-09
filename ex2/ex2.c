9e556cb778fb597eea7aced15c9f3c122dc8320b
bdurb <brian@rpmgreenville.com>
2017-10-09 19:43:46 -0400

    cx3 working

--- ex2/ex2.c
+++ ex2/ex2.c
@@ -17,6 +17,6 @@ int main(int argc, char* argv[])
         ch = getc(fp);
     }
     fclose(fp);
-
+    
     return 0;
 }
--- ex3/ex3.c
+++ ex3/ex3.c
@@ -5,10 +5,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/wait.h>    
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
-    // Your code here
+    printf("hello world (pid: %d)\n", (int) getpid());
+    
+    int rc = fork();
+    if (rc < 0) {   
+        fprintf(stderr, "fork failed\n");
+        exit(1);
+    } else if (rc == 0) {    
+        printf("hello, child here (pid: %d) \n", (int) getpid());
+    } else {
+        wait(&rc);    
+        printf("hello, parent here (pid: %d) of child %d\n", (int) getpid(), rc);
+    }
 
     return 0;
 }
