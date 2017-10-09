90721a2547c74e4bf5f307222544a80dbfe73818
mixelpixel <mixelpix@mac.com>
2017-10-09 18:17:15 -0400

    ex3 √

--- ex3/ex3.c
+++ ex3/ex3.c
@@ -2,13 +2,27 @@
 // the parent process should print "goodbye". You should ensure that the child
 // process always prints first.
 
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/wait.h>    // `waitpid` needs to be included separately
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
-    // Your code here
+  printf("What to say to the world? (pid: %d)\n", (int) getpid());
+  // ------------------------------------------------ child process starts executing here
+  int rc = fork();
+  if (rc < 0) {    // fork failed; exit
+    fprintf(stderr, "fork failed\n");
+    exit(1);
+  } else if (rc == 0) {    // child process satisfies this branch
+    printf("HELLO, child here (pid: %d)\n", (int) getpid());
+  } else {
+    int wc = waitpid(rc, NULL, 0); // `waitpid` call added here
+    printf("GOODBYE, parent here (pid: %d) of child %d\n", (int) getpid(), rc);
+  }
+  puts("wOt?");
 
-    return 0;
+  return 0;
 }
