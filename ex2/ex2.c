b165239aef5862ba4af7b35b8bb02acca76a77a3
Ryan Sherrill <ryanbsherrill@gmail.com>
2017-10-09 19:18:01 -0400

    yes, it seems that both can access the file description!

--- ex2/ex2.c
+++ ex2/ex2.c
@@ -6,9 +6,41 @@
 #include <unistd.h>
 #include <stdlib.h>
 
+// function to read the file
+void readTheFile() {
+    int ch;
+    FILE *fp;
+    fp = fopen("/Users/ryansherrill/Desktop/Lambda/Section 2/Processes/ex2/text.txt", "r");
+    ch = getc(fp);
+    while (ch != EOF) {
+        putchar(ch);
+        ch = getc(fp);
+    }
+    fclose(fp);
+}
+
 int main(int argc, char* argv[])
 {
-    // Your code here 
+    int x = 100;
+    printf("Hello from Main (pid: %d)\n", (int) getpid());
+    printf("Main says: x = %d \n", x);
 
+    int rc = fork();
+    if (rc < 0) {
+        fprintf(stderr, "fork failed\n");
+        exit(1);
+    } else if (rc == 0) {
+        x -= 50;
+        printf("Hello from Child (pid: %d)\n", (int) getpid());
+        printf("Child says: x = %d \n", x);
+        readTheFile();
+        
+    } else {
+        x += 25;
+        printf("Hello from Parent (pid: %d)\n", (int) getpid());
+        printf("Parent Process says: x = %d \n", x);
+        readTheFile();
+    }
+    printf("Final check says: x = %d \n", x);
     return 0;
 }
