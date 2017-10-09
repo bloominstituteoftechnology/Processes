8b21eaac83eb435fcd5ca11eff8bbeca66ea84ba
mixelpixel <mixelpix@mac.com>
2017-10-09 19:10:08 -0400

    ex2 √

--- ex2/ex2.c
+++ ex2/ex2.c
@@ -1,4 +1,4 @@
-// Write a program that opens the text.txt  file (with the `fopen()` system call) located in this directory 
+// Write a program that opens the text.txt  file (with the `fopen()` system call) located in this directory
 // and then calls `fork()` to create a new process. Can both the child and parent access the file descriptor
 // returned by `fopen()`? What happens when they are written to the file concurrently?
 
@@ -6,23 +6,21 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-#include <fcntl.h> // <~~~ for open()
 
 int main(int argc, char* argv[])
 {
-  int xmas = open("test.txt", O_RDONLY);
-  printf ("%d\n", xmas); // <~~~ number used to refer to file?
-// ----------------------------------------------- KIDS SAY THE DARNDEST THINGS!
+  FILE *file = fopen("./text.txt", "w");
+  // ---------------------------------------------- KIDS SAY THE DARNDEST THINGS
   int rc = fork();
-  if (rc < 0) {    // fork failed; exit
+  if (rc < 0) { // fork failed; try spoon
     fprintf(stderr, "fork failed\n");
     exit(1);
-  } else if (rc == 0) {    // child process satisfies this branch
-    printf ("CHILD:  %d\n", xmas);
+  } else if (rc == 0) { // child process
+    char strChild[] = "Child Process\n";
+    fwrite(strChild, 1, sizeof(strChild), file);
   } else {
-    printf ("PARENT: %d\n", xmas);
+    char strParent[] = "Parent process\n";
+    fwrite(strParent, 1, sizeof(strParent), file);
   }
-  printf("UNCONDITIONAL: %d\n", xmas);
-
   return 0;
 }
