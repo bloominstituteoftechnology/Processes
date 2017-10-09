ffcba42350accc68401d5319bb35a325d552026f
manishavi <manishalal972@gmail.com>
2017-10-09 18:47:41 -0500

    used waitpid in ex3

--- ex3/ex3.c
+++ ex3/ex3.c
@@ -5,10 +5,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/wait.h>
 
 int main(int argc, char* argv[])
 {
     // Your code here
-
+    printf("hello world (pid: %d)\n", (int) getpid());
+    int rc = fork();
+    if (rc < 0) {
+        fprintf(stderr, "fork failed\n");
+        exit(1);
+    } else if (rc == 0) {
+        printf("hello, child here (pid: %d) \n", (int) getpid());
+    } else {
+        int wc = waitpid(rc, NULL, WUNTRACED | WCONTINUED);
+        // pid_t waitpid(pid_t rc, int *status_ptr, int options);
+        printf("hello, parent here (pid: %d) of child %d\n", (int) getpid(), rc);
+    }
     return 0;
 }
