1caea05fe131e5cfe8d9c3903dbe33e25c3bec44
mixelpixel <mixelpix@mac.com>
2017-10-09 18:18:22 -0400

    ex2... ummm?

--- ex2/ex2.c
+++ ex2/ex2.c
@@ -1,14 +1,38 @@
-// Write a program that opens the text.txt  file (with the `open()` system call) located in this directory 
-// and then calls `fork()` to create a new process. Can both the child and parent access the file descriptor 
+// Write a program that opens the text.txt  file (with the `open()` system call) located in this directory
+// and then calls `fork()` to create a new process. Can both the child and parent access the file descriptor
 // returned by `open()`? What happens when they are written to the file concurrently?
 
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
 
+#include <fcntl.h> // <~~~ for open()
+
 int main(int argc, char* argv[])
 {
-    // Your code here 
+  // Your code here
 
-    return 0;
+  int xmas = open("test.txt", O_RDONLY);
+  printf ("%d", xmas);
+
+
+  // int rc = fork();
+  // if (rc < 0) {    // fork failed; exit
+  //   fprintf(stderr, "fork failed\n");
+  //   exit(1);
+  // } else if (rc == 0) {    // child process satisfies this branch
+  //   printf("\nhello, child here (pid: %d) \n", (int) getpid());
+  //   printf("The value of x in the child process is: %d\n", x);
+  //   x = 12345;
+  //   printf("The value of x in the child process has been changed: %d\n", x);
+  // } else {
+  //   printf("\nhello, parent here (pid: %d) of child %d\n", (int) getpid(), rc);
+  //   printf("The value of x in the parent process is: %d\n", x);
+  //   x = 54321;
+  //   printf("The value of x in the parent process has been changed: %d\n", x);
+  // }
+  // printf("Now what is the value of x? %d\n", x);
+
+  close();
+  return 0;
 }
