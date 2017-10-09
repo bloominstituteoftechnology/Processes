185f24bd696ac7a1495f6a8d9a607e395de05bca
manishavi <manishalal972@gmail.com>
2017-10-09 18:18:51 -0500

    used fopen() in ex2

--- ex2/ex2.c
+++ ex2/ex2.c
@@ -9,6 +9,33 @@
 int main(int argc, char* argv[])
 {
     // Your code here 
-
+    FILE *fp;
+    int c;
+    int y = 100;
+    fp = fopen("text.txt","r");
+    // while(1) {
+    //     c = fgetc(fp);
+    //     if( feof(fp) ) { 
+    //         break ;
+    //     }
+    //     printf("%c", c);
+    // }
+    int rc = fork();
+    if (rc < 0) {
+        fprintf(stderr, "fork failed\n");
+        exit(1);
+    } else if (rc == 0) {
+        printf("hello, child here (pid: %d) \n", (int) getpid());
+    } else {
+        printf("hello, parent here (pid: %d) of child %d\n", (int) getpid(), rc);
+    } 
+    while(1) {
+        c = fgetc(fp);
+        if( feof(fp) ) { 
+            break ;
+        }
+        printf("%c", c);
+    }
+    fclose(fp);
     return 0;
 }
