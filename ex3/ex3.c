17c6403cc5149a090e0a6a6508d5b4c5b3080d91
Ely Alamillo <ely0013@gmail.com>
2017-10-09 20:58:29 -0500

    completed sprint

--- ex3/ex3.c
+++ ex3/ex3.c
@@ -8,7 +8,21 @@
 
 int main(int argc, char* argv[])
 {
-    // Your code here
+  // Your code here
+  int status;
+  int rc = fork();
 
-    return 0;
+  if (rc < 0) {
+      fprintf(stderr, "fork failed\n");
+      exit(1);
+  } else if (rc == 0) {
+      printf("Hello!\n");
+  } else {
+      while (waitpid(rc, &status, WNOHANG) == 0) {
+          sleep(1);
+      }
+      printf("Goodbye!\n");
+  }
+
+  return 0;
 }
