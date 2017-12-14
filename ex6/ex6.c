b01571f64c2daaabba504dc67b1212295650bf4c
Sean Chen <skypemaster007@gmail.com>
2017-12-14 14:57:37 -0800

    Update README

--- ex6/ex6.c
+++ ex6/ex6.c
@@ -1,4 +1,6 @@
 // Write a program that will find the average time it takes for your computer
 // to make a system call. Your program should use the `clock_gettime` procedure
 // to time how long a single system call takes. It'll do this for one million
-// iterations, and then find the average of all of those iteratinons, in nanoseconds.
+// iterations, and then find the average of all of those iterations in nanoseconds.
+// For some helpful documentation and examples of using time-related system calls,
+// visit this site: https://www.cs.rutgers.edu/~pxk/416/notes/c-tutorials/gettime.html
