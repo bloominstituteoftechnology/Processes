323a7a3e9937f5c8f50f2e8ca2d0ab80cbb14442
Sean Chen <skypemaster007@gmail.com>
2017-12-14 14:03:29 -0800

    Add prompts to ex5 and ex6

---
+++ ex5/ex5.c
@@ -0,0 +1,4 @@
+// Write a program that forks a child and creates a shared pipe
+// between the parent and child processes. Have the child write 
+// the three messages to the parent and have the parent print out 
+// the messages. 
---
+++ ex6/ex6.c
@@ -0,0 +1,4 @@
+// Write a program that will find the average time it takes for your computer
+// to make a system call. Your program should use the `clock_gettime` procedure
+// to time how long a single system call takes. It'll do this for one million
+// iterations, and then find the average of all of those iteratinons, in nanoseconds.
