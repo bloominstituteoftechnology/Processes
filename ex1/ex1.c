6318336c055eee6ec2c4bc8e6847400ce35abf9c
david <david@Dradus.PK5001Z>
2018-02-05 15:52:36 -0600

    ex1

--- ex1/ex1.c
+++ ex1/ex1.c
@@ -16,10 +16,14 @@ int main(int argc, char *argv[])
     if (rc == 0)
     {
         printf("child: (pid: %d) \n", (int)getpid());
+        x = 22;
+        printf("x should be 22 in child: %d\n", x);
     }
     else
     {
         printf("parent (pid: %d) of child %d \n", (int)getpid(), rc);
+        x = 33;
+        printf("x should be 33 in parent: %d\n", x);
     }
 
     return 0;
