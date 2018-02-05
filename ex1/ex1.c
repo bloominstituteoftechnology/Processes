d1edbd08b49aff4e05c613353cd87e1f790cc976
Thomas Dillard <thomas.dillard.studios@gmail.com>
2018-02-05 15:28:11 -0700

    ex 1 - 3 finished.

--- ex1/ex1.c
+++ ex1/ex1.c
@@ -5,10 +5,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <time.h>
 
 int main(int argc, char *argv[])
 {
-    // Your code here
+  // Your code here
+  int x;
 
-    return 0;
+  x = 100;
+
+  int rc = fork();
+  char *val = "Child";
+
+  if (rc < 0) { // fork failed
+    fprintf(stderr, "fork failed\n");
+    exit(1);      
+  } else if (rc > 0) {
+    int wc = waitpid(rc, NULL, 0);
+    x--;
+    val = "Parent";
+  }
+  x++;
+  printf("%s: %i\n", val, x);
+
+  return 0;
 }
