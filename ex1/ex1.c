fcc7c768a019197ff3803e66bef17f1f361b545c
CosmonautJones <travisj_jones@hotmail.com>
2018-02-05 17:05:52 -0600

    working on ex4

--- ex1/ex1.c
+++ ex1/ex1.c
@@ -1,5 +1,5 @@
 // Write a program that calls `fork()`. Before calling `fork()`, have the main process access a variable
-// (e.g., x) and set its value to something (e.g., 100). 
+// (e.g., x) and set its value to something (e.g., 100).
 
 // What value is the variable in the child process?
 // --- the value in the child process persists and is the same value as before the fork
@@ -18,20 +18,25 @@ int main(int argc, char *argv[])
     printf("value of x in main is: %d\n", x);
 
     int rc = fork();
-    if (rc < 0) {
+    if (rc < 0)
+    {
         fprintf(stderr, "fork failed\n");
         exit(1);
-    } else if (rc == 0) {    // child process satisfies this branch
-       x++;
-        printf("hello, child here (pid: %d), my value of x is: %d\n", (int) getpid(), x);
     }
-    else {
-        printf("hello, parent here (pid: %d) of child %d and my value of x is: %d\n", (int) getpid(), rc, x);
+    else if (rc == 0)
+    { // Child process
+        x++;
+        printf("hello, child here (pid: %d), my value of x is: %d\n", (int)getpid(), x);
+    }
+    else
+    { // Parent process
+        printf("hello, parent here (pid: %d) of child %d and my value of x is: %d\n", (int)getpid(), rc, x);
         x--;
         printf("hello, parent again (pid: %d) and my value of x is: %d\n", rc, x);
     }
 
     printf("Value of x after the fork: %d\n", x);
+    x = 99;
 
     return 0;
 }
