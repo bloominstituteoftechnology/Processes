30ae4a949f938af0dc72abae16e5eed118be44ee
CosmonautJones <travisj_jones@hotmail.com>
2018-02-06 12:28:37 -0600

    Finished ex6. Starting EX C

--- extraC/extra.c
+++ extraC/extra.c
@@ -1,7 +1,6 @@
-// Write a program that forks a child and creates a shared pipe
-// between the parent and child processes. Have the child write
-// the three messages to the parent and have the parent print out
-// the messages.
+// Write an additional program that creates two processes P1 and P2.
+// P1 should accept a string from standard input and passes it to P2. 
+// P2 should concatenate the received string with another string and sends it back to P1 for printing. 
 
 #include <stdio.h>
 #include <unistd.h>
