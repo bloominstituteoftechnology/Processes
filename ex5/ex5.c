9d524c3c239e8c34d0ea66e2015aa05c94722bed
alexfigliolia <alexfigliolia@gmail.com>
2018-02-06 17:59:01 -0500

    update

--- ex5/ex5.c
+++ ex5/ex5.c
@@ -15,7 +15,35 @@ char* msg3 = "hello world #3";
 
 int main()
 {
-    // Your code here
-    
-    return 0;
+  // Your code here
+  char inbuffer[MSGSIZE];
+  int p[2], pid, nbytes;
+
+  if(pipe(p) < 0) {
+  	fprintf(stderr, "pipe failed\n");
+  	exit(1);
+  }
+
+  if((pid == fork()) > 0) {
+  	printf("Child writing to pipe\n");
+  	write(p[1], msg1, MSGSIZE);
+  	write(p[1], msg2, MSGSIZE);
+  	write(p[1], msg3, MSGSIZE);
+
+  	close(p[1]);
+  	wait(NULL);
+  } else {
+  	close(p[1]);
+  	printf("Parent reading from pipe\n");
+  	while((nbytes = read(p[0], inbuffer, MSGSIZE)) > 0) {
+  		printf("%s\n", inbuffer);
+  	}
+  	if(nbytes != 0) {
+  		exit(1);
+  	}
+
+  	printf("Finished reading\n");
+  }
+
+  return 0;
 }
