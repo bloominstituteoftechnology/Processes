af405b0eb57417210ba834c0f611bc1feb7faf14
Lorin Fields <lorin.fields@gmail.com>
2018-02-07 08:03:04 -0800

    the rest working

--- ex4/ex4.c
+++ ex4/ex4.c
@@ -32,3 +32,28 @@ int main(int argc, char* argv[])
     }
     return 0;
 }
+
+// int main(int argc, char* argv[])
+// {
+//     // Your code here    
+//     printf("Here is the first line of text.\n");
+
+//     int rc = fork();
+
+//     if (rc < 0){
+//         fprintf(stderr, "fork failed\n");
+//         return 1;
+//     } else if (rc == 0 ) {
+//         printf("And here is the child call. With a PID of %d", ((int) getpid()));
+//         char *args[2];
+//         args[0] = strdup("/bin/ls");
+//         args[1] = NULL;
+//         execvp(args[0], args);
+//         printf("This should not print...");
+
+
+//     } else {
+//         printf("And here is the parent");
+//     }
+//     return 0;
+// }
