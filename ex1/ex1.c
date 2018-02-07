d1e3e145ea78416d60f3c70c34be1e497309d9e6
Teresa Strout <qapla47@gmail.com>
2018-02-07 14:39:11 -0500

    ex1 runs

--- ex1/ex1.c
+++ ex1/ex1.c
@@ -8,7 +8,32 @@
 
 int main(int argc, char *argv[])
 {
-    // Your code here
+    // initialize X to 100
+    int X = 100;
+
+    // set pointer to the memory location of X
+    int *x = &X;
+
+    // creates the fork and increments the PC, child will pickup after this line
+    int rc = fork();
+
+    // if rc is less than 0, no fork occurred
+    if (rc <0) {
+        fprintf(stderr, "fork failed\n");
+        exit(1);
+    }
+
+    // if the rc IS 0, you are in the child process (rc only increments for the parent to track the child)
+    else if (rc == 0) {
+        X +=5;
+        printf("hello, child here, my x is valued at: %d\n", X);
+    }
+
+    //
+    else {
+        X += 10;
+        printf("hello there, parent speaking, my x is valued at: %d\n", X);
+    }
 
     return 0;
 }
