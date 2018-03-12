f77da8625505ca1878ed3996be55e404d83e221b
Jesse Hood <jessenoob@gmail.com>
2018-03-12 15:55:22 -0400

    did ex1

--- ex1/ex1.c
+++ ex1/ex1.c
@@ -9,6 +9,22 @@
 int main(int argc, char *argv[])
 {
     // Your code here
-
+    int x;
+    int f = fork();
+    if (f < 0)
+    {
+        printf("something bad happened");
+    }
+    else if (f == 0)
+    {
+        x = 200;
+        printf("Child: value of x is %d\n", x); // 200
+    }
+    else
+    {
+        x = 100;
+        printf("Parent: value of x is %d\n", x); // 100
+    }
+    // Forks have separate scopes from the parent
     return 0;
 }
