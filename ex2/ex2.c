b196bafbd04e00c71d2f41691061ab5081f28153
Jason Metten <metten@gmail.com>
2018-03-12 14:32:59 -0500

    Through ex3

--- ex2/ex2.c
+++ ex2/ex2.c
@@ -5,10 +5,28 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char* argv[])
 {
     // Your code here 
-    
+    FILE *fp;
+    fp = fopen("text.txt", "w");
+
+    int rc = fork();
+    if(rc < 0) {
+        fprintf(stderr, "fork failed\n");
+        exit(1);
+    } else if(rc == 0){
+        printf("child process here\n");
+        char *child_str = "This is child string!\n";
+        fwrite(child_str, 1, strlen(child_str), fp);
+    } else {
+        printf("parent process here\n");
+        char *parent_str = "This is parent string!\n";
+        fwrite(parent_str, 1, strlen(parent_str), fp);
+    }
+
+    fclose(fp);
     return 0;
 }
