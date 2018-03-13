ae9c724b12be246b64c91a119209647619463ada
FJF616 <franzjfernando@hotmail.com>
2018-03-13 02:07:19 -0700

    ex1 and ex2 completed

--- ex1/ex1.c
+++ ex1/ex1.c
@@ -8,7 +8,21 @@
 
 int main(int argc, char *argv[])
 {
-    // Your code here
-
-    return 0;
+//set main process value to 100
+int x = 100;
+printf("This is a parent program (pid: %d)\n", (int) getpid());
+int rc = fork();
+if (rc < 0) {
+fprintf(stderr, "fork failed\n");
+exit(1);
+} else if (rc == 0) {
+printf("This is a child (pid: %d) and x is: %d\n", (int) getpid(), x);
+x++;
+printf("This is still a child and x is now: %d\n", x);
+} else {
+printf("The parent of  %d (pid: %d) where x is: %d\n", rc, (int) getpid(), x);
+x--;
+printf("This is still the parent, x is now: %d\n", x);
+}
+return 0;
 }
