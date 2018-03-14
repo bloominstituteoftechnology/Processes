855b4f3de2b6ffaca9e0fbd6e56dd90b60b0ae77
leelali <narci2008@gmail.com>
2018-03-14 11:05:30 -0700

    edit ex5 and ex6

--- ex5/ex5.c
+++ ex5/ex5.c
@@ -18,36 +18,55 @@ int main()
 {
     // Your code here
 
-    char inbuf[128];
-    int fds[2]; 
-    int rc = fork();
-    int nbytes;
+    int fds[2];
+    char buffer[128];
+
     pipe(fds);
- 
-    if (pipe(fds) < 0)
-    {
-        exit(1);
-    }
-    /* continued */
-    if (rc == 0) {
+
+    if (fork() == 0) {
+        read(fds[0], buffer, 128);
+        printf("%s\n", buffer);
+        read(fds[0], buffer, 128);
+        printf("%s\n", buffer);
+        read(fds[0], buffer, 128);
+        printf("%s\n", buffer);
+    } else {
         write(fds[1], msg1, MSGSIZE);
         write(fds[1], msg2, MSGSIZE);
         write(fds[1], msg3, MSGSIZE);
-    }
-    else 
-    {
         wait(NULL);
-        while ((nbytes = read(fds[0], inbuf, 128)) > 0)
-        {
-            printf("Read from parent: %s\n", inbuf);
-        }
-            
-        if (nbytes != 0)
-        {
-            exit(2);
-        }  
-        printf("Finished reading\n");
     }
+
+    // char inbuf[128];
+    // int fds[2]; 
+    // int rc = fork();
+    // int nbytes;
+    // pipe(fds);
+ 
+    // if (pipe(fds) < 0)
+    // {
+    //     exit(1);
+    // }
+    // /* continued */
+    // if (rc == 0) {
+    //     write(fds[1], msg1, MSGSIZE);
+    //     write(fds[1], msg2, MSGSIZE);
+    //     write(fds[1], msg3, MSGSIZE);
+    // }
+    // else 
+    // {
+    //     wait(NULL);
+    //     while ((nbytes = read(fds[0], inbuf, 128)) > 0)
+    //     {
+    //         printf("Read from parent: %s\n", inbuf);
+    //     }
+            
+    //     if (nbytes != 0)
+    //     {
+    //         exit(2);
+    //     }  
+    //     printf("Finished reading\n");
+    // }
     
     return 0;
 }
