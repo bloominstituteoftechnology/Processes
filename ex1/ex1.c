8ac7951bc862293bba4af9de093a05a87f3849b6
Jamie Domingo <jaybmw@aol.com>
2018-03-14 17:10:05 -0700

    updated with solutions

--- ex1/ex1.c
+++ ex1/ex1.c
@@ -9,6 +9,28 @@
 int main(int argc, char *argv[])
 {
     // Your code here
+    int x = 100;                                      // declare a variable
+    printf("hello world (pid: %d)\n", (int)getpid()); // get process id of parent
+
+    int rc = fork(); // call fork function
+
+    if (rc < 0) // 0 represents child function
+    {
+        fprintf(stderr, "fork failed\n");
+        exit(1); // 1 represents error value
+    }
+    else if (rc == 0) // 0 represents success
+    {
+        printf("hello, I am child (pid: %d) and x is: %d\n", (int)getpid(), x);
+        x++; // count
+        printf("child again, x is now: %d\n", x);
+    }
+    else // if not child
+    {
+        printf("hello, I am parent of %d (pid: %d) and x is: %d\n", rc, (int)getpid(), x);
+        x--;
+        printf("parent again, x is now: %d\n", x);
+    }
 
     return 0;
 }
--- ex2/ex2.c
+++ ex2/ex2.c
@@ -1,14 +1,29 @@
-// Write a program that opens the text.txt  file (with the `fopen()` system call) located in this directory 
-// and then calls `fork()` to create a new process. Can both the child and parent access the file descriptor 
+// Write a program that opens the text.txt  file (with the `fopen()` system call) located in this directory
+// and then calls `fork()` to create a new process. Can both the child and parent access the file descriptor
 // returned by `fopen()`? What happens when they are written to the file concurrently?
 
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
-    // Your code here 
-    
+    // Your code here
+    FILE *fp = fopen("text.txt", "w");
+
+    if (fp == NULL)
+    {
+        fprintf(stderr, "failed to open file");
+        exit(1);
+    }
+    if (fork() == 0)
+    {
+        fprintf(fp, "Child writing to file!\n");
+    }
+    else
+    {
+        fprintf(fp, "Parent writing to file!\n");
+    }
+
     return 0;
 }
