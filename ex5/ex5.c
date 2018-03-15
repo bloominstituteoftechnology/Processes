854a796890929ed37438c35b75b8ee3bb58073fa
JacquelynnRohrich <33401573+JacquelynnRohrich@users.noreply.github.com>
2018-03-15 09:43:13 -0500

    fixed ex5

--- ex5/ex5.c
+++ ex5/ex5.c
@@ -1,7 +1,7 @@
 // Write a program that forks a child and creates a shared pipe
-// between the parent and child processes. Have the child write
-// the three messages to the parent and have the parent print out
-// the messages.
+// between the parent and child processes. Have the child write 
+// the three messages to the parent and have the parent print out 
+// the messages. 
 
 #include <stdio.h>
 #include <unistd.h>
@@ -9,31 +9,29 @@
 
 #define MSGSIZE 15
 
-char *msg1 = "hello world #1";
-char *msg2 = "hello world #2";
-char *msg3 = "hello world #3";
+char* msg1 = "hello world #1";
+char* msg2 = "hello world #2";
+char* msg3 = "hello world #3";
 
 int main()
 {
     int fds[2];
-    char buffer[128];
+    char buffer[MSGSIZE];
+
     pipe(fds);
 
-    int rc = fork();
-
-    if (rc == 0)
-    {
-        //Child Here
-        printf("Writing from the Child SOMESTUFF 11 Bytes\n");
-        write(fds[1], "SOMESTUFF", 11);
-    }
-    else
-    {
+    if (fork() == 0) {
+        read(fds[0], buffer, MSGSIZE);
+        printf("%s\n", buffer);
+        read(fds[0], buffer, MSGSIZE);
+        printf("%s\n", buffer);
+        read(fds[0], buffer, MSGSIZE);
+        printf("%s\n", buffer);
+    } else {
+        write(fds[1], msg1, MSGSIZE);
+        write(fds[1], msg2, MSGSIZE);
+        write(fds[1], msg3, MSGSIZE);
         wait(NULL);
-        //Parent Here
-        printf("Reading from the Parent\n");
-        read(fds[0], buffer, 128);
-        printf("Read from the Parent: %s\n", buffer);
     }
 
     return 0;
