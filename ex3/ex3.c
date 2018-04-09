da4876a710632286cbb97473997c5fb1e3405ce3
Samuel Cha <sunchipssam@gmail.com>
2018-04-09 11:06:44 -0700

    Finished ex3

--- ex3/ex3.c
+++ ex3/ex3.c
@@ -6,9 +6,22 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
-    // Your code here
+    int rc = fork();
 
+    if (rc < 0)
+    {
+        fprintf(stderr, "fork() failed\n");
+    }
+    else if (rc == 0)
+    {
+        printf("hello\n");
+    }
+    else
+    {
+        int wc = waitpid(rc, NULL, 0);
+        printf("goodbye\n");
+    }
     return 0;
 }
