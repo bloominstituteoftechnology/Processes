1bf7dfa2cd0cf6af9d98172284eec30c019e02de
groov1234 <c.groover@outlook.com>
2018-04-09 13:58:32 -0400

    ex1 complete

--- ex1/ex1.c
+++ ex1/ex1.c
@@ -9,6 +9,21 @@
 int main(int argc, char *argv[])
 {
     // Your code here
-
+    int a = 100;
+    printf("I'm the parent (pid: %d)\n", (int) getpid());
+    int b = fork();
+    if (b < 0) {
+        fprintf(stderr, "What the fork!!\n");
+        exit(1);
+    } else if (b == 0) {
+        printf("I'm the child (pid: %d) and a is: %d\n", (int) getpid(), a);
+        a++;
+        printf("I'm a little child and a is now: %d\n", a);
+    } else {
+        printf("I'm the parent of %d (pid: %d) and a is: %d\n", b, (int) getpid(), a);
+        a--;
+        printf("I'm the parent!!!, a is: %d\n", a);
+    } 
+    
     return 0;
 }
