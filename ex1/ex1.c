051e957b8a4a88950253cd67955b04c3b869448e
brcampbell001 <brcampbell001@outlook.com>
2018-04-09 15:54:16 -0500

    Ex2 Completed

--- ex1/ex1.c
+++ ex1/ex1.c
@@ -7,9 +7,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-
-// added for wait
-#include <sys/types.h>
 #include <sys/wait.h>
 
 int main(int argc, char *argv[])
--- ex2/ex2.c
+++ ex2/ex2.c
@@ -1,14 +1,39 @@
-// Write a program that opens the text.txt  file (with the `fopen()` system call) located in this directory 
-// and then calls `fork()` to create a new process. Can both the child and parent access the file descriptor 
-// returned by `fopen()`? What happens when they are written to the file concurrently?
+// Write a program that opens the text.txt file (with the `fopen()` system call) located in this directory 
+// Then calls `fork()` to create a new process.
+// Can both the child and parent access the file descriptor returned by `fopen()`?
+// What happens when they are written to the file concurrently?
 
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <wait.h>
 
 int main(int argc, char* argv[])
 {
-    // Your code here 
+    int n;
+    FILE *fptr;
+    fptr = fopen("text.txt","a+");
     
+    if(fptr == NULL) {
+        printf("Error");
+        exit(1);
+    }
+
+    int forked = fork();
+
+    if(forked == 0) {
+        printf("Welcome to the Thunder Dome!\n");
+        printf("Enter number: \n");
+        scanf("%d", &n);
+        fprintf(fptr,"%d", n);
+        fclose(fptr);
+    } else {
+        waitpid(forked, NULL, 0);
+        printf("Enter number: \n");
+        scanf("%d", &n);
+        fprintf(fptr, "%d", n);
+        fclose(fptr);
+        printf("It's been fun :/\n");
+    }
     return 0;
 }
