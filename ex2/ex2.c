d2c75d9d7f19a6a56585ca35cdce2ba4663c35e7
groov1234 <c.groover@outlook.com>
2018-04-09 14:54:33 -0400

    ex3 complete - files missing from ex2 commit added

--- ex2/ex2.c
+++ ex2/ex2.c
@@ -8,7 +8,31 @@
 
 int main(int argc, char* argv[])
 {
-    // Your code here 
+    // Your code here
+    // FILE* fopen(text file here)
+    FILE* fp;
+    fp = fopen("text.txt", "a");
+    // int rc(run commands?) fork()
+    int rc = fork();
+    if (rc < 0) {
+    // file print = fprintf()
+    fprintf(stderr, "What the Fork!!!\n");
+    exit(1);
+    // else if child process print the process
+    // write   
+    } else if (rc == 0) {
+        printf("wee little one\n");
+        char child_str[] = "wee little string!!!\n";
+        fwrite(child_str, 1, sizeof(child_str), fp);
+    // else print parent
+    // write
+    } else {
+        printf("big parent\n");
+        char parent_str[] = "big giant string!!\n";
+        fwrite(parent_str, 1, sizeof(parent_str), fp);
+    }
+    fclose(fp);
+    
     
     return 0;
 }
