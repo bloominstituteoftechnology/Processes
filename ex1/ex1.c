087e4b064cbbb1855b6ce0f61fc4e51da255af54
Synsyxx <travis.simpson92@hotmail.com>
2018-04-09 14:27:36 -0500

    ex1 complete

--- ex1/ex1.c
+++ ex1/ex1.c
@@ -8,7 +8,27 @@
 
 int main(int argc, char *argv[])
 {
-    // Your code here
+
+    int x = 100;
+
+    int rc = fork();
+    if (rc < 0) {
+        fprintf(stderr, "Fork Failed\n"); 
+        exit(1);
+    } else if (rc == 0) {
+        printf("Child (pid: %d), x = %d \n", (int) getpid(), x);
+        x = 48;
+        printf("Child (pid: %d), x = %d \n", (int) getpid(), x);
+    } else {
+        printf("Parent(pid: %d) of child %d, x = %d\n", (int) getpid(), rc, x);
+        x = 37;
+        printf("Parent(pid: %d) of child %d, x = %d\n", (int) getpid(), rc, x);
+    }
 
     return 0;
 }
+
+// Parent(pid: 78) of child 79, x = 100
+// Child (pid: 79), x = 100
+// Parent(pid: 78) of child 79, x = 37
+// Child (pid: 79), x = 48
