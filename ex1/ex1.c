36c91c69ae1041c5305547c8a9353cf6d2e5407a
brcampbell001 <brcampbell001@outlook.com>
2018-04-09 15:29:16 -0500

    Ex1 Completed

--- ex1/ex1.c
+++ ex1/ex1.c
@@ -8,9 +8,25 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+// added for wait
+#include <sys/types.h>
+#include <sys/wait.h>
+
 int main(int argc, char *argv[])
 {
-    // Your code here
+    int x = 100;
 
+    int rc = fork();
+    if (rc < 0) {
+        fprintf(stderr, "fork failed\n");
+        exit(1);
+    } else if (rc == 0) {
+        x = 500;
+        printf("Child pid: %d, x: %d\n", (int) getpid(), x);
+    } else {
+        int wc = waitpid(rc, NULL, 0); // unclear which process runs first so wait is used to have child run first and child to terminate properly preventing zombie
+        x = 31;
+        printf("Parent pid: %d of child %d, x: %d\n", (int) getpid(), rc, x);
+    }
     return 0;
 }
