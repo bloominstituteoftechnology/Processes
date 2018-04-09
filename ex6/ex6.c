67d65a25f6130cf4be73ef411a60d3576f22e690
Samuel Cha <sunchipssam@gmail.com>
2018-04-09 14:41:08 -0700

    Finished ex6

--- ex6/ex6.c
+++ ex6/ex6.c
@@ -1,13 +1,13 @@
 // Write a program that will find the average time it takes for your computer
-// to make a system call an empty write to stdout. Your program should use the 
-// `clock_gettime` procedure to time how long a single system call takes. It'll 
-// do this for one million iterations, and then find the average of all of those 
+// to make a system call an empty write to stdout. Your program should use the
+// `clock_gettime` procedure to time how long a single system call takes. It'll
+// do this for one million iterations, and then find the average of all of those
 // iterations in nanoseconds.
 // For some helpful documentation and examples of using time-related system calls,
 // visit this site: https://www.cs.rutgers.edu/~pxk/416/notes/c-tutorials/gettime.html
-// While the linked site does say that `clock_gettime()` does not work on OSX, this 
-// turns out to only be the case for OSX versions < 10.12. Anything later than that 
-// and `clock_gettime()` should work just fine. 
+// While the linked site does say that `clock_gettime()` does not work on OSX, this
+// turns out to only be the case for OSX versions < 10.12. Anything later than that
+// and `clock_gettime()` should work just fine.
 
 #include <stdio.h>
 #include <unistd.h>
@@ -19,7 +19,19 @@
 
 int main()
 {
-    // Your code here
-    
+    struct timespec start, end;
+    long avg = 0.0;
+
+    for (int i = 0; i < number_iter; i++)
+    {
+        clock_gettime(CLOCK_MONOTONIC, &start);
+        write(STDOUT_FILENO, NULL, sizeof(NULL));
+        clock_gettime(CLOCK_MONOTONIC, &end);
+
+        avg += BILLION * (end.tv_sec - start.tv_sec) + end.tv_nsec - start.tv_nsec;
+    }
+
+    printf("Average time to make system call: %ld nanoseconds\n", avg / number_iter);
+
     return 0;
 }
