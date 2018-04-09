2fb4db292b344bf125b7e473e3490eb391e1d18f
Samuel Cha <sunchipssam@gmail.com>
2018-04-09 10:45:59 -0700

    Finished ex1

--- ex1/ex1.c
+++ ex1/ex1.c
@@ -8,7 +8,44 @@
 
 int main(int argc, char *argv[])
 {
-    // Your code here
+    int x = 100;
+
+    int rc = fork();
+
+    if (rc < 0)
+    {
+        fprintf(stderr, "fork() failed\n");
+    }
+    else if (rc == 0)
+    {
+        printf("Child int x before= %d\n", x);
+        x = 1000;
+        printf("Child int x after= %d\n", x);
+    }
+    else
+    {
+        printf("Parent int x before= %d\n", x);
+        x = 10;
+        printf("Parent int x after= %d\n", x);
+    }
+
+    /*
+        1) The value of var x is the same in both child and parent.
+
+           Parent int x = 100
+           Child int x = 100
+
+        2) Changing the value in either parent or child does not affect
+           other process' x value.
+
+           Parent int x before= 100
+           Parent int x after= 10
+           Child int x before= 100
+           Child int x after= 1000
+
+           This is due to the fact that calling fork() copies the original code
+           (as opposed to "refs" like in arrays and objects in JS).
+    */
 
     return 0;
 }
