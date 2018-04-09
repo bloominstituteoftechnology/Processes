851c8c19a40e1f7401c5be7e94a70aedcec889ae
perryahern <pahern@gmail.com>
2018-04-09 13:57:40 -0400

    ex1 completed

--- ex1/ex1.c
+++ ex1/ex1.c
@@ -2,13 +2,25 @@
 // (e.g., x) and set its value to something (e.g., 100). What value is the variable in the child process?
 // What happens to the variable when both the child and parent change the value of x?
 
+// Answer: It increments only its own copy and does not affect the other process' copy
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 
 int main(int argc, char *argv[])
 {
-    // Your code here
+    int x = 100;
+
+    int rc = fork();
+    if (rc < 0) {
+        fprintf(stderr, "Fork failed, exiting.\n");
+        exit(1);
+    } else if (rc == 0) {
+        printf("Fork succeeded; incremented child x value is %d\n", ++x);
+    } else {
+        printf("Fork succeeded; incremented parent x value is %d\n", ++x);
+    }
 
     return 0;
 }
