94941b1f3ab6425909ffc4f20744b03dd219c61f
groov1234 <c.groover@outlook.com>
2018-04-09 16:53:08 -0400

    ex5 complete

--- ex5/ex5.c
+++ ex5/ex5.c
@@ -6,6 +6,7 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <sys/wait.h>
 
 #define MSGSIZE 16
 
@@ -16,6 +17,40 @@ char* msg3 = "hello world #3";
 int main()
 {
     // Your code here
-    
+    char inbuf[MSGSIZE];
+    int p[2], pid, nbytes;
+ 
+    if (pipe(p) < 0) {
+        fprintf(stderr, "Pipe has burst");
+        exit(1);
+    }
+ 
+    if ((pid = fork()) > 0) {
+
+        printf("The child has written on a pipe\n");
+
+        write(p[1], msg1, MSGSIZE);
+        write(p[1], msg2, MSGSIZE);
+        write(p[1], msg3, MSGSIZE);
+ 
+        // Close the write end of the pipe
+        close(p[1]);
+        wait(NULL);
+    }
+ 
+    else {
+        // Close the write end of the pipe
+        close(p[1]);
+
+        printf("Parent reading pipe\n");
+
+        while ((nbytes = read(p[0], inbuf, MSGSIZE)) > 0) {
+            printf("%s\n", inbuf);
+        }
+        if (nbytes != 0) {
+            exit(2);
+        }
+        printf("Parent cleaned the pipe after reading\n");
+    }
     return 0;
 }
