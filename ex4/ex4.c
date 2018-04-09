1b51b2da7d8697fe036329ecf92fae726cec4a8b
groov1234 <c.groover@outlook.com>
2018-04-09 15:54:10 -0400

    ex4 complete

--- ex4/ex4.c
+++ ex4/ex4.c
@@ -6,10 +6,33 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <sys/wait.h>
 
 int main(int argc, char* argv[])
 {
     // Your code here    
+    printf("I'm a parent!!\n");
+    int rc = fork();
+
+    if (rc < 0) {
+        fprintf(stderr, "What the Fork!!\n");
+        exit(1);
+    } else if (rc == 0) {
+        printf("I'm a child!!!\n");
+
+        execl("/bin/ls", "ls", "-l", (char *) NULL);
+        
+        // char *args[] = {"ls", "-l", NULL};
+        // execv("/bin/ls", args);
+
+        // execlp("ls", "ls", "-l", (char *) NULL);
+
+        // char *args[] = {"ls", "-l", NULL};
+        // execvp("ls", args);
+
+    } else {
+        wait(NULL);
+    }
 
     return 0;
 }
