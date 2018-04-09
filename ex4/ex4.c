bbb2141e49510b1dc54bd379ee14983bbf7b4571
Clara Andrew <candrewwani@gmail.com>
2018-04-09 16:53:59 -0400

    ex 3 + 4 completed

--- ex4/ex4.c
+++ ex4/ex4.c
@@ -6,10 +6,25 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char* argv[])
 {
-    // Your code here    
+    // Your code here
+    int rc = fork();
+
+    if (rc < 0) {
+        fprintf(stderr, "fork failed\n");
+        exit(1);
+    } else if (rc == 0) {
+        char *myargs = {"/bin/ls"};
+        printf("Chid here. I am about to run my own program.\n");
+        execv("/bin/ls", &myargs);
+        // execl("/bin/ls", "bin", NULL);
+    } else {
+        int wc = waitpid(rc, NULL, 0);
+        printf("Parent here. I'm coming after child program.\n");
+    }
 
     return 0;
 }
