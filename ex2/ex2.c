93ad42182fccc4b4e6ed8e2156c63434c8db19c7
brcampbell001 <brcampbell001@outlook.com>
2018-04-09 16:01:53 -0500

    Ex3 Completed - Added Err Handling to Ex2

--- ex2/ex2.c
+++ ex2/ex2.c
@@ -19,16 +19,19 @@ int main(int argc, char* argv[])
         exit(1);
     }
 
-    int forked = fork();
+    int rc = fork();
 
-    if(forked == 0) {
+    if (rc < 0) {
+        fprintf(stderr, "fork failed\n");
+        exit(1);
+    } else if (rc == 0) {
         printf("Welcome to the Thunder Dome!\n");
         printf("Enter number: \n");
         scanf("%d", &n);
         fprintf(fptr,"%d", n);
         fclose(fptr);
     } else {
-        waitpid(forked, NULL, 0);
+        waitpid(rc, NULL, 0);
         printf("Enter number: \n");
         scanf("%d", &n);
         fprintf(fptr, "%d", n);
--- ex3/ex3.c
+++ ex3/ex3.c
@@ -1,14 +1,28 @@
-// Write another program using `fork()`. The child process should print "hello";
-// the parent process should print "goodbye". You should ensure that the child
-// process always prints first.
+// Write another program using `fork()`.
+
+// The child process should print "hello";
+// The parent process should print "goodbye".
+
+// You should ensure that the child process always prints first.
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <wait.h>
 
 int main(int argc, char* argv[])
 {
-    // Your code here
+    int rc = fork();
+
+    if (rc < 0) {
+        fprintf(stderr, "fork failed\n");
+        exit(1);
+    } else if (rc == 0) {
+        printf("hello\n");
+    } else {
+        waitpid(rc, NULL, 0);
+        printf("goodbye\n");
+    }
 
     return 0;
 }
