2dbef16d0bc53cd44a8950392c3d14c839cb1c9d
Johnathan Justinn <jjustinn85@gmail.com>
2018-04-10 16:37:56 -0400

    ex5 complete

--- ex5/ex5.c
+++ ex5/ex5.c
@@ -15,7 +15,29 @@ char* msg3 = "hello world #3";
 
 int main()
 {
-    // Your code here
+    int pipe1[2];
+    int pipe2[2];
+    char buffer[128];
+
+    pipe(pipe1);
+    pipe(pipe2);
+    int forked = fork();
+
+    if (forked < 0)
+    {
+        fprintf(stderr, "Fork has failed\n");
+        exit(1);
+    } else if (forked == 0)
+    {
+        write(pipe1[1], "Hello World!", 14);
+        read(pipe2[0], buffer, 128);
+        printf("Child from pipe2: %s\n", buffer);
+    } else
+    {
+        read(pipe1[0], buffer, 128);
+        write(pipe2[1], "Complete", 7);
+        printf("Pipe1: %s\n", buffer);
+    }
     
     return 0;
 }
