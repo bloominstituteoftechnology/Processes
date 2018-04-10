15d1a7b2974a135202a0c108a3f31c7f6760f49d
perryahern <pahern@gmail.com>
2018-04-10 14:05:02 -0400

    example 4 completed

--- ex4/ex4.c
+++ ex4/ex4.c
@@ -3,13 +3,32 @@
 // `execl()`, `execle()`, `execv()`, and others. Why do you think there 
 // are so many variants of the same basic call?
 
+// There are so many variants as each is a specialized tool to fit a specific need
+
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/wait.h>
 
 int main(int argc, char* argv[])
 {
-    // Your code here    
+    int rc = fork();
+
+    if (rc < 0) {
+        exit(1);
+    } else if (rc == 0) {
+        // execl("/bin/ls", "ls", NULL);
+
+        char *args[2];
+        args[0] = strdup("/bin/ls");
+        args[1] = NULL;
+        // execv(args[0], args);
+        execvp(args[0], args);
+    } else {
+        int wc = waitpid(rc, NULL, 0);
+    };
+
 
     return 0;
 }
