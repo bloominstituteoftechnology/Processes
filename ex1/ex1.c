f3c455da6b7877cb2afe02a3b9b26334b5fe4f15
Camila Daniels <camila.daniels926@gmail.com>
2018-04-09 21:49:53 -0500

    Exercises 1-4 complete.

--- ex1/ex1.c
+++ ex1/ex1.c
@@ -9,6 +9,18 @@
 int main(int argc, char *argv[])
 {
     // Your code here
+    int x = 100;
+    printf("here is the value of the variable I initialized: %d\n", x);
+    // child process starts executing here
+    int rc = fork();
+    if (rc < 0) {
+    	printf("fork failed\n");
+    	exit(1);
+    } else if (rc == 0) {
+    	printf("child with pid: %d\n", (int) getpid());
+    } else {
+    	printf("parent with pid: %d of child with pid: %d\n", (int) getpid(), rc);
+    }
 
     return 0;
 }
