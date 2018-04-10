75ce95f61fd4675b60bfb1720467b3e77883c2df
Johnathan Justinn <jjustinn85@gmail.com>
2018-04-10 15:50:53 -0400

    ex1 complete

--- ex1/ex1.c
+++ ex1/ex1.c
@@ -8,7 +8,20 @@
 
 int main(int argc, char *argv[])
 {
-    // Your code here
+    int x = 100;
+
+    int rc = fork();
+    if (rc < 0) {
+        fprintf(stderr, "failure to fork\n");
+        exit(1);
+    } else if (rc == 0) {
+        x = 110;
+        printf("(pid: %d), x is %d \n");
+    } else {
+        int wc = waitpid(rc, NULL, 0);
+        x = 25;
+        printf("(pid: %d) of child %d, x is %d\n", (int) getpid(), rc, x);
+    }
 
     return 0;
 }
