337219bbe2485c8d6142b46e52cd97313d33ff7f
Your Name <haywood.d.johnson@gmail.com>
2018-04-10 22:50:46 -0500

    ex3 completed

--- ex3/ex3.c
+++ ex3/ex3.c
@@ -8,7 +8,22 @@
 
 int main(int argc, char* argv[])
 {
-    // Your code here
+    int rc = fork();
+
+    if (rc < 0) 
+    {
+      fprintf(stderr, "Nobody here but us chickens...\n");
+      exit(1);
+    } 
+    else if (rc == 0) 
+    {
+      printf("Hola! ¿Qué ondas, guey?\n");
+    } 
+    else 
+    {
+      int wc = waitpid(rc, NULL, 0);
+      printf("Ya, nos vemos!\n");
+    }
 
     return 0;
 }
