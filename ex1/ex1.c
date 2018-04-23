518d3ae03abcbf7787f91a27792282a5b60462fc
TERR1E <terrieb.kim@gmail.com>
2018-04-23 11:45:59 -0700

    initial commit - gitignore edit - ex1 complete

--- ex1/ex1.c
+++ ex1/ex1.c
@@ -9,6 +9,25 @@
 int main(int argc, char *argv[])
 {
     // Your code here
+    int x = 100;
+    int rc = fork();
+
+    if (rc < 0) {
+        fprintf(stderr, "Child process failed.\n");
+        exit(1);
+    } else if (rc == 0) {
+        printf("x value before child process: %d\n", x); //value not changed, equal to parent
+
+        x = 50;
+
+        printf("x value after child process: %d\n", x); //value changed but does not affect parent's value
+    } else {
+        printf("x value before parent process: %d\n", x); //value not changed, equal to child
+
+        x = 75;
+
+        printf("x value after parent process: %d\n", x); //value changed but does not affect child's value
+    }
 
     return 0;
 }
