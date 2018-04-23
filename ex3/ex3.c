e3d2d348bc77364c9c4287b83708669031baf1e9
Russell Leon Bates IV <russell.l.bates@gmail.com>
2018-04-23 14:57:44 -0700

    Finished ex3, plus some extra fun tinkering with more than two threads

--- ex3/ex3.c
+++ ex3/ex3.c
@@ -8,7 +8,24 @@
 
 int main(int argc, char* argv[])
 {
-    // Your code here
-
+    int responseFromFork = fork();
+    if (responseFromFork < 0) {
+        printf("You forked up!\n");
+        exit(1);
+    } else if (responseFromFork == 0) {
+        printf("There is no pain: you are receding!\n");
+    } else {
+        sleep(2);
+        printf("A distant ship - smoke on the horizon!\n");
+        responseFromFork = fork();
+        if (responseFromFork == 0) {
+            sleep(2);
+            printf("You are only coming through in waves. . .\n");
+        } else {
+            sleep(4);
+            printf("Your lips move, but I can't hear what you're saying\n");
+        }
+    }
+    
     return 0;
 }
