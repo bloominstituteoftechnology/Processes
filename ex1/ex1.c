13595a0434e8c162682dd6a3ba0d367c26076512
Nikhil Kamineni <nikhil.kamineni@gmail.com>
2018-04-23 15:53:39 -0400

    Finished messing with ex1

--- ex1/ex1.c
+++ ex1/ex1.c
@@ -9,6 +9,22 @@
 int main(int argc, char *argv[])
 {
     // Your code here
+    int x = 100;
+    int new_fork = fork();
+
+    if (new_fork < 0 ) {
+        printf("Fork failed!\n");
+    } else if (new_fork == 0) {
+        printf("This is the child with PID is %d\n", getpid());
+        printf("Here is x in child: %d\n", x);
+        x = 95;
+        printf("Here is x in child after assigning to 95: %d\n", x);
+    } else {
+        printf("This is the parent with PID: %d\n", getpid());
+        printf("Here is x in the parent: %d\n", x);
+        x = 70;
+        printf("Here is x in the parent after assign to 70: %d\n", x);
+    }
 
     return 0;
 }
