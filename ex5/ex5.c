740775a98469856ab47e0a918ecc1d20e1ba19ac
John Spraul <kylewibblesmith@gmail.com>
2018-04-23 18:21:23 -0400

    completed exercise 5

--- ex5/ex5.c
+++ ex5/ex5.c
@@ -16,6 +16,40 @@ char* msg3 = "hello world #3";
 int main()
 {
     // Your code here
+    char inbuf[MSGSIZE];
+    int p[2];
+
+    if (pipe(p) < 0)
+    {
+        printf("Something failed somewhere... .. . .... . .\n");
+        exit(1);
+    }
+
+    int fark = fork();
+    
+
+    if (fark < 0)
+    {
+        printf("Something failed somewhere... .. . .... . .\n");
+        exit(1);
+    }
+    else if (fark == 0)
+    {
+        // printf("we have reached the child process\n");
+        write(p[1], msg1, MSGSIZE);
+        write(p[1], msg2, MSGSIZE);
+        write(p[1], msg3, MSGSIZE);
+        // printf("about to close the thing");
+        close(p[1]);    
+    }
+    else
+    {
+        // printf("we have reached the parent process\n");
+        for (int i = 0; i < 3; i++) {
+            read(p[0], inbuf, MSGSIZE);
+            printf("%s\n", inbuf);
+        }
+    }
     
     return 0;
 }
