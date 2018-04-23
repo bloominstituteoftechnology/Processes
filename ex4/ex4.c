e9ebb12b4162266fc23518b72ac9f9434908036e
John Spraul <kylewibblesmith@gmail.com>
2018-04-23 17:20:20 -0400

    completed exercise 4

--- ex4/ex4.c
+++ ex4/ex4.c
@@ -6,10 +6,39 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <sys/wait.h>
+#include <string.h> 
 
 int main(int argc, char* argv[])
 {
-    // Your code here    
+    // Your code here  
+
+    int super_fork = fork();
+
+    if (super_fork < 0)
+    {
+        printf("something bad happened, sire");
+        exit(1);
+    }  
+    else if (super_fork == 0)
+    {
+        printf("the child is about to do its thing\n");
+        char *args[3];
+
+        args[0] = strdup("/bin/ls");
+        args[1] = strdup(".");
+        args[2] = NULL;
+
+        execvp(args[0], args);
+        // execl(".", args[0], 0);
+        // execle(".", args[0], 0, 'a');
+        // execv(args, args[1]);
+    }
+    else
+    {
+        waitpid(super_fork, NULL, 0);
+        printf("and the parent arrived late..\n");
+    }
 
     return 0;
 }
