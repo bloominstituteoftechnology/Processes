50da3fd000ff2e37bc21f205a87de4cc7822ac38
Lo Saephan <teamsky916@gmail.com>
2018-04-23 16:13:54 -0700

    refactored ex5 - output only printing msg1 three times

--- ex5/ex5.c
+++ ex5/ex5.c
@@ -18,33 +18,31 @@ int main()
     // Your code here
     char inbuf[MSGSIZE];
     int p[2];
-    int rc;
-	
+   
 	if(pipe(p) < 0){
 		fprintf(stderr, "pipe failed\n");
         exit(1);
 	}
 
-	if (rc = fork() < 0) {    // fork failed; exit
+	int rc = fork();
+
+	if (rc < 0) {    // fork failed; exit
         fprintf(stderr, "fork failed\n");
         exit(1);
-    } else if (rc = fork() > 0) {    // child process satisfies this branch
+    } else if (rc == 0) {    // child process satisfies this branch
     	printf("from child, writing msg to pipe\n");
         write(p[1], msg1, MSGSIZE);
     	write(p[1], msg2, MSGSIZE);
     	write(p[1], msg3, MSGSIZE);
-    	printf("child wrote msg to pipe\n");
 
     } else {
-    	// int wc = waitpid(rc = fork(), NULL, 0);
+    	int wc = waitpid(rc, NULL, 0);
+
     	printf("from parent, reading msg from pipe\n");
-    	close(p[1]);
+    	read(p[0], inbuf, MSGSIZE);
     	for (int i = 0; i < 3; i++) {
-    		printf("begining of loop: %d\n", i);
-	        read(p[0], inbuf, MSGSIZE);
 	        printf("inbuf msg: %s\n", inbuf);
-	        printf("end of each loop: %d\n", i);
-	    }
+	    };
     }
     return 0;
 }
