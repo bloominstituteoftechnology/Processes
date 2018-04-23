240412bd39c29882bc94c3646439d6814c897c18
Amanda Phillips <amanda@phillipsdev.org>
2018-04-23 17:12:18 -0500

    ex3 done

--- ex3/ex3.c
+++ ex3/ex3.c
@@ -8,7 +8,16 @@
 
 int main(int argc, char* argv[])
 {
-    // Your code here
+    int rc = fork();
+    if (rc < 0) {
+        fprintf(stderr, "Fork failed.\n");
+        exit(1);
+    } else if (rc == 0) {
+        printf("Hello!\n");
+    } else {
+        int wait = waitpid(rc, NULL, 0);
+        printf("Goodbye!\n");
+    }
 
     return 0;
 }
