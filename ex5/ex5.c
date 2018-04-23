104eee06edd988dd4537e8bc27258f9d215a9628
TERR1E <terrieb.kim@gmail.com>
2018-04-23 13:18:58 -0700

    ex5 complete

--- ex5/ex5.c
+++ ex5/ex5.c
@@ -16,6 +16,36 @@ char* msg3 = "hello world #3";
 int main()
 {
     // Your code here
+    char inbuf[MSGSIZE];
+    int p[2]; //set pipe
+
+    if (pipe(p) < 0) {
+        fprintf(stderr, "Pipe failed.\n");
+
+        exit(1);
+    }
+
+    int rc = fork();
+
+    if (rc < 0) {
+        fprintf(stderr, "Child process failed.\n");
+
+        exit(1);
+    } else if (rc == 0) {
+        write(p[1], msg1, MSGSIZE);
+        write(p[1], msg2, MSGSIZE);
+        write(p[1], msg3, MSGSIZE);
+
+        close(p[1]); //close writing pipe
+
+    } else {
+        int wc = waitpid(rc, NULL, 0);
+
+        read(p[0], inbuf, MSGSIZE);
+        for (int i = 0; i < 3; i++) {
+            printf("%s\n", inbuf);
+        }
+    }
     
     return 0;
 }
