84b6c58d556f0518d7b402fb10e08f58bb3bbf57
John Spraul <kylewibblesmith@gmail.com>
2018-04-23 16:40:26 -0400

    completed exercise 2

--- ex2/ex2.c
+++ ex2/ex2.c
@@ -9,6 +9,30 @@
 int main(int argc, char* argv[])
 {
     // Your code here 
-    
+    FILE *file = fopen("text.txt", "w");
+
+    int forky = fork();
+
+    if (forky < 0)
+    {
+        printf("SOMETHING TerRIBLE HAS HAPPENed, or something");
+        exit(1);
+    }
+    else if (forky == 0)
+    {
+        char str[] = "beans and rice, please";
+        fwrite(str, 1, sizeof(str)-1, file);
+        fclose(file);
+    }
+    else
+    {
+        char str[] = "If my child asks for rice or beans, do not give those to them\n";
+        fwrite(str, 1, sizeof(str)-1, file);
+        // printf("this is str: %s\nand size of srt: %lu\n", str, sizeof(str));
+        fclose(file);
+    }
     return 0;
 }
+
+
+//looks like parent writes, then child writes
