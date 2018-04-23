99dd39a398aa4bc93244be7630c1ace8bde0d36f
Mephestys <mephestys@gmail.com>
2018-04-23 16:10:18 -0600

    ex3 - child prints "hello" before parent prints "goodbye"

--- ex3/ex3.c
+++ ex3/ex3.c
@@ -5,10 +5,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/wait.h>
 
-int main(int argc, char* argv[])
+//int main(int argc, char* argv[])
+int main()
 {
     // Your code here
+    int status;
+    int rc = fork();
 
+    if (rc < 0) {
+        fprintf(stderr, "Failed to fork, exiting\n");
+        exit(1);
+    } else if (rc == 0) {
+        printf("hello\n");
+    } else {
+        while (waitpid(rc, &status, WUNTRACED) == 0) {
+            sleep(1);
+        }
+        printf("goodbye\n");
+    }
     return 0;
 }
