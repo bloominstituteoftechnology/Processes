9ec3e68799862b4c8a411051572c97ea568bac7b
Eric <erichech@gmail.com>
2018-04-23 17:12:49 -0400

    finished one and two

--- ex1/ex1.c
+++ ex1/ex1.c
@@ -8,7 +8,20 @@
 
 int main(int argc, char *argv[])
 {
-    // Your code here
+    int x = 100;
+
+    int rc = fork();
+
+    if (rc < 0) {
+        printf("Fork failed\n");
+        exit(1);
+    } else if (rc == 0) {
+        x = 1;
+        printf("x is %d\n", x);
+    } else {
+        x = 10;
+        printf("x is %d\n", x);
+    }
 
     return 0;
 }
