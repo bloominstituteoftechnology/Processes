e5ac342ad8d6edf1e6f5c29e46e93512bf709aad
Dixie Korley <dixiekorley@gmail.com>
2018-04-23 17:54:58 -0400

    Fork call not giving errors, need to use variable

--- ex1/ex1.c
+++ ex1/ex1.c
@@ -13,6 +13,26 @@ int main(int argc, char *argv[])
     int x = 100;
     int process = fork();
     // Your code here
-
+    if (process < 0)
+    {
+        fprintf(stderr, "fork failed \n");
+        exit(1);
+    }
+    else if (process == 0)
+    {
+        int i = 0;
+        for (; i < 5; ++i)
+        {
+            printf("Child process does this to x: %d, (pid: %d) \n", x, (int)getpid());
+        }
+    }
+    else
+    {
+        int j = 0;
+        for (; j < 5; ++j) {
+            
+        }
+        printf("Parent process does this to x: %d, (pid: %d) \n", x, (int)getpid());
+    }
     return 0;
 }
