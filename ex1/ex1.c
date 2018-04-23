8269cae25cf8dc817b65d54f9c7b6de8ead7c3c4
Dixie Korley <dixiekorley@gmail.com>
2018-04-23 18:21:04 -0400

    Still no errors, but trying a different way to show child/parent process.

--- ex1/ex1.c
+++ ex1/ex1.c
@@ -10,29 +10,40 @@
 
 int main(int argc, char *argv[])
 {
-    int x = 100;
-    int process = fork();
     // Your code here
-    if (process < 0)
-    {
-        fprintf(stderr, "fork failed \n");
-        exit(1);
-    }
-    else if (process == 0)
+
+    printf("--beginning of program\n");
+
+    int x = 100;
+    pid_t process = fork();
+
+    if (process == 0)
     {
         int i = 0;
         for (; i < 5; ++i)
         {
-            printf("Child process does this to x: %d, (pid: %d) \n", x, (int)getpid());
+            printf("Child process does this to x: %d \n", ++x);
         }
+        printf("Child process PID - (pid: %d) \n", (int)getpid());
     }
-    else
+
+    else if (process > 0)
     {
         int j = 0;
-        for (; j < 5; ++j) {
-            
+        for (; j < 5; ++j)
+        {
+            printf("Parent process does this to x: %d \n", ++x);
         }
-        printf("Parent process does this to x: %d, (pid: %d) \n", x, (int)getpid());
+        printf("Parent process PID - (pid: %d) \n", (int)getpid());
     }
+
+    else
+    {
+        fprintf(stderr, "fork failed \n");
+        exit(1);
+    }
+
+    printf("--end of program--\n");
+
     return 0;
 }
