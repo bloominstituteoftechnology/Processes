d291370e200b534f608ecf672aa206dd5712d902
Amanda Phillips <amanda@phillipsdev.org>
2018-04-23 15:27:42 -0500

    ex1 done

--- ex1/ex1.c
+++ ex1/ex1.c
@@ -8,7 +8,22 @@
 
 int main(int argc, char *argv[])
 {
-    // Your code here
+    int x = 100;
+    printf("hello, current value is: %d\n", x);
+
+    int rc = fork();
+    if (rc < 0) {
+        fprintf(stderr, "Fork Failed\n"); 
+        exit(1);
+    } else if (rc == 0) {
+        printf("Child x = %d \n", x);
+        x = 77;
+        printf("Child x = %d \n", x);
+    } else {
+        printf("Parent of child, x = %d\n", x);
+        x = 88;
+        printf("Parent of child, x = %d\n", x);
+    }
 
     return 0;
 }
