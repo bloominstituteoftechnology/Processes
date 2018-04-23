63d87d8ed8529bd8cbb2f42ef525ad1afad8ceb2
Mephestys <mephestys@gmail.com>
2018-04-23 17:03:51 -0600

    ex4 - Tested all `exec` family variants I could find. - This includes `execvpe` which is a GNU extension.

--- ex4/ex4.c
+++ ex4/ex4.c
@@ -3,13 +3,43 @@
 // `execl()`, `execle()`, `execv()`, and others. Why do you think there 
 // are so many variants of the same basic call?
 
+#define _GNU_SOURCE
+
 #include <stdio.h>
-#include <unistd.h>
 #include <stdlib.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
-int main(int argc, char* argv[])
+//int main(int argc, char* argv[])
+int main()
 {
-    // Your code here    
+    // Your code here
+    printf("Parent started, I don't exec.\n");
+    int rc = fork();
 
+    if (rc < 0) {
+        fprintf(stderr, "Failed to fork, exiting\n");
+        exit(1);
+    } else if (rc == 0) {
+        extern char **environ;
+        char *args[] = {"ls", "-1", NULL};
+
+        printf("Child started, I'm going to exec `ls`\n");
+        // execl("/bin/ls", "ls", "-1", (char *)NULL);
+        // execle("/bin/ls", "ls", "-1", (char *)NULL, environ);
+        // execlp("ls", "ls", "-1", environ);
+        // execv("/bin/ls", args);
+        // execve("/bin/ls", args, environ);
+        // execvp("ls", args);
+        execvpe("ls", args, environ);
+    } else {
+        wait(NULL);
+    }
     return 0;
 }
+
+// https://en.wikipedia.org/wiki/Exec_(system_call)
+// e – An array of pointers to environment variables is explicitly passed to the new process image.
+// l – Command-line arguments are passed individually (a list) to the function.
+// p – Uses the PATH environment variable to find the file named in the file argument to be executed.
+// v – Command-line arguments are passed to the function as an array (vector) of pointers.
