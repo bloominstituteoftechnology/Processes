0c80509b994cda1f4f580f412b45d4d8b128df75
Christopher Beards <beardscm@gmail.com>
2018-04-23 16:29:15 -0400

    ex1; Complete, pending review

--- ex1/ex1.c
+++ ex1/ex1.c
@@ -8,7 +8,22 @@
 
 int main(int argc, char *argv[])
 {
-    // Your code here
+    int x = 100;                            // have the main function access a variable and set it to 100
+    int rc = fork();                       //child process begins
+
+    // Structure inspired by the readme
+
+    if (rc < 0) {                           //fork failed so exit
+      fprintf(stderr, 'failed fork\n');
+      exit(1);
+    } else if (rc == 0) {                  // child process satisfies this branch, so do the thing
+      x = 101;                               
+      printf("(pid: %d), x is %d \n");      // PID = process identifier
+    } else {
+      int wc = waitpid(rc, NULL, 0);        // waitpid added - ensures child runs before parent
+      x = 102;
+      printf("(pid: %d) of child %d, x is %d\n", (int) getpid(), rc, x);
+    }
 
     return 0;
 }
