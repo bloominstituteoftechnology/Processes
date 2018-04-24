27959016b78d0fad69d288cca430daebc9898563
Cliff Kang <inlovewithhim@gmail.com>
2018-04-24 13:08:39 -0700

    finished ex4

--- ex4/ex4.c
+++ ex4/ex4.c
@@ -6,10 +6,26 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <sys/wait.h>
+#include <string.h>
 
 int main(int argc, char* argv[])
 {
     // Your code here    
-
+    int rc = fork();
+    if (rc < 0) {
+        fprintf(stderr, "fork failed\n");
+        exit(1);
+    } else if (rc == 0) {
+        char *myargs[3];
+        myargs[0] = strdup("/bin/ls");
+        myargs[1] = strdup(".");
+        myargs[2] = NULL;
+        execv(myargs[0], myargs);
+        printf("oh no\n");
+    } else {
+        int wc = waitpid(rc, NULL, 0);
+        printf("thanks\n");
+    }
     return 0;
 }
