1d43d123aba5a2db7c7df36aef0133ab2d66fc5f
Tyson Williams <tyson.j.williams@gmail.com>
2018-04-24 14:04:10 -0600

    Write to file according to process
    If parent write to text.txt file with parent_str
    If child write to text.txt file with child_str
    Test print and write functions
    printf: successfully prints
    fwrite: fails writing to file

--- ex2/ex2.c
+++ ex2/ex2.c
@@ -14,9 +14,21 @@ int main(int argc, char* argv[])
     fp = fopen("./text.txt", "w+");
 
     int cp = fork();
+    // ----------------------------------------------- child process starts here
     
-    fprintf(fp, "Testing fprintf function...\n");
-    fputs("Testing fputs function...\n", fp);
+    if (cp < 0) { // fork failed, exit
+        fprintf(stderr, "fork faild\n");
+        exit(1);
+    } else if (cp == 0) { // child process successfully created
+        printf("hello, child process (pid: %d)\n", (int) getpid());
+        char child_str[] = "hello child string!\n";
+        fwrite(child_str, 1, sizeof(child_str), fp);
+    } else {
+        printf("hello, parent process (pid: %d)\n", (int) getpid());
+        char parent_str[] = "hello parent string!\n";
+        fwrite(parent_str, 1, sizeof(parent_str), fp);
+    }
+
     fclose(fp);
 
     return 0;
