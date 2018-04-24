6c4e4b8e12583abe95243604e1a1ea2b78168c08
Christopher Beards <beardscm@gmail.com>
2018-04-24 10:31:56 -0400

    ex5; Complete

--- ex5/ex5.c
+++ ex5/ex5.c
@@ -6,6 +6,7 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <sys/wait.h>
 
 #define MSGSIZE 16
 
@@ -15,7 +16,37 @@ char* msg3 = "hello world #3";
 
 int main()
 {
-    // Your code here
+    int p[2]; // a two-element array to hold the read and write file descriptors that are used
+    char inbuf[MSGSIZE]; // a buffer that will hold the incoming data that is being written
+
+    // establish our pipe, passing it the p array so that it gets populated by the read and write file descriptors
+    if (pipe(p) < 0) {
+        printf("Pipe failed\n");
+        exit(1);
+    } 
+
+    int rc = fork(); // Child runs
+
+    if (rc < 0) {
+      printf("Fork failed.\n");
+      exit(1);
+    } else if (rc == 0) {
+      printf("Child writes\n");
+
+      write(p[1], msg1, MSGSIZE);
+      write(p[1], msg2, MSGSIZE);
+      write(p[1], msg3, MSGSIZE);
+    } else {
+      int wc = waitpid(rc, NULL, 0);
+      printf("Parent reads\n");
+      
+      for (int i = 0; i < 3; i++) {
+      // read 16 bytes of data from the read file descriptor 
+      read(p[0], inbuf, MSGSIZE);
+      printf("% s\n", inbuf);
+    }
+  }
+    
     
     return 0;
 }
