60b8f2b96732364dd0c2ff6b26a68bb21bce3f22
Christopher Beards <beardscm@gmail.com>
2018-04-24 10:05:52 -0400

    ex4; Complete

--- ex4/ex4.c
+++ ex4/ex4.c
@@ -6,10 +6,26 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <sys/wait.h>
+
+// Reference Material
+// https://linux.die.net/man/3/execvp
+// https://stackoverflow.com/questions/27541910/how-to-use-execvp?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
 
 int main(int argc, char* argv[])
 {
-    // Your code here    
+    int rc = fork();
+
+    if (rc < 0) {
+      printf("Fork failed\n");
+      exit(1);
+    } else if (rc == 0) {
+      char *args[] = {"/bin/ls", "-1", NULL};
+      execvp(args[0], args);
+    } else {
+      int wc = waitpid(rc, NULL, 0);
+      printf("Parent\n");
+    }
 
     return 0;
 }
