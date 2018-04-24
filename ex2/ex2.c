2d07275a4891cdacd76a8ac1cdd723299a94f685
Mephestys <mephestys@gmail.com>
2018-04-24 15:29:12 -0600

    Added `flock()` to ex2 - Also added .vscode dir to gitignore

--- ex2/ex2.c
+++ ex2/ex2.c
@@ -5,6 +5,7 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <sys/file.h>
 
 //int main(int argc, char* argv[])
 int main()
@@ -17,13 +18,17 @@ int main()
         fprintf(stderr, "Fork failed, exiting.\n");
         exit(1);
     } else if (rc == 0) {
+        flock((long)fp, LOCK_EX);
         printf("Child process writing...\n");
         char *child_string = "Written by the child process.\n";
         fprintf(fp, "%s", child_string);
+        flock((long)fp, LOCK_UN);
     } else {
+        flock((long)fp, LOCK_EX);
         printf("Parent process writing...\n");
         char *parent_string = "Written by the parent process.\r";
         fprintf(fp, "%s", parent_string);
+        flock((long)fp, LOCK_UN);
     }
     fclose(fp);
     return 0;
