d08984036bee94100a900cf9f2f064cd5ddf0d1c
Punit Rawal <rawalhome@gmail.com>
2018-04-24 11:45:31 -0700

    Punit Rawal...WIP

--- ex4/ex4.c
+++ ex4/ex4.c
@@ -1,15 +1,42 @@
-// Write a program that calls `fork()` and then calls some form of `exec()`
-// to run the program `/bin/ls`. Try a few variants of `exec()`, such as 
-// `execl()`, `execle()`, `execv()`, and others. Why do you think there 
-// are so many variants of the same basic call?
-
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <sys.wait.h>
+#include <string.h>
 
 int main(int argc, char* argv[])
 {
-    // Your code here    
+    // Your code here  
+    int rc = fork();
+
+    if (rc < 0) {
+        fprintf(stderr, "Child process failed.\n");
+        exit(1);
+    } else if (rc == 0) {
+        char *myargs[3];
+
+        myargs[0] = strdup("/bin/ls");
+        myargs[1] = strdup("-1");
+        myargs[2] = NULL;
+
+        execv(myargs[0], myargs);
+        // execl("/bin/ls", "bin", myargs[1], myargs[2]);
+        printf("this should not be seen\n");
+
+        /*
+            output:
+            $ ./ex4
+            Makefile
+            ex4
+            ex4.c
+            I am the parent that comes after the child.
+        */
+
+    } else {
+        int wc = waitpid(rc, NULL, 0);
+
+        printf("I am the parent that comes after the child.\n");
+    }
 
     return 0;
 }
