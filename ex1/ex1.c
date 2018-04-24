94405942b35aff108e5e09263a28e0ba9f82032e
AnthonyCatalfo <anthonycatalfo@gmail.com>
2018-04-24 19:30:00 -0400

    First 5 problems

--- ex1/ex1.c
+++ ex1/ex1.c
@@ -9,6 +9,23 @@
 int main(int argc, char *argv[])
 {
     // Your code here
-
+    int x=100;
+printf("hello world (pid: %d)\n", (int) getpid());
+    int rc = fork();
+    // ------------------------------------------------ child process starts executing here
+    if (rc < 0) {    // fork failed; exit
+        fprintf(stderr, "fork failed\n");
+        exit(1);
+    } else if (rc == 0) {    // child process satisfies this branch
+        printf("hello, child here (pid: %d) \n", (int) getpid());
+        printf("The value of x in child is %d\n",x);
+        printf("Adding 11 to x in child  %d\n",x+11);
+    } else {
+        printf("hello, parent here (pid: %d) of child %d\n", (int) getpid(), rc);
+        printf("The value of x in parent is %d\n",x);
+        printf("Adding 11 to x in parent  %d\n",x-11);
+    }
+// shows copy by value
     return 0;
+
 }
--- ex2/ex2.c
+++ ex2/ex2.c
@@ -9,6 +9,25 @@
 int main(int argc, char* argv[])
 {
     // Your code here 
-    
+    //
+    FILE * fp;
+    fp = fopen ("text.txt", "w+");
+    printf("Exercise 2 (pid: %d)\n", (int) getpid());
+    int rc = fork();
+    // ------------------------------------------------ child process starts executing here
+    if (rc < 0) {    // fork failed; exit
+        fprintf(stderr, "fork failed\n");
+        exit(1);
+    } else if (rc == 0) {    // child process satisfies this branch
+        printf("hello, child here (pid: %d) \n", (int) getpid());
+        fprintf(fp, "%s %s %s", "We", "are", "in the child\n");
+        fclose(fp);
+    } else {
+        printf("hello, parent here (pid: %d) of child %d\n", (int) getpid(), rc);
+        fprintf(fp, "%s %s %s", "We", "are", "in the parent");
+        fclose(fp);
+    }
+
     return 0;
+
 }
