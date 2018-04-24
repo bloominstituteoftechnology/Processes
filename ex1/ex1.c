a5bf5af78117b0d43c48dbb933ba10665e68266f
Nathan Daniel <natdf@icloud.com>
2018-04-23 19:14:44 -0500

    ex 1 - 3 are passing

--- ex1/ex1.c
+++ ex1/ex1.c
@@ -8,22 +8,20 @@
 
 int main(int argc, char *argv[])
 {
-    int x = 100;                            // have the main function access a variable and set it to 100
-    int rc = fork();                       //child process begins
+  printf("hello world (pid: %d)\n", (int) getpid());
+  int x = 100;
+  int rc = fork();
 
-    // Structure inspired by the readme
+  if (rc < 0) {
+    fprintf(stderr, "failed fork\n");
+    exit(1);
+  } else if (rc == 0) {
+    x = 1;
+    printf("x in child %d\n", x);
+  } else {
+    x = 10;
+    printf("x in parent %d\n", x);
 
-    if (rc < 0) {                           //fork failed so exit
-      fprintf(stderr, 'failed fork\n');
-      exit(1);
-    } else if (rc == 0) {                  // child process satisfies this branch, so do the thing
-      x = 101;                               
-      printf("(pid: %d), x is %d \n");      // PID = process identifier
-    } else {
-      int wc = waitpid(rc, NULL, 0);        // waitpid added - ensures child runs before parent
-      x = 102;
-      printf("(pid: %d) of child %d, x is %d\n", (int) getpid(), rc, x);
-    }
-
-    return 0;
+  }
+  return 0;
 }
