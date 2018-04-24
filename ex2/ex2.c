36bea25b525dec4ec19572d1c6e3fbd5882896b8
Igor Yermak <igor.yermak@gmail.com>
2018-04-24 17:05:12 -0400

    ex2
    Printing "Error opening file".

--- ex2/ex2.c
+++ ex2/ex2.c
@@ -1,14 +1,38 @@
-// Write a program that opens the text.txt  file (with the `fopen()` system call) located in this directory 
-// and then calls `fork()` to create a new process. Can both the child and parent access the file descriptor 
+// Write a program that opens the text.txt  file (with the `fopen()` system call) located in this directory
+// and then calls `fork()` to create a new process. Can both the child and parent access the file descriptor
 // returned by `fopen()`? What happens when they are written to the file concurrently?
 
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
-    // Your code here 
-    
+    FILE *o;
+    o = fopen("text.txt", "f");
+    if (o == NULL);
+    {
+        fprintf(stderr, "Error Opening file\n");
+        exit(1);
+    }
+    int chfork = fork();
+    if (chfork < 0)
+    {
+        fprintf(stderr, "Fork Unsuccessful.\n");
+        exit(1);
+    }
+    else if (chfork == 0)
+    {
+        printf("Child process.\n");
+        char child[64] = "Child String.\n";
+        fwrite(child, 1, sizeof(child), o);
+    }
+    else
+    {
+        printf("Parent process.\n");
+        char parent[64] = "Parent String.\n";
+        fwrite(parent, 1, sizeof(parent), o);
+    }
+    fclose(o);
     return 0;
 }
