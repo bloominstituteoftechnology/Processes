7a935f51706916016b5f9ae70f5b868cd246f540
Walter Woodward <walter.sekope.woodward@gmail.com>
2018-05-23 19:47:40 -0400

    ex2 complete

--- ex2/ex2.c
+++ ex2/ex2.c
@@ -1,14 +1,39 @@
-// Write a program that opens the text.txt  file (with the `fopen()` system call) located in this directory 
-// and then calls `fork()` to create a new process. Can both the child and parent access the file descriptor 
+// Write a program that opens the text.txt  file (with the `fopen()` system call) located in this directory
+// and then calls `fork()` to create a new process. Can both the child and parent access the file descriptor
 // returned by `fopen()`? What happens when they are written to the file concurrently?
+
+// Answer: The child and parent can indeed print one after another.  If you try to run the entire program
+// again,
+
+// Compile in correct ex2 folder: `gcc -Wall -Wextra -o ex2 ex2.c`
+// Then run: `./ex2`
 
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
 
-int main(int argc, char* argv[])
+// int argc, char *argv[]
+int main()
 {
-    // Your code here 
-    
+    FILE *fp = fopen("text.txt", "w+"); // https://www.geeksforgeeks.org/data-type-file-c/
+    int rc = fork();
+    printf("ex2 executing...\n");
+
+    if (rc < 0)
+    {
+        fprintf(stderr, "fork failed\n");
+        exit(1);
+    }
+    else if (rc == 0)
+    {
+        printf("Child running...\n");
+        fprintf(fp, "Child printed this!\n");
+    }
+    else
+    {
+        printf("Parent running...\n");
+        fprintf(fp, "Parent printed this!\n");
+    }
+    fclose(fp);
     return 0;
 }
