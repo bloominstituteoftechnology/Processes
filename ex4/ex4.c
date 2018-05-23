a4dcd7288bbe46e40f4c7e9f9146d7106334fa97
habib1234731 <habibrehman1234731@gmail.com>
2018-05-23 17:26:36 -0400

    ex4.c completed

--- ex4/ex4.c
+++ ex4/ex4.c
@@ -6,10 +6,26 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <sys/wait.h>
+#include <string.h>
 
 int main(int argc, char* argv[])
 {
     // Your code here    
-
+	printf("hello world (pid: %d)\n", (int) getpid());
+    int rc = fork();
+    if (rc < 0) {    // fork failed; exit
+        fprintf(stderr, "fork failed\n");
+        exit(1);
+    } else if (rc == 0) {    // child process satisfies this branch
+        printf("hello, child here (pid: %d) \n", (int) getpid());
+        char *myargs[2];
+        myargs[0] = strdup("/bin/ls");
+        myargs[1] = NULL;
+        execvp(myargs[0], myargs);
+    } else {
+        int wc = waitpid(rc, NULL, 0);
+        printf("hello, parent here (pid: %d) of child %d\n", (int) getpid(), rc);
+    }
     return 0;
 }
