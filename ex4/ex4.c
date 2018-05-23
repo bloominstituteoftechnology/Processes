159386977946c011f128a8022c0737e9f4e0a891
AJ Genung <DragonRydrz@me.com>
2018-05-23 17:30:37 -0400

    started ex4.  readme prettier formatted

--- ex4/ex4.c
+++ ex4/ex4.c
@@ -1,15 +1,31 @@
 // Write a program that calls `fork()` and then calls some form of `exec()`
-// to run the program `/bin/ls`. Try a few variants of `exec()`, such as 
-// `execl()`, `execle()`, `execv()`, and others. Why do you think there 
+// to run the program `/bin/ls`. Try a few variants of `exec()`, such as
+// `execl()`, `execle()`, `execv()`, and others. Why do you think there
 // are so many variants of the same basic call?
 
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
-    // Your code here    
+    // Your code here
+
+    int rc = fork();
+
+    if (rc < 0)
+    {
+        printf("stop trying to use a spoon!");
+        exit(1);
+    }
+    else if (rc == 0)
+    {
+        execl("/bin/ls");
+    }
+    else if (rc > 0)
+    {
+        printf("Parent completed");
+    }
 
     return 0;
 }
