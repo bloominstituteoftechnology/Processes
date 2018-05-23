98e40889b24b068937de544bf53cf942526f14e9
Carlos Andrade <carlos.andrade86@outlook.com>
2018-05-23 18:44:18 -0400

    Completely completed work

--- ex1/ex1.c
+++ ex1/ex1.c
@@ -1,7 +1,7 @@
 // Write a program that calls `fork()`. Before calling `fork()`, have the main process access a variable
 // (e.g., x) and set its value to something (e.g., 100). What value is the variable in the child process?
 // What happens to the variable when both the child and parent change the value of x?
-
+// What the fork is wrong with you man?
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
