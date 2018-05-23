94ee49c9db576595671f80e14bbf38ca7739a240
gooseandmegander <meganawilliamson@gmail.com>
2018-05-23 13:28:53 -0700

    ex1 attempt complete

--- ex1/ex1.c
+++ ex1/ex1.c
@@ -9,6 +9,20 @@
 int main(int argc, char *argv[])
 {
     // Your code here
+    int x = 100;
+    printf("x: %d\n", x);
+    int rc = fork();
+    // ------------------------------------------------ child process starts executing here
+    if (rc < 0) {    // fork failed; exit
+        fprintf(stderr, "fork failed\n");
+        exit(1);
+    } else if (rc == 0) {    // child process satisfies this branch
+        x = 90;
+        printf("child printing x: %d\n", x);
+    } else {
+        x = 80;
+        printf("parent printing x: %d\n", x);
+    }
 
     return 0;
 }
