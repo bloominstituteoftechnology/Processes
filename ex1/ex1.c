3adef17b2c5dcc0fce9fde37a74903d25dd2e302
mjd02 <mostdarryl@gmail.com>
2018-05-23 18:20:58 -0400

    completed first exercise

--- ex1/ex1.c
+++ ex1/ex1.c
@@ -2,6 +2,8 @@
 // (e.g., x) and set its value to something (e.g., 100). What value is the variable in the child process?
 // What happens to the variable when both the child and parent change the value of x?
 
+// if the values are changed, then the change is only represented in the process that changed that value.
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -9,6 +11,18 @@
 int main(int argc, char *argv[])
 {
     // Your code here
-
+    int k = 100;
+    int rc = fork();
+    if(rc < 0){
+      fprintf(stderr, "fork failed\n");
+      exit(1);
+    }
+    else if(rc == 0){
+      k = 150;
+      printf("k value: %d  from child process %d\n", k, (int) getpid());
+    }
+    else{
+      printf("k value: %d  from parent process %d\n", k, (int) getpid());
+    }
     return 0;
 }
