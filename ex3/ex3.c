7f434b9f458ea6b1e12759bce5548479e041f5f7
Erik Alonso <mr.alonsoea@protonmail.ch>
2018-05-23 17:19:57 -0500

    half of project processes

--- ex3/ex3.c
+++ ex3/ex3.c
@@ -6,9 +6,28 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-int main(int argc, char* argv[])
+char ERROR_FORK[] = "System failed to execute fork()!";
+
+int main(int argc, char *argv[])
 {
     // Your code here
+    char h[] = "Hello!";
+    char g[] = "Goodbye..";
+    int f = fork();
+
+    if (f < 0)
+    {
+        printf("%s", ERROR_FORK);
+    }
+    else if (f == 0)
+    {
+        printf("%s", h);
+    }
+    else
+    {
+        int wc = waitpid(f, NULL, 0);
+        printf("%s", g);
+    }
 
     return 0;
 }
