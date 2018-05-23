3528fcb7440156279db972621a6b9356993b5491
habib1234731 <habibrehman1234731@gmail.com>
2018-05-23 16:39:35 -0400

    ex3 completed

--- ex3/ex3.c
+++ ex3/ex3.c
@@ -5,10 +5,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/wait.h>
 
 int main(int argc, char* argv[])
 {
     // Your code here
-
+	printf("hello world (pid: %d)\n", (int) getpid());
+    int rc = fork();
+    if (rc < 0) {    // fork failed; exit
+        fprintf(stderr, "fork failed\n");
+        exit(1);
+    } else if (rc == 0) {    // child process satisfies this branch
+		printf("Hello\n");
+    } else {
+        int wc = waitpid(rc, NULL, 0);
+        printf("Goodbye\n");
+    }
     return 0;
 }
