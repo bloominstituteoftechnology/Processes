794e552d50ec1070a4d088fccc162459d9c394c4
Juber Ahmed <a.juber@hotmail.com>
2018-05-23 19:03:25 -0400

    Ex4 completed

--- ex4/ex4.c
+++ ex4/ex4.c
@@ -6,10 +6,48 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
-
+#include <string.h>
 int main(int argc, char* argv[])
 {
     // Your code here    
 
+    int rc = fork();
+    
+    if(rc < 0) {
+        fprintf(stderr, "fork failed\n");
+        exit(1);
+    } else if (rc == 0){
+
+        printf("hello, child here (pid: %d) \n", (int) getpid());
+       char *myargs[2];
+       myargs[0] = strdup("/bin/ls");
+       myargs[1] = NULL;
+       execvp(myargs[0], myargs);
+        execl("/bin/ls", "ls");
+        //execle("/bin/ls", "ls");
+    } else {
+          int wc = waitpid(rc, NULL, 0);
+          printf("Finished\n");
+    }
+
+
+//      int rc = fork();
+//     if (rc < 0) {    // fork failed; exit
+//         fprintf(stderr, "fork failed\n");
+//         exit(1);
+//     } else if (rc == 0) {    // child process satisfies this branch
+//        printf("hello, child here (pid: %d) \n", (int) getpid());
+//        char *myargs[2];
+//        myargs[0] = strdup("/bin/ls");
+//        myargs[1] = NULL;
+//        execvp(myargs[0], myargs);
+//    // execle("/bin/ls","ls");
+//     } else {
+//         int wc = waitpid(rc, NULL, 0);
+//         printf("DOne\n");
+// //        printf("hello, parent here (pid: %d) of child %d\n", (int) getpid(), rc);
+//     }
+//     return 0;
+    
     return 0;
 }
