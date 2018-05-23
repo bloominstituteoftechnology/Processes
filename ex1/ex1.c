d7e8b7bb4bacc338af2a0276d289add65dbb7328
Shaun Kelly <Shaun@redskelly.local>
2018-05-23 19:15:03 -0400

    ex1 done

--- ex1/ex1.c
+++ ex1/ex1.c
@@ -2,6 +2,8 @@
 // (e.g., x) and set its value to something (e.g., 100). What value is the variable in the child process?
 // What happens to the variable when both the child and parent change the value of x?
 
+// Both Parent and Child values of x are printed, beginning with Parent.
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -9,6 +11,19 @@
 int main(int argc, char *argv[])
 {
     // Your code here
+    int x = 100;
+    int rc = fork();
+    // ---------------- child process executes
+    if (rc < 0) {   // fork failed; exit
+      fprintf(stderr, "fork failed\n");
+      exit(1);
+    } else if (rc == 0) {   // child process satisfies this branch
+      x = 7;
+      printf("Child x value: %d\n", x);
+    } else {
+      x = 3;
+      printf("Parent x value: %d\n", x);
+    }
 
     return 0;
 }
