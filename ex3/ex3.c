60f8370ceb7286eec0bc31d84592be3d01dfb856
unknown <luisd824@gmail.com>
2018-05-23 18:45:28 -0400

    ex1 and ex3 done. ex2 wip

--- ex3/ex3.c
+++ ex3/ex3.c
@@ -5,10 +5,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/wait.h>
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
     // Your code here
+    printf("Hello World (pid: %d)\n", (int)getpid());
 
+    int rc = fork();
+
+    if (rc < 0)
+    {
+        printf("Fork failed");
+        exit(1);
+    }
+    else if (rc == 0)
+    {
+        printf("Child is first (pid: %d)\n", (int)getpid());
+    }
+    else
+    {
+        int wc = waitpid(rc, NULL, 0);
+        printf("Parent is last (pid: %d)\n", (int)getpid());
+    }
     return 0;
 }
