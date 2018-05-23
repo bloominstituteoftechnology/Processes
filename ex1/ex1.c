8fd74daada654e6d74c1aacfcfa7eaf27f977342
gooseandmegander <meganawilliamson@gmail.com>
2018-05-23 15:46:23 -0700

    ex3 attempt by eod

--- ex1/ex1.c
+++ ex1/ex1.c
@@ -6,23 +6,49 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+// int main(int argc, char *argv[])
+// {
+//     // Your code here
+//     int x = 100;
+//     printf("x: %d\n", x);
+//     int rc = fork();
+//     // ------------------------------------------------ child process starts executing here
+//     if (rc < 0) {    // fork failed; exit
+//         fprintf(stderr, "fork failed\n");
+//         exit(1);
+//     } else if (rc == 0) {    // child process satisfies this branch
+//         x = 90;
+//         printf("child printing x: %d\n", x);
+//     } else {
+//         x = 80;
+//         printf("parent printing x: %d\n", x);
+//     }
+
+//     return 0;
+// }
+
 int main(int argc, char *argv[])
 {
     // Your code here
-    int x = 100;
-    printf("x: %d\n", x);
+    int test = 0;
+    test = 100;
+    printf("Test: %d (pid: %d)\n\n", test, (int)getpid());
+
     int rc = fork();
-    // ------------------------------------------------ child process starts executing here
-    if (rc < 0) {    // fork failed; exit
-        fprintf(stderr, "fork failed\n");
-        exit(1);
-    } else if (rc == 0) {    // child process satisfies this branch
-        x = 90;
-        printf("child printing x: %d\n", x);
-    } else {
-        x = 80;
-        printf("parent printing x: %d\n", x);
+    if (rc == 0)
+    {
+        printf("Child: %d (pid: %d)\n", test, (int)getpid());
+        test = 111;
+        printf("New Child: %d (pid: %d)\n\n", test, (int)getpid());
     }
+    else
+    {
+        printf("Parent: %d (pid: %d)\n", test, (int)getpid());
+        test = 222;
+        printf("New Parent: %d (pid: %d)\n\n", test, (int)getpid());
+    }
+
+    printf("New test: %d (pid: %d)\n\n", test, (int)getpid());
 
     return 0;
 }
--- ex3/ex3.c
+++ ex3/ex3.c
@@ -6,9 +6,32 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
     // Your code here
+    char *hello = "hello";
+    char *goodbye = "goodbye";
+
+    int pid = (int)getpid();
+    printf("pid BEFORE: (pid: %d)\n\n", pid);
+
+    int rc = fork();
+
+    if (rc < 0)
+    {
+        printf("Child forked unsuccessfully");
+    }
+    else if (rc == 0)
+    {
+        printf("pid in child: (pid: %d)\n", (int)getpid());
+        printf("Test: (pid: %d)\n", pid);
+        printf("Child forked, %s\n", hello);
+    }
+    else
+    {
+        printf("Test: (pid: %d)\n", pid);
+        printf("Parent forked, %s\n", goodbye);
+    }
 
     return 0;
 }
