dc8a9fc65885e09ec95a369ea6b0b8d9cfb2dc83
gooseandmegander <meganawilliamson@gmail.com>
2018-05-23 14:02:48 -0700

    ex2 attempt complete

--- ex2/ex2.c
+++ ex2/ex2.c
@@ -8,7 +8,23 @@
 
 int main(int argc, char* argv[])
 {
-    // Your code here 
-    
+    // Your code here
+    FILE *fp;
+    fp = fopen("./text.txt", "r");
+    if (fp != NULL) {
+        printf("FILE address: %p\n", fp);
+    }
+
+    int rc = fork();
+    // ------------------------------------------------ child process starts executing here
+    if (rc < 0) {    // fork failed; exit
+        fprintf(stderr, "fork failed\n");
+        exit(1);
+    } else if (rc == 0) {    // child process satisfies this branch
+        printf("child printing FILE address: %p\n", fp);
+    } else {
+        printf("parent printing FILE address: %p\n", fp);
+    }
+
     return 0;
 }
