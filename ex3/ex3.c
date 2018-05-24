9da30a30f7ea17f70ca7bbce4ea977c76a42baab
csstone12 <36163530+csstone12@users.noreply.github.com>
2018-05-24 16:24:51 -0400

    Stone Chris | Processes

--- ex3/ex3.c
+++ ex3/ex3.c
@@ -5,10 +5,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/wait.h>
 
 int main(int argc, char* argv[])
 {
     // Your code here
+    int rc = fork();
+    if (rc < 0)
+    { // fork failed; exit
+        fprintf(stderr, "fork failed\n");
+        exit(1);
+    }
+    else if (rc == 0)
+    { // child process satisfies this branch
+        printf("hello");
+    }
+    else
+    {
+        int wc = waitpid(rc, NULL, 0);
+        printf("goodbye");
+    }
 
     return 0;
 }
