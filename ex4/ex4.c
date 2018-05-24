c6c37e5c751ec284690b1091e83a41c9b8dec40b
Robert Rak <robertrak@hotmail.com>
2018-05-24 13:29:37 -0400

    finished ex4

--- ex4/ex4.c
+++ ex4/ex4.c
@@ -6,12 +6,35 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char* argv[])
 {
     // Your code here    
     int rc = fork();
-    execv("/bin/ls", 0);  
+
+    if (rc < 0) {
+      fprintf(stderr, "fork failed\n");
+      exit(1);
+    }
+
+    else if (rc == 0) {
+      printf("Child process");
+      
+      char *myargs[3];
+      
+      char* home = getenv("HOME");
+
+      myargs[0] = strdup("/bin/ls");
+      myargs[1] = strdup(home);
+      myargs[2] = NULL;
+
+      execvp(myargs[0], myargs);  
+    }
+
+    else {
+      printf("Parent process");
+    }
 
     return 0;
 }
