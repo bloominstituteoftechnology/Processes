5a310fac44534b3155b5e31849c540f58be47b94
charletta <charlettabullard12@outlook.com>
2018-05-24 14:51:48 -0400

    Ex3 complete

--- ex3/ex3.c
+++ ex3/ex3.c
@@ -5,10 +5,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/wait.h>
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
-    // Your code here
+    int rc = fork();
+    if (rc < 0)
+    {
+        fprintf(stderr, "fork failed\n");
+        exit(1);
+    }
+    else if (rc == 0)
+    {
+        printf("Hello (pid: %d) %d\n", (int)getpid(), rc);
+    }
+    else
+    { //wc used to have the child say hello first if not the parent would say goodbye first (new child)
+        int wc = waitpid(rc, NULL, 0);
+        printf("Goodbye (pid: %d) %d\n", (int)getpid(), rc);
+    }
 
     return 0;
 }
