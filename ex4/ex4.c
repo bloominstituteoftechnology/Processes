9b7e0140129f2f778e89a9a745c2cb1a579bfb08
mjd02 <mostdarryl@gmail.com>
2018-05-24 17:10:45 -0400

    completed fourth and fifth

--- ex4/ex4.c
+++ ex4/ex4.c
@@ -6,10 +6,23 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char* argv[])
 {
     // Your code here    
-
+    int rc = fork();
+    if(rc < 0){
+      fprintf(stderr, "fork failed\n");
+      exit(1);
+    }
+    else if(rc == 0){
+      char *args[3];
+      args[0] = strdup("/bin/ls");
+      args[1] = strdup(".");
+      args[2] = NULL;
+      execvp(args[0],args);
+      printf("error exec call did not work\n");
+    }
     return 0;
 }
