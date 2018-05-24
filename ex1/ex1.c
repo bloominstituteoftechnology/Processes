05d23d1405e88a6f2a89723885b6556384eaeb74
gooseandmegander <meganawilliamson@gmail.com>
2018-05-24 14:02:03 -0700

    revisited ex1

--- ex1/ex1.c
+++ ex1/ex1.c
@@ -2,53 +2,74 @@
 // (e.g., x) and set its value to something (e.g., 100). What value is the variable in the child process?
 // What happens to the variable when both the child and parent change the value of x?
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-
-// int main(int argc, char *argv[])
-// {
-//     // Your code here
-//     int x = 100;
-//     printf("x: %d\n", x);
-//     int rc = fork();
-//     // ------------------------------------------------ child process starts executing here
-//     if (rc < 0) {    // fork failed; exit
-//         fprintf(stderr, "fork failed\n");
-//         exit(1);
-//     } else if (rc == 0) {    // child process satisfies this branch
-//         x = 90;
-//         printf("child printing x: %d\n", x);
-//     } else {
-//         x = 80;
-//         printf("parent printing x: %d\n", x);
-//     }
-
-//     return 0;
-// }
+// To create a new process, duplicate a process currently running using fork()
+#include <stdio.h>  // to use printf
+#include <stdlib.h> // to use getpid?
+#include <unistd.h> // to use fork
 
 int main(int argc, char *argv[])
 {
-    // Your code here
-    int test = 0;
-    test = 100;
-    printf("Test: %d (pid: %d)\n\n", test, (int)getpid());
+    // initial process
+    printf("I am (initial): %d\n", (int)getpid()); // the (int) is type casting the getpid return value, which is pid_t
 
-    int rc = fork();
-    if (rc == 0)
+    int x = 100;
+    printf("The value of 'x' is %d\n", x);
+
+    // duplicating process
+    // fork returns type pid_t
+    // returns once for each process
+    // returns child process (duplicated process)
+    // returns duplicate child pid to the parent
+    // returns 0 if child process is successful
+    // return -1 if not successful, errno -g variable is set
+    pid_t pid = fork();
+    printf("fork returned: %d\n", (int)pid);
+
+    // ===== child process executes from here =====
+    if (pid < 0)
     {
-        printf("Child: %d (pid: %d)\n", test, (int)getpid());
-        test = 111;
-        printf("New Child: %d (pid: %d)\n\n", test, (int)getpid());
+        perror("Fork failed");
     }
-    else
+    if (pid == 0)
     {
-        printf("Parent: %d (pid: %d)\n", test, (int)getpid());
-        test = 222;
-        printf("New Parent: %d (pid: %d)\n\n", test, (int)getpid());
+        // !! notice printing getpid() value instead of pid variable value
+        printf("I am the child with pid %d\n", (int)getpid());
+
+        // print value of x before child changes the value
+        printf("The value of 'x' before is %d\n", x);
+        x = 300;
+        // print value of x after child changes the value
+        printf("The value of 'x' after is %d\n", x);
+
+        exit(0);
     }
 
-    printf("New test: %d (pid: %d)\n\n", test, (int)getpid());
+    // with exit(0) in child conditional statement
+    // when execution gets here, it must be the parent
+    printf("I am the parent\n");
+
+    // print value of x before parent changes the value
+    printf("The value of 'x' before is %d\n", x);
+    x = 200;
+    // print value of x after parent changes the value
+    printf("The value of 'x' after is %d\n", x);
 
     return 0;
 }
+
+// output
+
+// I am (initial): 1637
+// The value of 'x' is 100
+// fork returned: 1638
+// I am the parent
+// The value of 'x' before is 100
+// The value of 'x' after is 200
+// fork returned: 0
+// I am the child with pid 1638
+// The value of 'x' before is 100
+// The value of 'x' after is 300
+
+// !! The value of 'x' before BOTH the parent and child change it is 100.
+// !! Observing 'x' before the child changes it is NOT 200, which the parent changes 'x' to before the child process executes the same command
+// !! Meaning the child receives the parent's lines of execution only before the fork
