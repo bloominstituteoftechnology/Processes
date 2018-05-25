b45c096ba195221c3ecf90e0bfa6098bc2f68d28
Walter Woodward <walter.sekope.woodward@gmail.com>
2018-05-25 10:15:11 -0400

    ex4 complete
    works but still unclear on what 'environ' is

--- ex4/ex4.c
+++ ex4/ex4.c
@@ -11,29 +11,38 @@
 int main()
 {
     // Your code here
-    printf("hello world (pid: %d)\n", (int)getpid());
+    printf("Parent started, I don't exec.\n");
     int rc = fork();
-    // ------------------------------------------------ child process starts executing here
+
     if (rc < 0)
-    { // fork failed; exit
-        fprintf(stderr, "fork failed\n");
+    {
+        fprintf(stderr, "Failed to fork, exiting\n");
         exit(1);
     }
     else if (rc == 0)
-    { // child process satisfies this branch
-        printf("hello, child here (pid: %d) \n", (int)getpid());
-        char *myargs[3]; // allocate an array of chars to hold 3 bytes
-        // `strdup` duplicates the given input string
-        myargs[0] = strdup("wc");      // pass the name of the program we want to run as the first array element
-        myargs[1] = strdup("/bin/ls"); // argument: the file to count
-        myargs[2] = NULL;              // marks the end of the array
-        execvp(myargs[0], myargs);     // runs the word count program, passing in the `myargs` array to the word count program as arguments
-        printf("this should not be seen");
+    {
+        printf("Child started, I'm going to exec `ls`\n");
+
+        // ===== exec() variant parameters (uncomment if needed) ===== //
+
+        extern char *environ;
+        char *args[] = {"ls", "-1", NULL};
+
+        // ====== exec() variants ======= // https://en.wikipedia.org/wiki/Exec_(system_call)
+
+        // execl("/bin/ls", "ls", "-1", (char *)NULL);
+        // execlp("ls", "-1", NULL);
+
+        // execvp("ls", args);
+        // execv("/bin/ls", args);
+
+        // execle("/bin/ls", "ls", "-1", (char *)NULL, environ);
+
+        execve("/bin/ls", args, environ); // runs, but with warnings
     }
     else
     {
-        waitpid(rc, NULL, 0); // `waitpid` call added here
-        printf("hello, parent here (pid: %d) of child %d\n", (int)getpid(), rc);
+        wait(NULL);
     }
     return 0;
 }
