0812949db19bfbc044564d58a89df9c17e4db889
nourmhassan <nour-pop1@hotmail.com>
2018-06-01 02:51:48 -0700

    ex5 trying on ex6 !

--- ex5/ex5.c
+++ ex5/ex5.c
@@ -17,5 +17,32 @@ int main()
 {
     // Your code here
     
+    char inbuf[MSGSIZE];    
+    int p[2];               
+    if (pipe(p) < 0) {
+        fprintf(stderr, "pipe failed\n");
+        exit(1);
+    }
+
+    int rc = fork();
+    if (rc < 0) {    // fork failed; exit
+        fprintf(stderr, "fork failed\n");
+        exit(1);
+    }
+    else if (rc == 0) {
+        printf("hello, child  (pid: %d) \n", (int) getpid());
+        write(p[1], msg1, MSGSIZE);
+        write(p[1], msg2, MSGSIZE);
+        write(p[1], msg3, MSGSIZE);
+   }
+    else {
+        printf("hello, parent  (pid: %d) \n", (int) getpid());
+        int wc = waitpid(rc, NULL, 0);
+       for (int i = 0; i < 3; i++) {
+        read(p[0], inbuf, MSGSIZE);
+        printf("%s\n", inbuf);
+    }
+}
+
     return 0;
 }
