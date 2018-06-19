67ed244064ed9cf41cca6b0d178d07b1a3a6ec37
harrisonbrock <harrison@harrisonbrock.com>
2018-06-19 16:18:43 -0400

    Add ex1 code

--- ex1/ex1.c
+++ ex1/ex1.c
@@ -9,6 +9,25 @@
 int main(int argc, char *argv[])
 {
     // Your code here
+    int x = 100;
+    printf(" The PID is: %d\n", (int)getpid());
+
+    int rc = fork();
+
+    if (rc < 0) {
+        fprintf(stderr, "fork failed\n");
+        exit(1);
+    }
+    else if (rc == 0) {
+        printf("hello, child here (pid: %d) and x is: %d\n", (int) getpid(), x);
+        x++; // add one to x
+        printf("child processes, x is: %d\n", x);
+    }
+    else {
+        printf("hello, parent here (pid: %d) and x is: %d\n", (int) getpid(), x);
+        x--;
+        printf("parent processes, x is: %d\n", x);
+    }
 
     return 0;
 }
