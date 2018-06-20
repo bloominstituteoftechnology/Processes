701cbda9b24c67cc74d78c8eb96907017f25b898
Justin Rigler <justinmrigler@gmail.com>
2018-06-20 19:31:18 -0400

    ex 1

--- ex1/ex1.c
+++ ex1/ex1.c
@@ -2,13 +2,31 @@
 // (e.g., x) and set its value to something (e.g., 100). What value is the variable in the child process?
 // What happens to the variable when both the child and parent change the value of x?
 
+// Answers: The child and parent retain the values in their variables since fork() is only creating a copy of the parent process. They cannot affect each other's data.
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 
 int main(int argc, char *argv[])
 {
-    // Your code here
+    int x = 50;
+    int rc = fork();
 
+    if (rc < 0)
+    { // fork failed; exit
+        fprintf(stderr, "fork failed\n");
+        exit(1);
+    }
+    else if (rc == 0)
+    {
+        x = 7;
+        printf("Child x value: %d\n", x);
+    }
+    else
+    {
+        x = 3;
+        printf("Parent x value: %d\n", x);
+    }
     return 0;
 }
