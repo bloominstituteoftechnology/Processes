d74e98351547a82133f1cdeb3e8dd7b968d4f209
sadrak105 <sadrak10@gmail.com>
2018-06-20 17:23:43 -0400

    ex3

--- ex3/ex3.c
+++ ex3/ex3.c
@@ -5,10 +5,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/wait.h>
 
 int main(int argc, char* argv[])
 {
     // Your code here
+    int rc = fork();
+
+    if (rc < 0) {
+        fprintf(stderr, "fork failed\n");
+    }
+    else if (rc == 0) {
+        printf("hello\n");
+    }
+    else {
+        wait(NULL);
+        printf("goodbye\n");
+    }
 
     return 0;
 }
