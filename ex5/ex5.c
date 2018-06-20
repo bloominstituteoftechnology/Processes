738d16e53b5476d5f8d13832e30ba2b746b0b15e
harrisonbrock <harrison@harrisonbrock.com>
2018-06-20 11:22:39 -0400

    Add ex5 code

--- ex5/ex5.c
+++ ex5/ex5.c
@@ -1,21 +1,53 @@
 // Write a program that forks a child and creates a shared pipe
-// between the parent and child processes. Have the child write 
-// the three messages to the parent and have the parent print out 
-// the messages. 
+// between the parent and child processes. Have the child write
+// the three messages to the parent and have the parent print out
+// the messages.
 
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <sys/wait.h>
 
 #define MSGSIZE 16
 
-char* msg1 = "hello world #1";
-char* msg2 = "hello world #2";
-char* msg3 = "hello world #3";
+char* msg1 = "hello world #1\n";
+char* msg2 = "hello world #2\n";
+char* msg3 = "hello world #3\n";
 
 int main()
 {
     // Your code here
-    
+    char buffer[MSGSIZE];
+    int numberOfBytes;
+    int pipes[2];
+
+
+    if (pipe(pipes) < 0)
+    {
+        fprintf(stderr, "fork failed\n");
+        exit(1);
+    }
+
+    if (fork() > 0) {
+        printf("child writing to pipe\n");
+
+        write(pipes[1], msg1, MSGSIZE);
+        write(pipes[1], msg2, MSGSIZE);
+        write(pipes[1], msg3, MSGSIZE);
+    }
+    else
+    {
+        close(pipes[1]);
+
+        while ((numberOfBytes = (int) read(pipes[0], buffer, MSGSIZE)) > 0) {
+            printf("% s\n", buffer);
+        }
+
+        if (0 != numberOfBytes) exit(2);
+
+        printf("Done!!!\n");
+    }
+
     return 0;
 }
+
