9c818eea6a4dcfce63cf57a3ad58984477b64801
Jared Cuffe <jcuffe@gmail.com>
2018-06-20 11:44:56 -0700

    Fork+Waitpid exercise complete

--- ex3/ex3.c
+++ ex3/ex3.c
@@ -8,7 +8,20 @@
 
 int main(int argc, char* argv[])
 {
-    // Your code here
-
+    int rc = fork();
+    if (rc < 0)
+    {
+        printf("failure!");
+        exit(1);
+    }
+    else if (rc == 0)
+    {
+        printf("hello\n");
+    }
+    else
+    {
+        int wc = waitpid(rc, NULL, 0);
+        printf("goodbye\n");
+    }
     return 0;
 }
