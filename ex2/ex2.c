95e71fb1fb99f82c49e58fdfeda6c17157492854
Mark Wong <fmarkwong@gmail.com>
2018-06-20 14:25:27 -0700

    ex2

--- ex2/ex2.c
+++ ex2/ex2.c
@@ -9,6 +9,29 @@
 int main(int argc, char* argv[])
 {
     // Your code here 
+   
+    FILE* fp;
     
+    fp = fopen("text.txt", "w");
+
+    if (fp == NULL) {
+        fprintf(stderr, "sometihng went wrong with opening file\n");
+    } else {
+        int rc = fork();
+
+        if (rc < 0) {
+            fprintf(stderr, "fork failed\n");
+            exit(1);
+        } else if (rc == 0) {
+            printf("this is the child process with pid: %d \n", getpid());
+            fprintf(fp, "this is written from the child process!\n");
+        } else {
+            printf("this is the parent process with pid %d.  Child pid is %d \n", getpid(), rc);
+            fprintf(fp, "this is written from the parent process!\n");
+        }
+    }
+
+    fclose(fp);
+
     return 0;
 }
