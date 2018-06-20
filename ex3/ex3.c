59d9b53295b5c9c155470230318f5bf7f809e0e2
totsuya <supertatortots@gmail.com>
2018-06-20 17:05:49 -0400

    finished ex3

--- ex3/ex3.c
+++ ex3/ex3.c
@@ -5,10 +5,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/wait.h>
 
 int main(int argc, char* argv[])
 {
     // Your code here
+    int rc = fork();
 
+    if (rc < 0) {
+        fprintf(stderr, "fork blew up, try again! \n");
+        exit(1);
+    }
+        else if (rc == 0) {
+            printf("I am a child!\n");  
+        } else {
+            int wc = waitpid(rc, NULL, 0);
+            printf("I'm a parent and I should be going second!\n");
+        }
     return 0;
 }
