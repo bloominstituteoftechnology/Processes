8fcd334b0c9dedb8e6bd3a21e0a0b0f8ce980de4
Eric Gomez <eric_gomez_1@hotmail.com>
2018-06-20 13:32:25 -0700

    ex3 completed.
    initialized two character pointers that point to strings 'Hello' and 'Goodbye'. and then forked the process to have the child print hello and the parent goodbye but also implemented the waitpid function on the else statement to have the parent process wait on the process id specified in the first parameter to finish running.

--- ex3/ex3.c
+++ ex3/ex3.c
@@ -5,10 +5,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/wait.h>
 
 int main(int argc, char* argv[])
 {
+    char *p = "Goodbye";
+    char *c = "Hello";
     // Your code here
-
+    int rc = fork();
+    if (rc < 0 ) {
+        fprintf(stderr, "for failed\n");
+    }
+    else if (rc == 0) {
+        printf("%s\n", c);
+    }
+    else {
+        int wc = waitpid(rc, NULL, 0);
+        printf("%s\n", p);
+    }
     return 0;
 }
