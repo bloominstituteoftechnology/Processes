866de48fbf1c5e420b225d270bf37b8b538693f5
Mark Wong <fmarkwong@gmail.com>
2018-06-20 13:34:27 -0700

    ex1

--- ex1/ex1.c
+++ ex1/ex1.c
@@ -8,7 +8,22 @@
 
 int main(int argc, char *argv[])
 {
-    // Your code here
+    char* x = "hey you";
+
+    int rc = fork();
+
+    if (rc < 0) {
+        fprintf(stderr, "fork failed\n");
+        exit(1);
+    } else if (rc == 0) {
+        printf("this is the child process with pid: %d \n", getpid());
+        x = "I got assigned by the child\n";
+    } else {
+        printf("this is the parent process with pid %d.  Child pid is %d \n", getpid(), rc);
+        x = "I got assigned by the parent\n";
+    }
+
+    puts(x);
 
     return 0;
 }
