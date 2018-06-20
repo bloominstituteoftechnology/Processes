c7fa260304405acdd33d2fa677f3907076902e15
Nedu Robert <nedumrobert@gmail.com>
2018-06-20 14:50:31 -0700

    implemented function in ex1

--- ex1/ex1.c
+++ ex1/ex1.c
@@ -9,6 +9,21 @@
 int main(int argc, char *argv[])
 {
     // Your code here
+    int x = 100;
+    int rc = fork();
+
+    if (rc < 0) {    // fork failed; exit
+        fprintf(stderr, "fork failed\n");
+        exit(1);
+    } else if (rc == 0) {    // child process satisfies this branch
+        printf("hello, child here (pid: %d) and value of x here is: %d \n", (int) getpid(), x); // x = 100
+        x = 20;
+        printf("now, the value of x in child is: %d \n", x); // x = 20
+    } else {
+        printf("hello, parent here (pid: %d) of child %d and value of x here is: %d \n", (int) getpid(), rc, x); // x = 100
+        x = 20;
+        printf("now, the value of x in parent is: %d \n", x); // x = 200
+    }
 
     return 0;
 }
