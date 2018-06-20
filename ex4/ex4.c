91e724680518655a8276df0cfcf92aa19ffcacdd
Justin Hong <jhong2009@gmail.com>
2018-06-20 15:12:31 -0700

    in ex4.c: set up exec calls

--- ex4/ex4.c
+++ ex4/ex4.c
@@ -3,13 +3,39 @@
 // `execl()`, `execle()`, `execv()`, and others. Why do you think there 
 // are so many variants of the same basic call?
 
+// 
+
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char* argv[])
 {
     // Your code here    
+		printf("pid before fork(): %d\n", (int) getpid());
+		int rc = fork();
+		// --- child process start here ---
+
+    if (rc < 0) {
+      fprintf(stderr, "fork blew up, try again! \n");
+      exit(1);
+    } else if (rc == 0) {
+    	printf("child pid: %d\n", (int) getpid());
+    	char *myargs[3]; 
+
+    	myargs[0] = strdup("/bin/ls");
+    	myargs[1] = strdup("./");
+    	myargs[2] = NULL;
+
+    	// execvp(myargs[0], myargs);
+    	execl(myargs[0], myargs[1], myargs[2]);
+    	// execle(myargs[0], myargs[1], myargs[2]);
+			// execv(myargs[0], myargs); 
+    	printf("this should not be seen");
+    } else {
+    	printf("parent pid: %d\n", (int) getpid());
+    }
 
     return 0;
 }
