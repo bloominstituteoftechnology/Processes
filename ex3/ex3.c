3f004642c8ac5e5015e207272ac2aa77c4c370fe
Tylar Pierson <tylarpiersonbusiness@gmail.com>
2018-06-20 13:20:03 -0700

    added code for ex3 wait and fork

--- ex3/ex3.c
+++ ex3/ex3.c
@@ -5,10 +5,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <wait.h>
 
 int main(int argc, char* argv[])
 {
     // Your code here
-
+    int rc = fork();
+    if (rc < 0) {
+        fprintf(stderr, "fork failed\n");
+        exit(1);
+    } else if (rc == 0) {
+        printf("hello (pid: %d) \n", (int) getpid());
+    } else {
+        int wc = waitpid(rc, NULL, 0);
+        printf("goodbye (pid: %d) \n", (int) getpid());
+    }
     return 0;
 }
