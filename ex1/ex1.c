8a1cf22d5d2aa599e0ef95d0c97ff721959cd3a1
Finlie <dfinley2917@gmail.com>
2018-06-20 18:29:10 -0400

    ex1 complete

--- ex1/ex1.c
+++ ex1/ex1.c
@@ -5,10 +5,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <wait.h>
 
 int main(int argc, char *argv[])
 {
-    // Your code here
+    int x = 100;
+    int rc = fork();
+    // ------------------------------------------------ child process starts executing here
+    if (rc < 0)
+    { // fork failed; exit
+        fprintf(stderr, "fork failed\n");
+        exit(1);
+    }
+    else if (rc == 0)
+    { // child process satisfies this branch
+        printf("CHILD: the value of X in the child process is %d and the pid: %d) \n", x, (int)getpid());
+        x++;
+        printf("The incremented value of X in the child is %d\n", x);
+    }
+    else
+    {
+        int wc = waitpid(rc, NULL, 0);
+        printf("PARENT:  The value of X is %d and the PID is (pid: %d)\n", x, (int)getpid());
+    } // Your code here
 
     return 0;
 }
