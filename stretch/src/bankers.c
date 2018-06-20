1008aeea7781e097d3ad78e53c523e4e7abf7db5
harrisonbrock <harrison@harrisonbrock.com>
2018-06-20 15:19:09 -0400

    ADDED check to make sure command has two agreements

--- stretch/src/bankers.c
+++ stretch/src/bankers.c
@@ -94,23 +94,10 @@ int get_random_amount(void)
  */
 int main(int argc, char **argv)
 {
-	// Parse the command line
-	
-	// vvvvvvvvvvvvvvvvvv
-	// !!!! IMPLEMENT ME:
 
-	// We expect the user to add the number of simulataneous processes
-	// after the command name on the command line.
-	//
-	// For example, to fork 12 processes:
-	//
-	//  ./bankers 12
-
-	// Check to make sure they've added one paramter to the command line
-	// with argc. If they didn't specify anything, print an error
-	// message to stderr, and exit with status 1:
-	//
-	// "usage: bankers numprocesses\n"
+    if (argc != 2) {
+        fprintf(stderr, "usage: bankers numprocesses\n");
+    }
 	
 	// Store the number of processes in this variable:
 	// How many processes to fork at once
