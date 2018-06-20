19d09b408d8fdd809ce76dc81f7f48a5e3c03530
Eric Gomez <eric_gomez_1@hotmail.com>
2018-06-20 15:13:55 -0700

    ex5 completed.
    ex5 now forks the main process and creates a pipe between the parent and child. the child writes to the pipe array and the parent reads from the pipe array and displays to the terminal

--- ex5/ex5.c
+++ ex5/ex5.c
@@ -16,6 +16,37 @@ char* msg3 = "hello world #3";
 int main()
 {
     // Your code here
+    char inbuf[MSGSIZE];
+    int p[2];
+
+    if (pipe(p) < 0) 
+    {
+        fprintf(stderr, "pipe failed\n");
+        exit(1);
+    }
     
+    const int RC = fork();
+    
+    if (RC < 0) 
+    {
+        fprintf(stderr, "fork failed!\n");
+    }
+    
+    else if (RC == 0) 
+    {
+        write(p[1], msg1, MSGSIZE);
+        write(p[1], msg2, MSGSIZE);
+        write(p[1], msg3, MSGSIZE);
+    }
+    else
+    {
+        for (int i = 0; i < 3; i++) 
+        {
+            // read 16 bytes of data from the read file descriptor 
+            read(p[0], inbuf, MSGSIZE);
+            printf("parent: %s\n", inbuf);
+        }
+    }
+
     return 0;
 }
