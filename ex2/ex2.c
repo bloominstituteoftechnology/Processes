73625bb1e19f6423d9fd71e0ae14a2b25fdd70db
Justin Hong <jhong2009@gmail.com>
2018-06-20 13:40:21 -0700

    in ex2.c: set up solution to check if both child and parent access the same file pointer

--- ex2/ex2.c
+++ ex2/ex2.c
@@ -9,6 +9,46 @@
 int main(int argc, char* argv[])
 {
     // Your code here 
-    
+    FILE *fp;
+    int c;
+
+    fp = fopen("text.txt", "w"); // open text.txt
+    fprintf(fp, "%s %s %s %d", "We", "are", "in", 2018); // write a string to text.txt
+    fclose(fp);
+
+    int rc = fork();
+    // --- child process starts here ---
+    if (rc < 0)
+    {
+        fprintf(stderr, "Fork blew up! Try again \n");
+        exit(1);
+    }
+    else if (rc == 0)
+    {
+        printf("Child result here, fp = %p\n \n", fp); // print address of the text.txt
+        fp = fopen("text.txt", "r");
+        while(1) {
+		    	c = fgetc(fp); // get next character from fp and advances index
+		    	if( feof(fp) ) { // test end of file indicator
+		    		break ;
+		    	}
+		    	printf("%c", c);
+		    }
+		    printf("\n");
+		    fclose(fp);
+    } else {
+        printf("Parent result here, fp = %p\n \n", fp);
+        fp = fopen("text.txt", "r");
+        while(1) {
+			  	c = fgetc(fp);
+			  	if( feof(fp) ) {
+			  		break ;
+			  	}
+			  	printf("%c", c);
+			  }
+			  printf("\n");
+			  fclose(fp);
+    }
+
     return 0;
 }
