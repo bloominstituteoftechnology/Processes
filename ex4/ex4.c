082524db8b4d4fb04b3c39890c9ab7f4866ec516
kyle <kauzlarich.kyle@gmail.com>
2018-06-21 13:17:01 -0700

    Calling LS added in a rc fork so the child prints before calling ls letting you know its calling ls then ls is called and shows you the proper usage on the parrent fork it waits for the child to finish before telling you that the child called called LS

--- ex4/ex4.c
+++ ex4/ex4.c
@@ -6,10 +6,24 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <sys/wait.h>
+#include <string.h>
 
 int main(int argc, char* argv[])
 {
     // Your code here    
+    const char *ls = "/bin/ls";
+    int rc = fork();
+    
+    if (rc == 0) {
+        printf("i am the child and now im going to call LS\n");
+        execv(ls, NULL);
+        return 0;
+    } else if (rc != 0) {
+        waitpid(rc, NULL, 0);
+        printf("the child has called LS\n");
+        return 0;
+    }
 
     return 0;
 }
