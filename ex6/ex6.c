df2b52f5518da4ad67d40c544bd53c864480d101
Abraham Bueno <abueno@localhost.localdomain>
2018-06-21 15:46:53 -0500

    Completed project requirements. Redid some exercises, for practice.

--- ex6/ex6.c
+++ ex6/ex6.c
@@ -11,48 +11,35 @@ turns out to only be the case for OSX versions < 10.12. Anything later than that
 and `clock_gettime()` should work just fine. 
 */
 
-// #include <stdio.h>
-// #include <unistd.h>
-// #include <stdlib.h>
-// #include <time.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <time.h>
 
-// #define number_iter 1000000
-// #define BILLION 1000000000L
-
-// int main()
-// {
-//     // Your code here
-    
-//     return 0;
-// }
-#include <stdio.h>	/* for printf */
-#include <stdint.h>	/* for uint64 definition */
-#include <stdlib.h>	/* for exit() definition */
-#include <time.h>	/* for clock_gettime */
-
-#define BILLION 1000000000L
 #define number_iter 1000000
+#define BILLION 1000000000L
 
-int localpid(void) {
-	static int a[9] = { 0 };
-	return a[0];
-}
-
-main(int argc, char **argv)
+int main()
 {
-	uint64_t diff;
+    // Your code here
+	__uint64_t diff;
+    	// uint64_t diff;
 	struct timespec start, end;
-	int i;
+	int i = 0;
 
 
 	/* the time spent sleeping will not count (but there is a bit of overhead */
 	clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &start);	/* mark start time */
-	sleep(1);	/* do stuff */
-    
+	//sleep(1);	/* do stuff */
+	for (;i < number_iter; i++) {
+		fprintf(stdout, "");
+	}
+
 	clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &end);		/* mark the end time */
 
 	diff = BILLION * (end.tv_sec - start.tv_sec) + end.tv_nsec - start.tv_nsec;
+	__uint64_t avg = (diff/number_iter);
 	printf("elapsed process CPU time = %llu nanoseconds\n", (long long unsigned int) diff);
-
-	exit(0);
+	printf("Average time, CPU time / number of interations: %llu\n", (long long unsigned int) avg);
+    return 0;
 }
