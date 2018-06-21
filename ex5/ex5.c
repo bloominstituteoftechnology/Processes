82528c304a553294c6163a359b55ab7912a0b26c
Justin Hong <jhong2009@gmail.com>
2018-06-21 13:17:30 -0700

    in ex5.c: set up a solution for piping

--- ex5/ex5.c
+++ ex5/ex5.c
@@ -15,7 +15,49 @@ char* msg3 = "hello world #3";
 
 int main()
 {
-    // Your code here
-    
-    return 0;
+  // Your code here
+  // buffer that holds incoming data
+  char inbuf[MSGSIZE];
+  // instantiate pipe where p[0] is to read and p[1] is to write
+	int fd[2];
+
+	// error check: pipe fails
+	if (pipe(fd) == -1) {
+		printf("pipe failed");
+		return 1;
+	}
+
+	printf("pid before fork(): %d\n", (int) getpid());
+	int rc = fork();
+  
+  if (rc < 0) {
+    fprintf(stderr, "fork blew up, try again! \n");
+    exit(1);
+  } else if (rc == 0) {
+  	printf("child pid: %d\n", (int) getpid());
+  	// close reading end of pipe
+  	close(fd[0]); 
+  	// write messages
+  	write(fd[1], msg1, MSGSIZE);
+  	write(fd[1], msg2, MSGSIZE);
+  	write(fd[1], msg3, MSGSIZE);
+  	// close writing end of pipe
+  	close(fd[1]);
+  } else {
+  	printf("parent pid: %d\n", (int) getpid());
+  	int wc = waitpid(rc, NULL, 0);
+  	wait(NULL);
+  	// close writing end of pipe
+  	close(fd[1]);
+  	// read messages from the read file descriptor
+  	for (int i = 0; i < 3; i++) {
+  		// read data from inbuf
+  		read(fd[0], inbuf, MSGSIZE);
+  		printf("%s\n", inbuf);
+  	}
+  	// close reading end
+  	close(fd[0]);
+  } 
+
+  return 0;
 }
