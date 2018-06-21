51f23ac132e72c2266347e6ec9d05a9552a962c6
Michal Chmielniak <michal@Michals-MacBook-Pro.local>
2018-06-21 10:11:28 -0400

    added in code for ex3 & ex4 with potneital printf issues with ex4 currently

--- ex3/ex3.c
+++ ex3/ex3.c
@@ -9,6 +9,16 @@
 int main(int argc, char* argv[])
 {
     // Your code here
-
+int rc = fork();
+    if (rc < 0) {
+        fprintf(stderr, "fork failed\n");
+        exit(1);
+        } 
+    else if (rc == 0) {
+        printf("hello, I'm a child\n");
+    } else {
+        int wc = waitpid(rc, NULL, 0);
+        printf("goodbye, I'm a parent\n");
+    }
     return 0;
 }
--- ex4/ex4.c
+++ ex4/ex4.c
@@ -10,6 +10,19 @@
 int main(int argc, char* argv[])
 {
     // Your code here    
-
+int rc = fork();
+    if (rc < 0) {
+        fprintf(stderr, "fork failed\n");
+        exit(1);
+    }
+    else if (rc == 0) {
+        printf("hello, child here (pid: %d) \n", (int) getpid());
+        char *args[]={"/bin/ls",NULL};
+        execvp(args[0], args);
+        printf("invisible");
+    } else {
+        int wc = waitpid(rc, NULL, 0);
+        printf("hello, parent here (pid: %d) of child %d\n", (int) getpid(), rc);
+    }
     return 0;
 }
