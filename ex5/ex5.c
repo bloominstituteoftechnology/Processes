163bb9f3a4aedd4aab18901e158f7157a95086c3
2940cristian <2940cristian@gmail.com>
2018-06-21 11:26:07 -0700

    Ex5 completed

--- ex5/ex5.c
+++ ex5/ex5.c
@@ -15,7 +15,26 @@ char* msg3 = "hello world #3";
 
 int main()
 {
+    int rc = fork();
     // Your code here
+
+    char inbuf[MSGSIZE];
+
+    int p[2];
+
+    if(pipe(p) < 0) {
+        fprintf(stderr, "pipe failed\n");
+        exit(1);
+    }
+
+    write(p[1], msg1, MSGSIZE);
+    write(p[1], msg2, MSGSIZE);
+    write(p[1], msg3, MSGSIZE);
+
+    for (int i = 0; i < 3; i++) {
+        read(p[0], inbuf, MSGSIZE);
+        printf("%s\n", inbuf);
+    }
     
     return 0;
 }
