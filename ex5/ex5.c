4f106669c5c730350d7538d4ccc603c1466c1386
Anthony <anthony@anthonys-mbp.lan>
2018-06-21 14:03:29 -0400

    Exercise 5:
    in Processes > ex5 > ex5.c:
    Finished program

--- ex5/ex5.c
+++ ex5/ex5.c
@@ -15,7 +15,28 @@ char* msg3 = "hello world #3";
 
 int main()
 {
-    // Your code here
-    
+    char inbuf[MSGSIZE];    
+    int p[2]; 
+
+    if (pipe(p) < 0) {
+        exit(1);
+    } 
+    int rc = fork();
+    if (rc < 0) {
+        exit(1);
+    } else if (rc == 0) {
+        write(p[1], msg1, MSGSIZE);
+        write(p[1], msg2, MSGSIZE);
+        write(p[1], msg3, MSGSIZE);
+    } else {
+        for (int i = 0; i < 3; i++) {
+        read(p[0], inbuf, MSGSIZE);
+        printf('%s\n', inbuf);
+        }
+    }
+
     return 0;
 }
+
+
+
