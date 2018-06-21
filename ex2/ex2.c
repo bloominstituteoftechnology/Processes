4ffdcd4d661b1a7b0f0782e0fa45954acbf0c94c
Anthony <anthony@anthonys-mbp.lan>
2018-06-21 13:29:15 -0400

    Exercise 2
    in Process > ex2.c:
    Completed program to call wait function

--- ex2/ex2.c
+++ ex2/ex2.c
@@ -2,13 +2,26 @@
 // and then calls `fork()` to create a new process. Can both the child and parent access the file descriptor 
 // returned by `fopen()`? What happens when they are written to the file concurrently?
 
+// Purpose: Wait on the child before executing
+
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
 
 int main(int argc, char* argv[])
 {
-    // Your code here 
-    
+    // Open file
+    FILE *fp = fopen('./text.txt', 'w+');
+    int rc = fork();
+    if (rc < 0) {  
+        fprintf(stderr, "fork failed\n");
+        exit(1);
+    } else if (rc == 0) {
+        fprintf(fp, '%s', ' from child\n');
+        fclose(fp);
+    } else {
+        fprintf(fp, '%s', ' from parent\n');
+        fclose(fp);
+    }
     return 0;
 }
