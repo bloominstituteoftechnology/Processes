094ae55605005bc130569dd0d96c5453e2b0ab78
Finlie <dfinley2917@gmail.com>
2018-06-21 10:31:26 -0400

    ex3 complete

--- ex3/ex3.c
+++ ex3/ex3.c
@@ -5,10 +5,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <wait.h>
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
-    // Your code here
+    int rc = fork();
+    if (rc < 0)
+    {
+        fprintf(stderr, "fork failed\n");
+        exit(1);
+    }
+    else if (rc == 0)
+    {
+        printf("Child says \"Hello!\"\n");
+    }
+    else
+    {
+        int wc = waitpid(rc, NULL, 0);
+        printf("Parent says \"Goodbye!\"\n");
+    }
 
     return 0;
 }
