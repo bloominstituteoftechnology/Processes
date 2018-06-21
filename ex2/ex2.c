80b657a0d304aa89ca5332c5f7af0c738f12600a
Justin Rigler <justinmrigler@gmail.com>
2018-06-20 20:35:09 -0400

    ex2

--- ex2/ex2.c
+++ ex2/ex2.c
@@ -1,14 +1,34 @@
-// Write a program that opens the text.txt  file (with the `fopen()` system call) located in this directory 
-// and then calls `fork()` to create a new process. Can both the child and parent access the file descriptor 
+// Write a program that opens the text.txt  file (with the `fopen()` system call) located in this directory
+// and then calls `fork()` to create a new process. Can both the child and parent access the file descriptor
 // returned by `fopen()`? What happens when they are written to the file concurrently?
 
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
-    // Your code here 
-    
+    FILE *file = fopen("text.txt", "w");
+
+    int rc = fork();
+
+    if (rc < 0)
+    {
+        printf(stderr, "fork failed, exiting\n");
+        exit(1);
+    }
+    else if (rc == 0)
+    {
+        printf("child processes\n");
+        char childText[] = "This text is from the child\n";
+        fwrite(childText, 1, sizeof(childText) - 1, file);
+    }
+    else
+    {
+        printf("parent processes\n");
+        char parentText[] = "This text is from the parent\n";
+        fwrite(parentText, 1, sizeof(parentText) - 1, file);
+    }
+
     return 0;
 }
