33cbbf5793dfbda3bb0a92792c992f952d6cb000
hilal aissani <hilalaissani@gmail.com>
2018-06-20 23:09:42 -0400

    80%

--- ex2/ex2.c
+++ ex2/ex2.c
@@ -1,14 +1,64 @@
-// Write a program that opens the text.txt  file (with the `fopen()` system call) located in this directory 
-// and then calls `fork()` to create a new process. Can both the child and parent access the file descriptor 
+// Write a program that opens the text.txt  file (with the `fopen()` system call) located in this directory
+// and then calls `fork()` to create a new process. Can both the child and parent access the file descriptor
 // returned by `fopen()`? What happens when they are written to the file concurrently?
 
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <sys/types.h>
 
-int main(int argc, char* argv[])
+int readFile()
+
 {
-    // Your code here 
-    
+    FILE *fp;
+    fp = fopen("text.txt", "r");
+    char singleLine[150];
+    while (!feof(fp))
+    {
+        fgets(singleLine, 150, fp);
+    }
+    puts(singleLine);
+    fclose(fp);
+    return (0);
+}
+
+int writeFile()
+
+{
+    FILE *fp;
+    fp = fopen("text.txt", "w");
+    fprintf(fp, " i love lambda school too much \n");
+    fclose(fp);
+    return (0);
+}
+
+int main(int argc, char *argv[])
+{
+    printf(" getpid is %d \n", (int)getpid());
+    //writeFile();
+    readFile();
+
+    //////////////////////////////
+    int rc = fork();
+    printf("*****(rc:%d) \n", rc);
+    /////////////////////////////////////
+    if (rc < 0)
+    {
+        fprintf(stderr, " fork failed \n");
+    }
+    else if (rc == 0)
+    {
+        printf("=======> child here (pid:%d)\n", (int)getpid());
+        writeFile();
+        // readFile();
+    }
+    else
+    {
+
+        printf("========>parent here (pid: %d)  of child  (rc:%d)\n", (int)getpid(), rc);
+        writeFile();
+        //readFile();
+    }
+
     return 0;
 }
--- ex4/ex4.c
+++ ex4/ex4.c
@@ -1,15 +1,45 @@
 // Write a program that calls `fork()` and then calls some form of `exec()`
-// to run the program `/bin/ls`. Try a few variants of `exec()`, such as 
-// `execl()`, `execle()`, `execv()`, and others. Why do you think there 
+// to run the program `/bin/ls`. Try a few variants of `exec()`, such as
+// `execl()`, `execle()`, `execv()`, and others. Why do you think there
 // are so many variants of the same basic call?
 
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
 
-int main(int argc, char* argv[])
+int ronaldo = 7;
+
+int main(int argc, char *argv[])
 {
-    // Your code here    
+    printf("====>ronaldo : %d \n", ronaldo);
+    //////////////////////////////////////////////
+    // for (int i = 0; i < argc; i++)
+    // {
+
+    //     printf(" %s \n", argv[i]);
+    // }
+
+    printf(" .......hello exec......... \n");
+    printf(" process runing: %d \n", (int)getpid());
+    // //////////////////////////
+    //int rc = fork();
+    //printf("*****(rc:%d) \n", rc);
+    // ///////////////////////////////
+    // if (rc < 0)
+    // {
+    //     fprintf(stderr, " fork failed \n");
+    //     exit(1);
+    // }
+    // else if (rc == 0)
+    // {
+
+    //     printf("=======> child here (pid:%d)\n", (int)getpid());
+    // }
+    // else
+    // {
+
+    //     printf("========>parent here (pid: %d)  of child  (rc:%d)\n", (int)getpid(), rc);
+    // }
 
     return 0;
 }
