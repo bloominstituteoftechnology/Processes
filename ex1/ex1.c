382f2b362ffdca3f9a5efd4c243b6363611cc182
ktan114 <22802321+ktan114@users.noreply.github.com>
2018-06-21 15:28:10 -0400

    Fixed ex1 and ex2

--- ex1/ex1.c
+++ ex1/ex1.c
@@ -18,9 +18,9 @@ int main(int argc, char *argv[])
         fprintf(stderr, "fork failed\n");
         exit(1);
     } else if (rc == 0) {
-        printf("hello, child here (pid: %d)\n", (int) getpid());
+        printf("hello, child here (pid: %d)\n", (int) getpid()); // Child: gets rc = 0 when fork is called
     } else {
-        printf("hello, parent (pid: %d) of child %d\n", (int) getpid(), rc);
+        printf("hello, parent (pid: %d) of child %d\n", (int) getpid(), rc); // Parent: gets rc = child's PID if fork is done correctly 
     }
     return 0;
 }
--- ex2/ex2.c
+++ ex2/ex2.c
@@ -6,9 +6,7 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-int main(int argc, char* argv[])
-{
-    // Your code here 
+void openFile() {
     FILE * fp;
     int c;
     fp = fopen("text.txt", "r");
@@ -17,6 +15,23 @@ int main(int argc, char* argv[])
             putchar(c);
             printf("\n");
         fclose(fp);
+    }
+}
+
+int main(int argc, char* argv[])
+{
+    // Your code here 
+    openFile();
+    int rc = fork();
+
+    if (rc < 0) {
+        fprintf(stderr, "fork failed\n");
+        exit(1);
+    } else if (rc == 0) {
+        // openFile();
+        printf("Child: success with opening file\n"); // Does not have access to openFile() or the parent process aside from fork
+    } else {
+        printf("Parent: success with opening file\n");
     }
     return 0;
 }
