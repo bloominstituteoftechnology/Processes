0324a9df313d784c13ab660828878f826870b0cf
K <kkhaag@gmail.com>
2018-06-21 13:47:42 -0600

    Generic Update

--- ex3/ex3.c
+++ ex3/ex3.c
@@ -5,10 +5,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/wait.h>
 
 int main(int argc, char* argv[])
 {
-    printf
+    int ret = fork();
+
+    if (ret < 0) {
+        fprintf(stderr, "Failed to Fork");
+        exit(1);
+    }
+    else if (ret == 0) {
+        printf("hello\n");
+        printf("Child (pid: %d)\n\n", (int) getpid());
+    }
+    else {
+        int wc = waitpid(ret, NULL, 0); //Is this all I need to do?
+        printf("goodbye\n\n");
+        printf("Parent (pid: %d) \nAND\nChild (pid: %d)\n", (int) getpid(), ret);
+    }
 
     return 0;
 }
