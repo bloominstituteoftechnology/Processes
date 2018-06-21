b64d9ec825e85078899734e2e0dbc2ed72443ddf
kyle <kauzlarich.kyle@gmail.com>
2018-06-21 12:27:00 -0700

    added hello world added a fork and if else if on the fork to get each process to only print one word and added waitpid to ensure the child will print hello before the parrent prints world

--- ex3/ex3.c
+++ ex3/ex3.c
@@ -5,10 +5,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/wait.h>
 
 int main(int argc, char* argv[])
 {
     // Your code here
+    
+    int rc = fork();
+
+    if (rc == 0) {
+        printf("hello ");
+        return 0;
+    } else if (rc != 0) {
+        waitpid(rc, NULL, 0);
+        printf("world\n");
+        return 0;
+    } else {
+        printf("Error");
+        return 1;
+    }
+
 
     return 0;
 }
