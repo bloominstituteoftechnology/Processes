4d9a7d8ba617096e6f670ad2cbf7cd45d479f6d8
Nedu Robert <nedumrobert@gmail.com>
2018-06-21 09:45:23 -0700

    Implemented ex3

--- ex3/ex3.c
+++ ex3/ex3.c
@@ -8,7 +8,17 @@
 
 int main(int argc, char* argv[])
 {
-    // Your code here
+    int rc = fork();
+
+    if (rc < 0) {    // fork failed; exit
+        fprintf(stderr, "fork failed\n");
+        exit(1);
+    } else if (rc == 0) {    // child process satisfies this branch
+        printf("Hello!\n");
+    } else {
+        int wc = waitpid(rc, NULL, 0);    // `waitpid` call added here
+        printf("Goodbye!\n");
+    }
 
     return 0;
 }
