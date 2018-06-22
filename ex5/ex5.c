f6f1589cebf9da9a65e95511028c7f3d975a4d61
odax <nr10@albion.edu>
2018-06-22 12:34:11 -0400

    completed ex5

--- ex5/ex5.c
+++ ex5/ex5.c
@@ -15,7 +15,38 @@ char* msg3 = "hello world #3";
 
 int main()
 {
-    // Your code here
+    
+    char inbuf[MSGSIZE];
+    int p[2];
+
+    //if this call below returns a status less than 0, pipe failed
+    if (pipe(p) < 0) {
+        fprintf(stderr, "pipe failed");
+        exit(1);
+    }
+
+    int rc = fork();
+    if (rc < 0) {
+        fprintf(stderr, "fork failed\n");
+        exit(2);
+    }
+
+    else if (rc == 0) {
+        printf("child writing to pipe\n");
+        write(p[1], msg1, MSGSIZE);
+        write(p[1], msg2, MSGSIZE);
+        write(p[1], msg3, MSGSIZE);
+    }
+
+    else {
+        close (p[1]);
+        printf("parent reading from pipe\n");
+        while (read(p[0], inbuf, MSGSIZE) > 0) {
+            printf("%s\n", inbuf);
+        }
+
+        printf("finished reading\n");
+    }
     
     return 0;
 }
