42387de25a5eb3f9db7f79b64ad1afa3ee95e35d
Danny Vail <danny_vail@outlook.com>
2018-08-08 17:47:39 -0400

    Completed exercise 4 - Example 4 demonstrates several exec() system calls. Each accomplishes the same thing but requires different formats for its arguments: - execl() takes the full path of the file to execute and a list of arguments to be passed to the file. The list also starts with the file name and ends with NULL. - execlp() takes the file name and the same type of argument list as execl() - execv() takes the full path of the file to execute and an array of arguments. The first element in the array is the file name and the last element is NULL. - execvp() takes the file name and the same type of argument array as execv()

--- ex4/ex4.c
+++ ex4/ex4.c
@@ -10,7 +10,63 @@
 
 int main(void)
 {
-    // Your code here    
+  printf("Main process (pid: %d)\n", (int)getpid());
+  printf("Forking child process...\n");
+  int child = fork();
 
-    return 0;
+  if (child < 0)
+  {
+    fprintf(stderr, "Fork failed\n");
+    exit(1);
+  }
+  else if (child == 0)
+  {
+    printf("Child process (pid: %d)\n", (int)getpid());
+    printf("Transforming child process into list command...\n");
+    
+    /*
+    // Using execl(): int execl(const char *path, const char *arg, ...);
+    char *path = "/bin/ls";
+    char *arg0 = "ls";
+    char *arg1 = "-a";
+    char *arg2 = NULL;
+    execl(path, arg0, arg1, arg2);
+    printf("Sanity check. This should not print.");
+    */
+
+    /*
+    // Using execlp(): int execlp(const char *file, const char *arg, ...);
+    char *filename = "ls";
+    char *arg0 = "ls";
+    char *arg1 = "-a";
+    char *arg2 = NULL;
+    execlp(filename, arg0, arg1, arg2);
+    printf("Sanity check. This should not print.");
+    */
+
+    /*
+    // Using execv(): int execv(const char *path, char *const argv[]);
+    char *path = "/bin/ls";
+    char *myargs[3];
+    myargs[0] = "ls";
+    myargs[1] = "-a";
+    myargs[2] = NULL;
+    execv(path, myargs);
+    printf("Sanity check. This should not print.");
+    */
+
+    // Using execvp(): int execvp(const char *file, char *const argv[]);
+    char *myargs[3];
+    myargs[0] = "ls";
+    myargs[1] = "-a";
+    myargs[2] = NULL;
+    execvp(myargs[0], myargs);
+    printf("Sanity check. This should not print.");
+  }
+  else
+  {
+    printf("Parent process (pid: %d) of child %d\n", (int)getpid(), child);
+  }
+
+  return 0;
 }
