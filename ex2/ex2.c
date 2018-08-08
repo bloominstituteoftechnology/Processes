6d6cecbfb5d41bb07ed58e1be648b60f5ee512fc
Christina Kopecky <kopecky12112@gmail.com>
2018-08-08 13:34:33 -0700

    added forking capability to ex2
    parent/child can access file and write to it
    
    used song lyric to demo that parent writes first then child.

--- ex2/ex2.c
+++ ex2/ex2.c
@@ -1,5 +1,7 @@
-// Write a program that opens the text.txt  file (with the `fopen()` system call) located in this directory 
-// and then calls `fork()` to create a new process. Can both the child and parent access the file descriptor 
+// Write a program that opens the text.txt  file (with the `fopen()` system call) located in this 
+//directory 
+// and then calls `fork()` to create a new process. 
+//Can both the child and parent access the file descriptor 
 // returned by `fopen()`? What happens when they are written to the file concurrently?
 
 #include <stdio.h>
@@ -10,7 +12,7 @@
 int main(void)
 {
     FILE *fp;
-    char data[140];
+    // char data[140];
     printf("opening the file text.txt in read/write mode");
     fp = fopen("text.txt", "r+");
     if (fp == NULL){
@@ -18,12 +20,30 @@ int main(void)
         return 1;
     }
     
-    printf("\n Enter some text to write into file text.txt:");
-    while(strlen (gets( data )) > 0){
-        fputs(data, fp);
-        fputs("\n", fp);
+    int rc = fork();
+
+    if (rc < 0){
+        fprintf(stderr, "fork failed\n");
+        exit(1);
     }
-    printf("Closing the file text.txt");
+    else if (rc == 0){
+        printf("\nFork process child...\n");
+        char *fileStr1 = "Take a sad song and make it better...\n";
+        fwrite(fileStr1, 1, strlen(fileStr1), fp);
+    }
+    else {
+        printf("\nParent process...\n");
+        char *fileStr2 = "Hey, Jude...Don't make it bad...\n";
+        fwrite(fileStr2, 1, strlen(fileStr2), fp);
+    }
+    // printf("\n Enter some text to write into file text.txt:\n");
+    // while(strlen(gets(data))> 0){
+    //     fputs(data, fp);
+    //     fputs("\n", fp);
+    // }
+    
+    
+    printf("Closing the file text.txt\n");
     fclose(fp);
     return 0;
 }
