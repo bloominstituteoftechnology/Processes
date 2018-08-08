e00cf9530f68e828af0160b5c13e0d4e1da95bf5
bbenefield89 <bsquared18@gmail.com>
2018-08-08 15:16:01 -0400

    WRITE TO `text.txt` WITH `fopen()`
    - in ex2.c
    create child process and have both parent & child write to `text.txt`

--- ex2/ex2.c
+++ ex2/ex2.c
@@ -9,6 +9,27 @@
 int main(void)
 {
     // Your code here 
+    FILE *text = fopen("./text.txt", "r+");
+
+    // printf("\nPointer to `text.txt`: %p\n\n", text);
+
+    int child = fork();
+
+    if (child < 0)
+    {
+        printf("There was an error when creating the child process.");
+        exit(1);
+    }
+    if (child == 0)
+    {
+        printf("\nPointer to Child process `text.txt`: %p\n\n", text);
+        fwrite("Child", 1, 5, text);
+    }
+    else
+    {
+        printf("\nPointer to Parent process `text.txt`: %p\n\n", text);
+        fwrite("Parent\n\n", 1, 8, text);
+    }
     
     return 0;
 }
