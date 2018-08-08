fd3a02c133d84fcf5e6ba9ae76a8ec6afc7e4f7b
Bee C <b.choeung@gmail.com>
2018-08-08 15:15:09 -0400

    bee Choeung

--- ex1/ex1.c
+++ ex1/ex1.c
@@ -1,3 +1,4 @@
+//making comments to make pull request
 // Write a program that calls `fork()`. Before calling `fork()`, have the main process access a variable
 // (e.g., x) and set its value to something (e.g., 100). What value is the variable in the child process?
 // What happens to the variable when both the child and parent change the value of x?
