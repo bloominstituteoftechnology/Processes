6f2d4deea211cd3ebe5e93c889119b9a29e2df59
astrebeck <alexstrebeck@gmail.com>
2018-08-08 16:05:17 -0600

    ex4 initial commit

--- ex4/ex4.c
+++ ex4/ex4.c
@@ -10,7 +10,19 @@
 
 int main(void)
 {
-    // Your code here    
+    int rc = fork();
+
+    if (rc < 0) {    // fork failed; exit
+    fprintf(stderr, "fork failed\n");
+    exit(1);   
+    } else if (rc == 0) {    // child process satisfies this branch
+        printf("hello, child here (pid: %d) \n", (int) getpid());
+        execvp("/bin/ls");     // runs the word count program, passing in the `myargs` array to the word count program as arguments
+        printf("this should not be seen");
+    } else {
+        int wc = waitpid(rc, NULL, 0);    // `waitpid` call added here
+        printf("hello, parent here (pid: %d) of child %d\n", (int) getpid(), rc);
+    }
 
     return 0;
 }
