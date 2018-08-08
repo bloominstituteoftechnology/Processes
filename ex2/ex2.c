a8891502387c5a8f7a260211872ccd605f4bfa74
honda0306 <christopher.honda92@gmail.com>
2018-08-08 14:56:25 -0700

    Completed ex2; text.txt file has string position reversed e.g. String 2 is string 1

--- ex2/ex2.c
+++ ex2/ex2.c
@@ -4,11 +4,37 @@
 
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
 #include <stdlib.h>
 
 int main(void)
 {
     // Your code here 
+    FILE *fp;
+    printf("In progress: open text.txt in read/write");
+    fp = fopen("text.txt", "r+");
     
+    if (fp == NULL) {
+        printf("File will not open");
+        return 1;
+    }
+
+    int rc = fork();
+
+    if (rc < 0) {
+        fprintf(stderr, "Failure to fork\n");
+        exit(1);
+    } else if (rc == 0) {
+        printf("Forking child\n");
+        char *fileStr1 = "but perfect practice does.\n";
+        fwrite(fileStr1, 1, strlen(fileStr1), fp);
+    } else {
+        printf("\nProcessing parent\n");
+        char *fileStr2 = "Practice does not lead to perfection, \n";
+        fwrite(fileStr2, 1, strlen(fileStr2), fp);
+    }
+    
+    printf("The file will now close.");
+    fclose(fp);
     return 0;
 }
