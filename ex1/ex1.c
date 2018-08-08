97ba95a625bdc0f10a51cce433cb745052bd3eac
Steve <shalomlandsman@gmail.com>
2018-08-08 17:43:56 -0400

    fork

--- ex1/ex1.c
+++ ex1/ex1.c
@@ -9,6 +9,20 @@
 int main(void)
 {
     // Your code here
-
+    int x = 100;
+    printf("%d\n", x);
+    int rc = fork();
+    if (rc < 0) {    // fork failed; exit
+        fprintf(stderr, "fork failed\n");
+        exit(1);
+    } else if (rc == 0) {    // child process satisfies this branch
+        printf("%d\n", x);
+        x = 200;
+        printf("%d\n", x);
+    } else {
+        printf("%d\n", x);
+        x = 200;
+        printf("%d\n", x);
+    }
     return 0;
 }
