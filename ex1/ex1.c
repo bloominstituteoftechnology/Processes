ff3c7ed34101c3ac0b62dd08303c58d148306672
eddygonzalez9708 <edward.gonzalez9708@gmail.com>
2018-08-08 19:04:49 -0400

    Completed Exercise 1.

--- ex1/ex1.c
+++ ex1/ex1.c
@@ -9,6 +9,21 @@
 int main(void)
 {
     // Your code here
-
+    int x = 100; 
+    printf("The value of x in the parent process is %d and its pid is %d.\n", x, (int) getpid()); 
+    int rc = fork(); 
+    // Child process starts executing here
+    if (rc < 0) { // fork failed; exit
+        fprintf(stderr, "Fork Failed!\n"); 
+        exit(1); 
+    }
+    else if (rc == 0) { // Child process satisfies this branch 
+    printf("\nThe value of x in the child process before its changed is %d and its pid is %d.\n", x, (int) getpid()); 
+    x = 25; 
+    printf("The value of x in the child process after its changed is %d and its pid is %d.\n", x, (int) getpid()); 
+    }
+    else {
+        printf("The value of x after the child process executes is %d. The parent pid is %d and the child pid is %d.\n", x, (int) getpid(), rc); 
+    }
     return 0;
 }
