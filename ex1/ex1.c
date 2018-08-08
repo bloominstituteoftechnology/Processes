6b88c23d66446687ea094a40fa677757d4bc5cfb
mister-corn <code@ronaldlibago.com>
2018-08-08 15:34:27 -0700

    15:33 PDT 8-Aug: Exercises 1 & 2 have progress.
    * TODO: Answer questions in comments.

--- ex1/ex1.c
+++ ex1/ex1.c
@@ -2,6 +2,14 @@
 // (e.g., x) and set its value to something (e.g., 100). What value is the variable in the child process?
 // What happens to the variable when both the child and parent change the value of x?
 
+/*
+> What value is the variable in the child process?
+
+
+> What happens to the variable when both the child and parent change the value of x?
+
+
+*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -9,6 +17,20 @@
 int main(void)
 {
     // Your code here
-
+    int hot_var = 100;
+    int rc = fork();
+    if (rc < 0) {
+        fprintf(stderr, "fork failed\n");
+        exit(1);
+    }
+    else if (rc == 0) {
+        hot_var = 0;
+        printf("The value of hot_var is ICE COLD, BRUH! %i DEGREEEES\n", hot_var);
+    } 
+    else {
+        hot_var = 69;
+        printf("The value of hot_var is %i\n", hot_var);
+    }
+    
     return 0;
 }
