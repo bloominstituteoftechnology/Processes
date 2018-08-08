460d6df2043f877d1d8c64022ba7618e278c3500
AmyShackles <amyshackles@gmail.com>
2018-08-08 12:35:26 -0700

    Completed ex1
    - Program initiates a variable, x, with the value of 100
    - It then prints out the value of x
    - The fork call is then made, creating a child process
    - If the result of the fork call is less than 0, it means that the fork failed
    - In the event of fork failure, program prints ‘fork failed’ to stderr
    - In the event of fork failure, program also exits.
    - If the value of rc is 0, the process handling the program is the child process
    - Child process changes the value of x to 50
    - Child process then prints out its pid number and the value of x”

--- ex1/ex1.c
+++ ex1/ex1.c
@@ -8,7 +8,24 @@
 
 int main(void)
 {
-    // Your code here
+    int x = 100;
+    printf("What is the value of x? %d\n", x);
+    int rc = fork();
 
+    if (rc < 0)
+    {
+        fprintf(stderr, "Fork failed\n");
+        exit(1);
+    }
+    else if (rc == 0)
+    {
+        x = 50;
+        printf("This is the child (pid: %d) process - the value of x is %d\n", (int) getpid(), x);
+    }
+    else
+    {
+        x = 25;
+        printf("This is the parent (pid: %d) process of %d - the value of x is %d\n", (int) getpid(), rc, x);
+    }
     return 0;
 }
