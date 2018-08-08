8a8f88e7a976ede567ad69f3660f40d056ca4789
AmyShackles <amyshackles@gmail.com>
2018-08-08 15:51:46 -0700

    Completed ex4

--- ex4/ex4.c
+++ ex4/ex4.c
@@ -10,7 +10,26 @@
 
 int main(void)
 {
-    // Your code here    
-
+    char *env[] = { "HOME=/usr/home", "LOGNAME=home", (char *)0 };
+    int rc = fork();
+    if (rc < 0)
+    {
+        fprintf(stderr, "Fork failed.\n");
+        exit(1);
+    }
+    else if (rc == 0)
+    {
+        printf("Child process, pid(%d)\n", (int) getpid());
+        // execl("/bin/ls", "ls", "-l", 0);
+        
+        execle("/bin/ls", "ls", "-l", (char *)0, env);
+        printf("I've made a terrible mistake\n");
+    }
+    else
+    {
+        waitpid(rc, NULL, 0);
+        printf("Waiting for the child process\n");
+        
+    }
     return 0;
 }
