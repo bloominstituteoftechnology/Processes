b745ac65c54f17fea644259c17b9e4b6c4527046
Xang Thao <xangthao24@yahoo.com>
2018-08-08 14:07:06 -0700

    Completed ex1

--- ex1/ex1.c
+++ ex1/ex1.c
@@ -9,6 +9,20 @@
 int main(void)
 {
     // Your code here
+    int mainNum = 100;
+    int rc = fork();
+    if(rc < 0) {
+        perror("Error forking");
+        exit(-1);
+    }
+    if(rc == 0) {
+        mainNum += 10;
+        printf("Child added ten to the mainNum so now mainNum = %d \n", mainNum);
+    } else {
+        mainNum -= 5;
+        printf("Parent subtracted 5 from the mainNum so now mainNum = %d \n", mainNum);
+    }
+    printf("mainNum is = %d \n", mainNum);
 
     return 0;
 }
