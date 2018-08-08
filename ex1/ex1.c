61ca0b1af9a3b023a80d06b4ab2fa1ea130f09b2
samarv <samarvir1996@gmail.com>
2018-08-09 03:18:56 +0530

    completed

--- ex1/ex1.c
+++ ex1/ex1.c
@@ -9,6 +9,24 @@
 int main(void)
 {
     // Your code here
+    printf("hello world (pid: %d)\n", (int) getpid());
+    int x = 100;
+    int rc = fork();
+    // ------------------------------------------------ child process starts executing here
+    if (rc < 0) {    // fork failed; exit
+        fprintf(stderr, "fork failed\n");
+        exit(1);
+    } else if (rc == 0) {    // child process satisfies this branch
+        x = 50;
+        printf("x be like %d \n", (int) x);
+        printf("hello, child here (pid: %d) \n", (int) getpid());
+    } else {
+        printf("hello, parent here (pid: %d) of child %d\n", (int) getpid(), rc);
+    }
 
     return 0;
 }
+
+
+//when x is changed in parent - it gets fbombed? its stuck nonethe less! 
+//when x is changed in child, everything works as planned! 50 is printed out
