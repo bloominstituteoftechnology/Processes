7c31fb14125a63f98bcab474b26c6f886fb57eeb
Christina Kopecky <kopecky12112@gmail.com>
2018-08-08 11:55:36 -0700

    wrote a quick test to see if fopen fclose works

--- ex2/ex2.c
+++ ex2/ex2.c
@@ -3,12 +3,27 @@
 // returned by `fopen()`? What happens when they are written to the file concurrently?
 
 #include <stdio.h>
+#include <string.h>
 #include <unistd.h>
 #include <stdlib.h>
 
 int main(void)
 {
-    // Your code here 
+    FILE *fp;
+    char data[140];
+    printf("opening the file text.txt in read/write mode");
+    fp = fopen("text.txt", "r+");
+    if (fp == NULL){
+        printf("Could not open text.txt");
+        return 1;
+    }
     
+    printf("\n Enter some text to write into file text.txt:");
+    while(strlen (gets( data )) > 0){
+        fputs(data, fp);
+        fputs("\n", fp);
+    }
+    printf("Closing the file text.txt");
+    fclose(fp);
     return 0;
 }
