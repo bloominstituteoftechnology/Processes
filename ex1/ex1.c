14a6fb6302c7e6f4acb6b557a43c75ed66ee43e6
Iqra Javed <iqrajaved@Iqras-MacBook-Air.local>
2018-08-08 15:47:10 -0500

    ex1 complete

--- ex1/ex1.c
+++ ex1/ex1.c
@@ -1,6 +1,8 @@
 // Write a program that calls `fork()`. Before calling `fork()`, have the main process access a variable
 // (e.g., x) and set its value to something (e.g., 100). What value is the variable in the child process?
+    // --> the value of x is the same for the parent and the child
 // What happens to the variable when both the child and parent change the value of x?
+    // --> the value of x changes to the value that was assigned in each instance;
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -8,7 +10,21 @@
 
 int main(void)
 {
-    // Your code here
+    int x = 100;
+    printf("hello world (pid: %d, x: %d)\n", (int) getpid(), x);
 
+    int rc =fork();
+
+    if (rc < 0) {
+        printf(stderr, "fork failed\n");
+        exit(1);
+    } else if (rc == 0) {
+        x = 200;
+        printf("hello, child here (pid: %d, x: %d)\n", (int) getpid(), x);
+    } else {
+        x = 300;
+        printf("hello, parent here (pid: %d, x: %d) of child %d\n", (int) getpid(), x, rc);
+    }
+     
     return 0;
 }
