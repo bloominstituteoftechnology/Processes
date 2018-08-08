70daef3b478aceb8fea385ea3b2dd7be4d9ae5a2
Danny Vail <danny_vail@outlook.com>
2018-08-08 16:09:44 -0400

    Completed exercise 2 - Example 2 further illustrates fork behaviour. The main process and child processes both have access to the file pointer and can make changes to the same file.

--- ex2/ex2.c
+++ ex2/ex2.c
@@ -8,7 +8,27 @@
 
 int main(void)
 {
-    // Your code here 
-    
-    return 0;
+  FILE *fp = fopen("text.txt", "w+");
+  printf("Main process (pid: %d)\n", (int)getpid());
+  printf("Forking child process...\n");
+  int child = fork();
+
+  if (child < 0)
+  {
+    fprintf(stderr, "Fork failed\n");
+    exit(1);
+  }
+  else if (child == 0)
+  {
+    printf("Child process (pid: %d), writing to file fp...\n", (int)getpid());
+    fputs("This is a string written by the child process.\n", fp);
+  }
+  else
+  {
+    printf("Parent process (pid: %d) of child %d, writing to file fp...\n", (int)getpid(), child);
+    fputs("This is a string written by the parent process.\n", fp);
+  }
+
+  fclose(fp);
+  return 0;
 }
