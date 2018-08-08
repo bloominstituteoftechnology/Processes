415ce1fc2c633231eb9cb777af4df7ff24535aed
bbenefield89 <bsquared18@gmail.com>
2018-08-08 16:28:55 -0400

    WRITE TO PARENT FROM CHILD PROCESS
    - in ex5.c

--- ex5/ex5.c
+++ ex5/ex5.c
@@ -17,6 +17,43 @@ char* msg3 = "hello world #3";
 int main(void)
 {
     // Your code here
+    char inbuf[ MSGSIZE ];
+    int fds[2];
+
+    if (pipe(fds) < 0)
+    {
+        printf("Error establishing a pipe");
+        exit(1);
+    }
+
+    int child = fork();
+
+    if (child < 0)
+    {
+        printf("Error creating child process");
+        exit(1);
+    }
+    // when we `fork()` into our `child` process it creates it's own variation of the `pipe` READ=>WRITE system
+    // originally the parent holds the READ and the child is the WRITE
+    // in our `child` `fork()` the `child` is now the READ and the `parent` is now the WRITE
+    else if (child == 0)
+    {
+
+        write(fds[1], msg1, MSGSIZE);
+        write(fds[1], msg2, MSGSIZE);
+        write(fds[1], msg3, MSGSIZE);
+
+        for (int i = 0; i < 3; i++)
+        {
+            read(fds[0], inbuf, MSGSIZE);
+            printf("%s\n", inbuf);
+        }
+    }
+    else
+    {
+        waitpid(child, NULL, 0);
+        printf("Parent & Child complete\n");
+    }
     
     return 0;
 }
