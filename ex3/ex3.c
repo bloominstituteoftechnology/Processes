04baddb2029ee40eaa6fdde9fce2e240080dd5e3
astrebeck <alexstrebeck@gmail.com>
2018-08-08 15:58:46 -0600

    removed all of x variable

--- ex3/ex3.c
+++ ex3/ex3.c
@@ -18,7 +18,7 @@ int main(void)
     } else if (rc == 0) {    // child process satisfies this branch
         printf("\nhello, child here (pid: %d) \n", (int) getpid());
         printf("Child says: Hello\n");
-        printf("this is the value of x in the child scope after change: %d\n", x);
+
     } else {
         int wc = waitpid(rc, NULL, 0);    // `waitpid` call added here
         printf("hello, parent here (pid: %d) of child %d\n", (int) getpid(), rc);
