6ec754f753b8b65d986dfe7b151868c294eb5649
AmyShackles <amyshackles@gmail.com>
2018-08-08 13:11:03 -0700

    Completed ex2
    - Program tries to open text.txt
    - Program checks to see if the result from fopen is null and if it is, prints a failure message to stderr and exits
    - Program then prints a line of text to the now open file
    - There is a fork system call to create a child process
    - If the result from that system call is less than 0, prints a failure message to stderr and exits
    - If the value of the variable assigned to the fork system call is 0, that indicates it's the child process currently operating
    - Child process then prints its pid and where the pointer is for the file
    - Child process then writes a message to the file
    - In the parent process, the parent process prints its pid, the pid of the child process, and the pointer to the file
    - The parent process then writes a message to the open file.

--- ex2/ex2.c
+++ ex2/ex2.c
@@ -8,7 +8,33 @@
 
 int main(void)
 {
-    // Your code here 
+    FILE *file;
+
+    file = fopen("text.txt", "r+");
+
+    if (file == NULL)
+    {
+        fprintf(stderr, "File failed to open\n");
+        exit(1);
+    }
+    fprintf(file, "%s %s %s %s %s %s %s %s %s\n", "Parent", "gets", "first", "crack", "at", "writing", "to", "the", "file");
+
+    int rc = fork();
+    if (rc < 0)
+    {
+        fprintf(stderr, "Fork failed\n");
+        exit(1);
+    }
+    else if (rc == 0)
+    {
+        printf("Child process, pid# %d and I think the file is ... %p\n", (int) getpid(), file);
+        fprintf(file, "%s %s %s %s %s %s %s?\n", "Can", "a", "child", "write", "to", "this", "file");
+    }
+    else
+    {
+        printf("Parent (%d) process of (%d) and the file is ... %p\n", (int) getpid(), rc, file);
+        fprintf(file, "%s %s %s %s %s %s?\n", "What", "if", "the", "parent", "writes", "too");
+    }
     
     return 0;
 }
