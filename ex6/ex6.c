22b607bb5d77bf4cdf74a918c60a42c9731a4650
bbenefield89 <bsquared18@gmail.com>
2018-08-08 18:00:49 -0400

    RETURN AVG TIME OF STDOUT
    - in ex6.c
    returns average time it takes for one million stdout calls

--- ex6/ex6.c
+++ ex6/ex6.c
@@ -12,15 +12,40 @@ and `clock_gettime()` should work just fine.
 */
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <time.h>
 
 #define number_iter 1000000
-#define BILLION 1000000000L
+#define BILLION 1000000000L  // do not try an malloc this much space!
 
 int main()
 {
     // Your code here
+    long int *times_nanosec = malloc(sizeof(long int) * number_iter);
+    struct timespec start, end;
+
+    for (long int i = 0; i < number_iter; i++)
+    {
+        clock_gettime(CLOCK_REALTIME, &start);
+
+        printf("\nOutput\n: %ld", i);
+        
+        clock_gettime(CLOCK_REALTIME, &end);
+
+        *(times_nanosec + i ) = end.tv_nsec;
+    }
+
+    long int times_sum = 0;
+
+    for (long int i = 0; i < number_iter; i++)
+    {
+        times_sum = times_sum + *(times_nanosec + i );
+    }
+
+    printf("\n\n=====\nAverage Time in Nanoseconds: %ld\n=====\n\n", times_sum / number_iter);
+    
+    free(times_nanosec);
     
     return 0;
 }
