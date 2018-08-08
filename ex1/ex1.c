b68bf2f96797d0d1b91d15e7ea6258928e8f9094
David <AquilaVirtual>
2018-08-08 13:32:04 -0700

    ex1 complete

--- ex1/ex1.c
+++ ex1/ex1.c
@@ -10,5 +10,39 @@ int main(void)
 {
     // Your code here
 
+int x = 0;
+     x = 100;
+    printf("Variable x = %d\n",x);
+     int rc = fork();
+    // start of child process
+    if (rc < 0) {    // fork failed;
+        fprintf(stderr, "fork failed\n");
+        exit(1); //exit
+    } else if (rc == 0) {    // child process satisfies this branch
+        printf("Hello, child \n");
+        printf("Child x = %d \n",x);
+        printf("Child second timex = %d \n",x);
+        x = 10;
+        printf("Child x = %d \n",x);
+    } else {
+        printf("Hello, parent of child\n");
+        printf("Parent x = %d \n",x);
+        x = 50;
+        printf("Parent x = %d \n",x);
+    }
+    printf("Variable x = %d (pid: %d) \n",x, (int) getpid());
     return 0;
 }
+
+//EXPECTED OUTPUT
+
+// Variable x = 100
+// Hello, parent of child
+// Hello, child
+// Parent x = 100
+// Child x = 100
+// Parent x = 50
+// Child second timex = 100
+// Variable x = 50 (pid: 5130)
+// Child x = 10
+// Variable x = 10 (pid: 5131)
