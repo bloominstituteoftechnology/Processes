1450be1a4f21e4b7b3aa3c7d11012ebab8a38539
AmyShackles <amyshackles@gmail.com>
2018-08-08 13:27:34 -0700

    Completed ex3
    - Program makes a fork system call and assigns the return of that system call to variable rc
    - If rc is less than 0, the fork call has failed, so the program prints a failure message to stderr
    - If rc is equal to 0, it means that it is in the child process
    - The child process then prints a greeting
    - The parent process waits until the child process is finished
    - The parent process then prints a parting.

--- ex3/ex3.c
+++ ex3/ex3.c
@@ -9,7 +9,22 @@
 
 int main(void)
 {
-    // Your code here
+    int rc = fork();
+
+    if (rc < 0)
+    {
+        fprintf(stderr, "Fork failed\n");
+        exit(1);
+    }
+    else if (rc == 0)
+    {
+        printf("Hello!\n");
+    }
+    else
+    {
+        wait(NULL);
+        printf("Goodbye!\n");
+    }
 
     return 0;
 }
