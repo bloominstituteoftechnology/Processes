2533e9f371c38fbc40925256defa9fc75b5390e9
Christina Kopecky <kopecky12112@gmail.com>
2018-08-08 15:41:28 -0700

    pipe works

--- ex5/ex5.c
+++ ex5/ex5.c
@@ -16,7 +16,22 @@ char* msg3 = "hello world #3";
 
 int main(void)
 {
-    // Your code here
+    char buffer[MSGSIZE];
+    int fd[2];
+
+    if(pipe(fd) < 0) {
+        fprintf(stderr, "call the plumber - you have a broken pipe!\n");
+        exit(1);
+    }
+
+    write(fd[1], msg1, MSGSIZE);
+    write(fd[1], msg2, MSGSIZE);
+    write(fd[1], msg3, MSGSIZE);
+
+    for( int i = 0; i < 3; i++) {
+        read(fd[0], buffer, MSGSIZE);
+        printf("%s\n", buffer);
+    }
     
     return 0;
 }
