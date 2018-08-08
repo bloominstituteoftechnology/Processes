e4f90e0b7f67e241510178e882b4c691bfa17062
William VanDolah <william.vandolah@gmail.com>
2018-08-08 16:49:21 -0500

    ex3 done

--- ex3/ex3.c
+++ ex3/ex3.c
@@ -9,7 +9,23 @@
 
 int main(void)
 {
-    // Your code here
+    int x = 100;
+    printf("first(pid: %d)\n", (int) getpid());
+    int rc = fork();
+    // ------------------------------------------------ child process starts executing here
+    if (rc < 0) {    // fork failed; exit
+        fprintf(stderr, "fork failed\n");
+        exit(1);
+    } else if (rc == 0) {    // child process satisfies this branch
+        printf("second, hello, child here (pid: %d) value of x = %d \n", (int) getpid(), x);
+        x++;
+        printf("Third, hello, child here (pid: %d) value of x = %d \n", (int) getpid(), x);
+    } else {
+        int wc = waitpid(rc, NULL, 0);
+        printf("Forth, hello, parent here (pid: %d) of child %d, value of x = %d\n", (int) getpid(), rc, x);
+        x++;
+        printf("Fifth, hello, parent here (pid: %d) of child %d, value of x = %d\n", (int) getpid(), rc, x);
+    }
 
     return 0;
 }
