090c0e3497dd462f7bab91fe94d60113c56bdab5
kelly <kelfro@fastmail.com>
2018-08-08 13:26:56 -0700

    ex2 done

--- ex2/ex2.c
+++ ex2/ex2.c
@@ -9,6 +9,30 @@
 int main(void)
 {
     // Your code here 
-    
-    return 0;
+  FILE *fp;
+  fp = fopen("text.txt", "w");
+// this was really helpful with this: https://www.tutorialspoint.com/c_standard_library/c_function_fclose.htm
+ 
+  int rc = fork();
+  if (rc < 0)
+  {
+    fprintf(stderr, "fork failure alert\n");
+    exit(1);
+  }
+// I used this for the fwrite part: https://www.tutorialspoint.com/c_standard_library/c_function_fwrite.htm
+  else if (rc == 1)
+  {
+    char str1[] = "child";
+    fwrite(str1, 1, sizeof(str1), fp);
+  }
+  else
+  {
+    char str2[] = "parent";
+    fwrite(str2, 1, sizeof(str2), fp);
+  } 
+  fclose(fp);
+  
+  return 0;
 }
+
+// after running this nothing appeared to happen so I checked the text.txt file that I was writing to and it contained "parent@parent". I erased it and put my own text in there and then ran it again. It again contained "parent@parent".
