a18c41ba31bd2b079e4938cb98c6e5ed56b80fb7
jesuarva <jesuarva.developer@gmail.com>
2018-08-08 21:43:41 +0200

    EXC 1:
    *********  OUTPUT  *********
    === PARENT_1 === variable 'v' = 0
    === PARENT_2 === variable 'v' = -1
    === CHILD_1 === variable 'v' = 0
    === CHILD_2 === variable 'v' = 1
    
    I could infer that
    1. Child process inherit the current STATE of the Parent process.
    2. Due to the naturallity of processes (they are in independent 'scopes/execution context')
    variable manipulation do not extends to other processes.

--- ex1/ex1.c
+++ ex1/ex1.c
@@ -9,6 +9,37 @@
 int main(void)
 {
     // Your code here
+    int fork_process = fork();
+    int v = 0;
+
+    if (fork_process < 0)
+    {
+        fprintf(stderr, "Something went wronk forking %d process\n", (int)getpid());
+    }
+    else if (fork_process == 0)
+    {
+        printf("=== CHILD_1 === variable 'v' = %d\n", v);
+        v++;
+        printf("=== CHILD_2 === variable 'v' = %d\n", v);
+    }
+    else
+    {
+        printf("=== PARENT_1 === variable 'v' = %d\n", v);
+        v--;
+        printf("=== PARENT_2 === variable 'v' = %d\n", v);
+    }
 
     return 0;
 }
+
+// *********  OUTPUT  *********
+/*
+=== PARENT_1 === variable 'v' = 0
+=== PARENT_2 === variable 'v' = -1
+=== CHILD_1 === variable 'v' = 0
+=== CHILD_2 === variable 'v' = 1
+
+1. I could infer that Child process inherit the current STATE of the Parent process.
+2. And that due to the naturallity of processes (they are in independent 'scopes/execution context')
+variable manipulation do not extends to other processes.
+*/
