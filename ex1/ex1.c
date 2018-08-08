636b5e86e5b5615674f753b8b784917bf36f38d1
NandoTheessen <nando.theessen@gmail.com>
2018-08-08 23:10:43 +0200

    added solution to ex 1

--- ex1/ex1.c
+++ ex1/ex1.c
@@ -8,7 +8,20 @@
 
 int main(void)
 {
+    int variable = 0;
     // Your code here
+    printf("pid: %d\n", (int)getpid());
+    variable = 100;
 
+    int rc = fork();
+    if(rc < 0){
+        printf("Fork failed! U stoopid?!\n");
+    }else if (rc == 0){
+        variable = 25;
+        printf("hello, child here (pid: %d) \n", (int) getpid());
+        printf("variable in child process: %d\n", variable);
+    }else{
+        printf("hello, parent here (pid: %d) of child %d\n", (int) getpid(), rc);
+    }
     return 0;
 }
