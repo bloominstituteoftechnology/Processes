aed1e3f2a05d351783202284f8b7eec75c60ade4
Erinc Emer <erincemer@Erincs-MBP.attlocal.net>
2018-08-08 13:43:55 -0700

    ex1 done

--- ex1/ex1.c
+++ ex1/ex1.c
@@ -9,6 +9,17 @@
 int main(void)
 {
     // Your code here
+    int x = 15;
+	int rc = fork();
 
+	if (rc < 0) {
+        fprintf(stderr, "fork failed\n");
+    }  else if (rc == 0) {
+    		x = 25;
+        printf("Child is: x=%i\n", x);
+    } else { 
+    	x = 90;
+		printf("Parent is: x=%i\n", x);
+}
     return 0;
 }
