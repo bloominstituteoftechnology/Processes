2f98cb5897f5b95fb83ccefbd139d1f183d1f101
Steve <shalomlandsman@gmail.com>
2018-08-09 17:40:15 -0400

    time

--- ex6/ex6.c
+++ ex6/ex6.c
@@ -13,6 +13,8 @@ and `clock_gettime()` should work just fine.
 
 #include <stdio.h>
 #include <unistd.h>
+#include <stdint.h>
+#include <stdlib.h>
 #include <time.h>
 
 #define number_iter 1000000
@@ -21,6 +23,23 @@ and `clock_gettime()` should work just fine.
 int main()
 {
     // Your code here
-    
+    int arr[number_iter];
+    int counter = 0;
+    while (counter < number_iter) {
+        clock_t t;
+        t = clock();
+        printf("");
+        t = clock() - t;
+        double time_taken = ((double)t)/CLOCKS_PER_SEC;
+        double nanosecond = time_taken * BILLION;
+        arr[counter] = nanosecond;
+        counter++;
+    }
+    double sum = 0;
+    for (int i = 0; i < number_iter; i++) {
+        sum += arr[i];
+    }
+    double average = sum / number_iter;
+    printf("%f\n", average);
     return 0;
 }
