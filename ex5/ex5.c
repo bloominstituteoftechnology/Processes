d3101aa38f84af104b960db8f8d8374461a3f366
sagardesai90 <saagardesai@gmail.com>
2018-08-09 16:48:43 -0500

    ex5 ex6

--- ex5/ex5.c
+++ ex5/ex5.c
@@ -17,6 +17,31 @@ char* msg3 = "hello world #3";
 int main(void)
 {
     // Your code here
-    
+    char incomingDataBuffer[MSGSIZE];
+    int p[2];
+
+    if (pipe(p) < 0){
+        fprintf(stderr, "pipe failed");
+        exit(1);
+    } 
+
+    int forked = fork();
+
+    if (forked < 0){
+        fprintf(stderr, "fork failed");
+        exit(2);
+    } else if (forked == 0) {
+        write(p[1], msg1, MSGSIZE);
+        write(p[1], msg2, MSGSIZE);
+        write(p[1], msg3, MSGSIZE);
+    } else {
+        waitpid(forked, NULL, 0);
+        close(p[1]);
+
+        while(read(p[0], incomingDataBuffer, MSGSIZE > 0)){
+            printf("%s\n", incomingDataBuffer);
+        }
+    }
+
     return 0;
 }
