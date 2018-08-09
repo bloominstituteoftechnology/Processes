b3828bcec520a7214551b87e005b526b3bfb7a6c
AmyShackles <amyshackles@gmail.com>
2018-08-08 17:30:12 -0700

    Completed ex5
    - Program defines a constant size for messages to be 16 bytes
    - Program declares three char pointers that point to string arrays to be sent during execution of the program
    - Program creates a temporary storage location (buffer) for the messages to be transferred
    - Program then establishes a pipe, passing in the two-element array being used to hold read/write file descriptors
    - If the pipe returns less than one, it is a failure condition, so it prints to stderr and exits
    - If the pipe is successful, the program then performs a fork call
    - If the result of the fork call is less than 0, the program prints a failure message to stderr and exits
    - If the result of the fork call is 0, it means that it is currently in the child process
    - The program then prints a message with the current pid
    - The program follows that up by having the child process write to the parent process
    - Strike that.  The child process writes to the write file descriptor.
    - The program then prints a message with the current pid.
    - Meanwhile, the parent process has been held up by use of the waitpid command, where it was waiting until the child process finished executing before continuing.
    - Now that the parent process can do its thing, it reads from the read file descriptor up to the stated message size.
    - The program then prints the current pid followed by the messages that had been transferred along the pipe.

--- ex5/ex5.c
+++ ex5/ex5.c
@@ -8,15 +8,54 @@
 #include <stdlib.h>
 #include <sys/wait.h>
 
-#define MSGSIZE 16
+#define MSGSIZE 16 // defining a constant size for each message
 
-char* msg1 = "hello world #1";
+// char pointers to string arrays to be sent
+char* msg1 = "hello world #1"; 
 char* msg2 = "hello world #2";
 char* msg3 = "hello world #3";
 
 int main(void)
 {
-    // Your code here
+    char inbuf[MSGSIZE]; // buffer to hold incoming data being written
+    int p[2]; // two-element array to hold read/write file descriptors
+
+    printf("%d is the current process running\n", (int) getpid());
+// establish the pipe
+// print an error to stderr if failure
+    if (pipe(p) < 0)
+    {
+        fprintf(stderr, "Pipe failure!\n");
+        exit(1);
+    }
+    else
+    {
+        int rc = fork();
+
+    if (rc < 0)
+    {
+        fprintf(stderr, "Fork failed\n");
+        exit(1);
+    }
+    else if (rc == 0)
+    {
+        printf("%d is the process running before writing\n", (int) getpid());
+        write(p[1], msg1, MSGSIZE);
+        write(p[1], msg2, MSGSIZE);
+        write(p[1], msg3, MSGSIZE);
+        printf("%d is the process running after writing\n", (int) getpid());
+    }
+    else
+    {
+        waitpid(rc, NULL, 0);
+        for (int i = 0; i < 3; i++)
+        {
+            read(p[0], inbuf, MSGSIZE);
+            printf("%d prints out the message %s\n", (int) getpid(), inbuf);
+        }
+    }
+    }
+
     
     return 0;
 }
