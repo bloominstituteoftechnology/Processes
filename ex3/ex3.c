2ef58ae69adc98d7c1aacfc1ad5d7768b961bf0f
JSNavey <jsnavey@gmail.com>
2018-08-09 16:59:13 -0400

    complete ex3

--- ex3/ex3.c
+++ ex3/ex3.c
@@ -10,6 +10,27 @@
 int main(void)
 {
     // Your code here
+    printf("Initial Process (pid: %d)\n", (int) getpid());
+
+    int rc = fork();
+
+    if(rc < 0) {
+        fprintf(stderr, "Fork failed\n");
+        exit(1);
+    }
+    else if (rc == 0)
+    {
+        printf("Hello. Child Process (pid: %d)\n", (int) getpid());
+    }
+    else
+    {
+        // The wait() system call 
+        // (along with the more complete waitpid() system call) 
+        // allows us to get around this non-determinism if that is something 
+        // that needs to be accounted for. In this case, we want the child prints first.
+        int wc = waitpid(rc, NULL, 0);
+        printf("Goodbye. Parent Process (pid: %d)\n", (int) getpid());
+    }
 
     return 0;
 }
