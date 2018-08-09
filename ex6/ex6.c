0340ddfa40ff260941a68aaa6628b3484b9c1b22
derrick-mei <derrick.mei.168@gmail.com>
2018-08-09 14:55:44 -0500

    done: ex6.c

--- ex6/ex6.c
+++ ex6/ex6.c
@@ -4,9 +4,9 @@ Your program should use the `clock_gettime` procedure to time how long a single 
 It'll do this for one million iterations, and then find the average of all of those iterations in nanoseconds.
 For some helpful documentation and examples of using time-related system calls,
 visit this site: https://www.cs.rutgers.edu/~pxk/416/notes/c-tutorials/gettime.html
-While the linked site does say that `clock_gettime()` does not work on OSX, this 
-turns out to only be the case for OSX versions < 10.12. Anything later than that 
-and `clock_gettime()` should work just fine. 
+While the linked site does say that `clock_gettime()` does not work on OSX, 
+this turns out to only be the case for OSX versions < 10.12. 
+Anything later than that and `clock_gettime()` should work just fine. 
 */
 
 #include <stdio.h>
@@ -19,12 +19,30 @@ and `clock_gettime()` should work just fine.
 int main()
 {
     // Your code here
-    int rc = fork();
-
-    if (rc < 0)
-    {
-        fprintf(stderr, "fork failed\n");
-        exit(1);
+    struct timespec startTime, endTime;
+    long sum = 0;
+    double avg;
+    for (int i = 0; i < number_iter; i++){
+        clock_gettime(CLOCK_MONOTONIC, &startTime);
+        printf("");
+        //write(fileno(stdout), NULL, 0);
+        clock_gettime(CLOCK_MONOTONIC, &endTime);
+        long difference = BILLION * (endTime.tv_sec - startTime.tv_sec) + endTime.tv_nsec - startTime.tv_nsec;
+        sum += difference;
     }
+
+    avg = sum / (float) number_iter;
+
+    printf("Average time it takes to make a printf call is %f ns\n", avg);
+
+
+
+    // int rc = fork();
+
+    // if (rc < 0)
+    // {
+    //     fprintf(stderr, "fork failed\n");
+    //     exit(1);
+    // }
     return 0;
 }
