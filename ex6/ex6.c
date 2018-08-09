ce4f2a0e185a6c1706ca228dd3cde51931022c0d
Bee C <b.choeung@gmail.com>
2018-08-09 19:22:43 -0400

    working on ex6, copy code from example

--- ex6/ex6.c
+++ ex6/ex6.c
@@ -18,37 +18,29 @@ and `clock_gettime()` should work just fine.
 #define number_iter 1000000
 #define BILLION 1000000000L
 
-int main()
+
+main(int argc, char **argv)
 {
-    int n = 1;
-    FILE *fp;
-    unint64_t diff;
-    // long long unsigned int diff;
-    struct timespec start, end;
-    int i;
-    clock_gettime(CLOCK_MONOTONIC, &start);
-    while (n < number_iter)
-    {
-        char *childStr = "";
-        fwrite(childStr, 1, strlen(childStr), fp);
-        n++;
-    }
-    clock_gettime(CLOCK_MONOTONIC, &end);
+	uint64_t diff;
+	struct timespec start, end;
+	int i;
 
-    diff = (BILLION * (end.tv_sec - start.tv_sec) + end.tv_nsec - start.tv_nsec) / number_iter;
-    printf("elapsed time = %llu nanoseconds \n", (long long unsigned int)diff);
+	/* measure monotonic time */
+	clock_gettime(CLOCK_MONOTONIC, &start);	/* mark start time */
+	sleep(1);	/* do stuff */
+	clock_gettime(CLOCK_MONOTONIC, &end);	/* mark the end time */
 
-    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &start);
-    while (n < number_iter)
-    {
-        char *childStr = "";
-        fwrite(childStr, 1, strlen(childStr), fp);
-        n++;
-    }
-    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &end);
+	diff = BILLION * (end.tv_sec - start.tv_sec) + end.tv_nsec - start.tv_nsec;
+	printf("elapsed time = %llu nanoseconds\n", (long long unsigned int) diff);
 
-    diff = (BILLION * (end.tv_sec - start.tv_sec) + end.tv_nsec - start.tv_nsec) / number_iter;
-    printf("elapsed process CPU time = %llu nanoseconds\n", (long long unsigned int)diff);
+	/* now re-do this and measure CPU time */
+	/* the time spent sleeping will not count (but there is a bit of overhead */
+	clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &start);	/* mark start time */
+	sleep(1);	/* do stuff */
+	clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &end);		/* mark the end time */
 
-    return 0;
+	diff = BILLION * (end.tv_sec - start.tv_sec) + end.tv_nsec - start.tv_nsec;
+	printf("elapsed process CPU time = %llu nanoseconds\n", (long long unsigned int) diff);
+
+	exit(0);
 }
