b987de9ded14262f2e9b118deb402fa80f5cf38a
derrick-mei <derrick.mei.168@gmail.com>
2018-08-09 02:56:34 -0500

    comments for ex2.c

--- ex2/ex2.c
+++ ex2/ex2.c
@@ -1,6 +1,7 @@
 // Write a program that opens the text.txt  file (with the `fopen()` system call) located in this directory
-// and then calls `fork()` to create a new process. Can both the child and parent access the file descriptor
-// returned by `fopen()`? What happens when they are written to the file concurrently?
+// and then calls `fork()` to create a new process. 
+//Can both the child and parent access the file descriptor returned by `fopen()`?  -> yes
+//What happens when they are written to the file concurrently? -> the ordering of the children and parent can be switched over time
 
 #include <stdio.h>
 #include <unistd.h>
@@ -21,13 +22,13 @@ int main(void)
     else if (rc == 0)
     {
         printf("child process here\n");
-        char *child_str = "This is child string!\n"; 
+        char *child_str = "This is child string2!\n"; 
         fwrite(child_str, 1, strlen(child_str), fp);
     }
     else
     {
         printf("parent process here\n");
-        char *parent_str = "This is parent string\n";
+        char *parent_str = "This is parent string2\n";
         fwrite(parent_str, 1, strlen(parent_str), fp);
     }
 
