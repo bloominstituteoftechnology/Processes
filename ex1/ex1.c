bea98d9b425f7812f79a06beb929cc473ed9644c
rayheberer <ray.heberer@gmail.com>
2018-08-09 11:37:05 -0700

    Complete ex1

--- ex1/ex1.c
+++ ex1/ex1.c
@@ -8,7 +8,24 @@
 
 int main(void)
 {
-    // Your code here
+    // initialize variable
+    int x = 100;
 
+    int rc = fork();
+    if (rc < 0) {    // fork failed; exit
+        fprintf(stderr, "fork failed\n");
+        exit(1);
+    } else if (rc == 0) {    // child process
+        printf("child process here, variable value: %d\n", x);
+        x = 50;
+    } else {
+        printf("parent process here, variable value: %d\n", x);
+        x = 200;
+    }
+    
+    printf("final variable value: %d\n", x);
     return 0;
 }
+
+/* The variable value is 100 in the child process. 
+If the variable value is changed in the parent process, this does not affect its value in the child process and vice versa.
