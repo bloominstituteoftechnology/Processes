52e11c601b5d107ea2fa4e2a321b2d25cac4cc2d
Bee C <b.choeung@gmail.com>
2018-08-09 19:16:06 -0400

    corrected ex5

--- ex5/ex5.c
+++ ex5/ex5.c
@@ -18,13 +18,14 @@ int main(void)
 {
     char inbuf[MSGSIZE];    // a buffer that will hold the incoming data that is being written
     int p[2];               // a two-element array to hold the read and write file descriptors that are used by the pipe 
-    int rc = fork();  
+    
 
     // establish our pipe, passing it the p array so that it gets populated by the read and write file descriptors
     if (pipe(p) < 0) {
         fprintf(stderr, "pipe failed\n");
         exit(1);
     }
+    int rc = fork();  
     if (rc == 0) 
     {
             // write 16 bytes of data to the write file descriptor
@@ -39,7 +40,7 @@ int main(void)
         read(p[0], inbuf, MSGSIZE);
         printf("% s\n", inbuf);
         }
-    }t
+    }
     return 0;
 }
 
