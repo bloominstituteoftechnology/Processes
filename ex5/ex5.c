3d0284564c625856b7d6e5154feda416ac1bd74c
Zach Campbell <beazlebubbles@gmail.com>
2018-08-09 12:55:37 -0600

    Completed ex5.c

--- ex5/ex5.c
+++ ex5/ex5.c
@@ -1,7 +1,7 @@
 // Write a program that forks a child and creates a shared pipe
-// between the parent and child processes. Have the child write 
-// the three messages to the parent and have the parent print out 
-// the messages. 
+// between the parent and child processes. Have the child write
+// the three messages to the parent and have the parent print out
+// the messages.
 
 #include <stdio.h>
 #include <unistd.h>
@@ -16,7 +16,25 @@ char* msg3 = "hello world #3";
 
 int main(void)
 {
-    // Your code here
-    
+    char inbuf[MSGSIZE];
+    int p[2];
+    int forked = fork();
+
+    if (forked < 0) {    // fork failed; exit
+        fprintf(stderr, "fork failed\n");
+        exit(1);
+    }
+    else if (forked == 0) { //this is the child process
+        write(p[1], msg1, MSGSIZE);
+        write(p[1], msg2, MSGSIZE);
+        write(p[1], msg3, MSGSIZE);
+    }
+    else {
+        int wc = waitpid(forked, NULL, 0);
+        for (int i = 0; i < 3; i++) {
+            read(p[0], inbuf, MSGSIZE);
+            printf("% s\n", inbuf);
+        }
+    }
     return 0;
 }
