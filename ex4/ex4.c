66ee10aee562bd35713e865dda7964111147112d
William VanDolah <william.vandolah@gmail.com>
2018-08-09 10:02:55 -0500

    ex4 done

--- ex4/ex4.c
+++ ex4/ex4.c
@@ -7,10 +7,31 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <sys/wait.h>
+#include <string.h>
 
 int main(void)
 {
-    // Your code here    
+    printf("hello world (pid: %d)\n", (int) getpid());
+    int rc = fork();
+    // ------------------------------------------------ child process starts executing here
+    if (rc < 0) {    // fork failed; exit
+        fprintf(stderr, "fork failed\n");
+        exit(1);
+    } else if (rc == 0) {    // child process satisfies this branch
+        printf("hello, child here (pid: %d) \n", (int) getpid());
+        char *myargs[3];    // allocate an array of chars to hold 3 bytes
+        // `strdup` duplicates the given input string 
+        myargs[0] = strdup("ls");      // pass the name of the program we want to run as the first array element 
+        myargs[1] = strdup("-ls");     // 
+        myargs[2] = NULL;              // marks the end of the array
+        // execvp(myargs[0], myargs);     // runs the word count program, passing in the `myargs` array to the word count program as arguments
+        execv("/bin/ls", myargs);      // execv needs a path, followed by an array
+        printf("this should not be seen");
+    } else {
+        // int wc = waitpid(rc, NULL, 0);    // `waitpid` call added here
+        printf("hello, parent here (pid: %d) of child %d\n", (int) getpid(), rc);
+    }
+   
 
     return 0;
 }
