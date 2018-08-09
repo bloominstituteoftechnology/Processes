eaa7d32fce2d2d29c3db350d3fb03f5c8401ed00
honda0306 <christopher.honda92@gmail.com>
2018-08-09 11:25:52 -0700

    Coded through ex5, cannot compile due to a few errors, will debug in a bit

--- ex5/ex5.c
+++ ex5/ex5.c
@@ -17,6 +17,34 @@ char* msg3 = "hello world #3";
 int main(void)
 {
     // Your code here
+    char inbuf[MSGSIZE];
+    int i[2];
+
+    printf("%d is currently running\n", (int) getpid());
+
+    if (pipe(i) < 0) {
+        fprintf(stderr, "Failed pipe\n");
+        exit(1);
+    } else {
+        int rc = fork();
     
+
+    if (rc < 0) {
+        fprintf(stderr, "Failed fork\n");
+        exit(1);
+    } else if (rc == 0) {
+        printf("%d will run before writing\n", (int) getpid());
+        write(i[1], msg1, MSGSIZE);
+        write(i[1], msg2, MSGSIZE);
+        write(i[1], msg3, MSGSIZE);
+        printf("%d will run after writing\n", (int) getpid());
+    } else {
+        waitpid(rc, NULL, 0);
+        for (int i = 0; i < 3; i++) {
+            read(i[0], inbuf, MSGSIZE);
+            printf("%d returns %s\n", (int), getpid(), inbuf);
+        }
+    }
+    }
     return 0;
 }
