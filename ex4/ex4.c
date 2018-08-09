b553d06601362630eb135ed74f2d21178a660bd1
eddygonzalez9708 <edward.gonzalez9708@gmail.com>
2018-08-09 17:35:28 -0400

    Completed exercise four.

--- ex4/ex4.c
+++ ex4/ex4.c
@@ -7,10 +7,32 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <sys/wait.h>
+#include <string.h>
 
 int main(void)
 {
     // Your code here    
+    printf("Hello world! I am the parent process. I will go ahead and create a child process."); 
+    int rc = fork(); 
+    if (rc < 0) {
+        fprintf(stderr, "Fork Failed"); 
+        exit(1); 
+    }
+    else if (rc == 0) {
+        printf("\nHello world! I am the child process. I will go ahead and execute a different program."); 
+        char *myargs[3]; // allocate an array of chars to hold 3 bytes 
+        // strdup duplicates the given input string 
+        myargs[0] = strdup("ls"); // pass the name of the program we want to run as the first array element
+        myargs[1] = strdup("-l"); // pass -l argument to display a long listing format for the program 
+        myargs[2] = strdup("ex4.c"); // argument: the file to list
+        myargs[3] = NULL; // marks the end of the array 
+        execvp(myargs[0], myargs); // runs the list directory content program, passing in the myargs array to the word count program as arguments
+        printf("\nThis should not be seen."); 
+    }
+    else {
+        waitpid(rc, NULL, 0); // waitpid call added here
+        printf("\nHello world! I am the parent process. My child process and the program it executed have completed."); 
+    }
 
     return 0;
 }
