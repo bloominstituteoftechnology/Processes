e6d3f889fead6ab8f5d126f5de9169eb569c4267
Erinc Emer <erincemer@Erincs-MBP.attlocal.net>
2018-08-09 17:57:43 -0700

    ex6 done

--- ex4/ex4.c
+++ ex4/ex4.c
@@ -11,6 +11,24 @@
 int main(void)
 {
     // Your code here    
+    printf("Starting program.\n");
 
+    int rc = fork();
+    if(rc<0) {
+        fprintf(stderr, "fork failed\n");
+        exit(1);
+    } else if (rc == 0) {  
+        printf("child started\n");
+        // call exec
+        char *myargs[2];
+        myargs[0] = strdup("/bin/ls");
+        //myargs[1] = strdup("ex4.c");
+        myargs[1] = NULL;
+        execv(myargs[0], myargs);
+        printf("this should not be seen");
+    } else {  
+        int wc = waitpid(rc, NULL, 0);
+        printf("parent finished.\n");
+    }
     return 0;
 }
