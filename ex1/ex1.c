97505882208f5ad13a53c78af54ce9995d177f62
maribelcuales <maribelcuales@gmail.com>
2018-08-16 07:21:05 +0800

    added codes for fork() in ex1

--- ex1/ex1.c
+++ ex1/ex1.c
@@ -2,13 +2,26 @@
 // (e.g., x) and set its value to something (e.g., 100). What value is the variable in the child process?
 // What happens to the variable when both the child and parent change the value of x?
 
-#include <stdio.h>
+
+#include <stdio.h>  //installing the necessary libraries
 #include <stdlib.h>
 #include <unistd.h>
 
-int main(void)
+int main(int argc, char *argv[])
 {
-    // Your code here
+    printf("hello world (pid: %d)\n", (int) getpid()); // run hello world and pid
+    int rc = fork();    // fork funnction
+    
+    if (rc < 0) {   // handling error during fork()
+        fprintf(stderr, "fork failed\n");   // error message
+        exit(1);    // exit
+    } else if (rc == 0) {   // if child is == 0, run this
+        printf("hello, child here(pid: %d) \n", (int)
+        getpid());
+    } else {    // run this for parent
+        printf("hello, parent here (pid: %d) of child %d\n",
+        (int) getpid(), rc);
+    }
 
     return 0;
 }
