f51cbaa296bc93055b83b1483e5c44730e5c9a37
april <martinezapril56@yahoo.com>
2018-08-15 18:59:25 -0400

    start

--- ex1/ex1.c
+++ ex1/ex1.c
@@ -9,6 +9,20 @@
 int main(void)
 {
     // Your code here
+ int x = 100;
+ printf("hello world(pid: %d)\n", (int)getpid());
+
+ int rc = fork();
+
+ if (rc < 0) {
+     fprintf(stderr, "fork failed\n");
+      exit(1);
+  
+      }else if (rc == 0 ) {
+
+          printf("hello, I am child (pid: %d) and x  is: %d\n", (int) getpid(), x);
+      }
+ }   
 
     return 0;
 }
