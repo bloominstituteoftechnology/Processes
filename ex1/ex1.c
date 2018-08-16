2d335f3e99b0eed9b4991be0b4bff45a9d07c8fa
Jacob Henry <jacobiswinning@gmail.com>
2018-08-16 13:35:47 -0600

    added 2 thru 5 w/ comments

--- ex1/ex1.c
+++ ex1/ex1.c
@@ -10,26 +10,26 @@ int main(void)
 {
     // Your code here
     int x = 100;
-    printf("hello world (pid: %d)\n", (int)getpid());
+    printf("hello world (pid: %d)\n", (int)getpid()); // prints the pid along with hello world
 
-    int rc = fork();
+    int rc = fork(); // sets rc as fork
 
-    if (rc < 0)
+    if (rc < 0) // if there is no fork
     {
-        fprintf(stderr, "fork failed\n");
+        fprintf(stderr, "fork failed\n"); // prints fork failed error
         exit(1);
     }
-    else if (rc == 0)
+    else if (rc == 0) // if child is true
     {
-        printf("hello, I am child (pid: %d) and x is: %d\n", (int)getpid(), x);
-        x++;
-        printf("child again, x is now: %d\n", x);
+        printf("hello, I am child (pid: %d) and x is: %d\n", (int)getpid(), x); // prints pid of child
+        x++;                                                                    // counts x up one
+        printf("child again, x is now: %d\n", x);                               // prints the x count
     }
     else
     {
-        printf("hello, I am parent of %d (pid: %d) and x is: %d\n", rc, (int)getpid(), x);
-        x--;
-        printf("parent again, x is now: %d\n", x);
+        printf("hello, I am parent of %d (pid: %d) and x is: %d\n", rc, (int)getpid(), x); // prints parent pid
+        x--;                                                                               // counts x down
+        printf("parent again, x is now: %d\n", x);                                         // prints what x is currently
     }
-    return 0;
+    return 0; // because int main and not void main returns 0
 }
