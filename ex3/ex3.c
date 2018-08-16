8bd84489eb0c669aceac8cbe01373b4a1e4f525a
maribelcuales <maribelcuales@gmail.com>
2018-08-16 20:52:56 +0800

    passed codes for ex3.c

--- ex3/ex3.c
+++ ex3/ex3.c
@@ -2,14 +2,25 @@
 // the parent process should print "goodbye". You should ensure that the child
 // process always prints first.
 
-#include <stdio.h>
+#include <stdio.h>  // installing the necessary header files
 #include <stdlib.h>
 #include <unistd.h>
-#include <sys/wait.h>
+#include <sys/wait.h>   // for calling the wait system call
 
 int main(void)
 {
     // Your code here
+    int rc = fork();    // fork function: creates a new process by duplicating the calling process. Child process is created.
 
+    if (rc < 0) {   // handling error during fork()
+        fprintf(stderr, "fork failed\n");   // error message; fprintf printing to standard error
+        exit(1);    // exit 
+    } else if (rc == 0) {   // on successful forking, run this child process 
+        printf("Hello!\n"); // child prints Hello!
+    } else {
+        int wc = waitpid(rc, NULL, 0);  // call wait() function on parent to ensure that child process will always run first
+        printf("Goodbye!\n");   // parent prints Goodbye!
+    }
+    
     return 0;
 }
