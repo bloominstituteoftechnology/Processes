886b48f869c7b877f2c4d2d787da4640fb649980
april <martinezapril56@yahoo.com>
2018-08-16 18:14:09 -0400

    addingcomments

--- ex1/ex1.c
+++ ex1/ex1.c
@@ -9,15 +9,18 @@
 int main(void)
 {
     // Your code here
- int x = 100;
+ int x = 100;//Program initiates a variable, x, with the value of 100
  printf("hello world(pid: %d)\n", (int)getpid());//pid is a process identifer
 
  int rc = fork();//fork is a copy of the original
+//The fork call is then made, creating a child process
 
  if (rc < 0) {
      fprintf(stderr, "fork failed\n");
       exit(1);
-  
+  //error message 
+  //If the result of the fork call is less than 0, it means that the fork failed
+//In the event of fork failure, program prints ‘fork failed’ to stderr
       }else if (rc == 0 ) {
 
           printf("hello, I am child (pid: %d) and x  is: %d\n", (int) getpid(), x);
@@ -28,6 +31,9 @@ int main(void)
           x--;
           printf("parent again, x is now: %d\n", x);
       }
+      //If the value of rc is 0, the process handling the program is the child process
+//Child process changes the value of x to 50
+//Child process then prints out its pid number and the value of x”
       return 0;
     
 }
