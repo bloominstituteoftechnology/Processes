56cc6d06d44a6fc37e4efa4f5fcb6cc852a8a5c9
unknown <jesse.reichel@live.com>
2018-10-01 21:28:09 -0700

    fork done

--- ex1/ex1.c
+++ ex1/ex1.c
@@ -8,8 +8,27 @@
 
 int main(void)
 {
-    char string = "This is just a test";
-    printf("%c\n", string);
+    char string[30] = "This is just a test";
+    int chameleon = 79;
+    printf("%d\n", chameleon);
+    printf("%s\n", string);
+    int rc = fork();
+    if (rc < 0) {
+        fprintf(stderr, "fork failed\n");
+        exit(1);
+    }
+    else if (rc == 0) {
+        int chameleon = 168;
+        printf("%d\n", chameleon);
+        // string = "This is not cool";
+        // printf("I am a child of %s, my id is %d \n", string, (int) getpid());
+    } else {
+        int chameleon = 3;
+        printf("%d\n", chameleon);
+        // string = "I really am your...";
+        // printf("%s\n", string);
+        // printf("Luke... I am... %d\n", (int) getpid());
+    }
     // Your code here
 
     return 0;
