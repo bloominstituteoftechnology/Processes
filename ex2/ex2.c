85aee5e56f6f12a3f48515b0440319a5ee32fe0e
Alec <afroenygam@gmail.com>
2018-10-03 16:19:00 -0400

    ex2.c updated

--- ex2/ex2.c
+++ ex2/ex2.c
@@ -5,10 +5,31 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(void)
 {
-    // Your code here 
-    
+    // Your code here
+    FILE *fp;
+    fp = fopen("text.txt", "w");
+    int rc = fork();
+    if (rc < 0)
+    {
+        fprintf(stderr, "fork failed\n");
+        exit(1);
+    }
+    else if (rc == 0)
+    {
+        printf("Here is child\n");
+        char *child_str = "This is child string!\n";
+        fwrite(child_str, sizeof(char), strlen(child_str), fp);
+    } else 
+    {
+        printf("Here is parent\n");
+        char *parent_str = "This is the parent string!\n";
+        fwrite(parent_str, sizeof(char), strlen(parent_str), fp);
+    }
+    fclose(fp);
+    printf("Something here?\n");
     return 0;
 }
