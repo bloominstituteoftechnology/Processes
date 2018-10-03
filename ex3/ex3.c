85337f1334fe9e2b2de2724e8292ca3af607ade0
Erinc Emer <erincemer@Erincs-MBP.attlocal.net>
2018-10-03 13:57:06 -0700

    ex1,2,3 done

--- ex3/ex3.c
+++ ex3/ex3.c
@@ -9,7 +9,22 @@
 
 int main(void)
 {
-    // Your code here
+    int rc = fork();
+
+    if (rc < 0)
+    {
+        fprintf(stderr, "fork failed\n");
+        exit(1);
+    }
+    else if (rc == 0)
+    {
+        printf("Hello folks\n");
+    }
+    else
+    {
+        int wc = waitpid(rc, NULL, 0);
+        printf("Goodbye peeps\n");
+    }
 
     return 0;
 }
