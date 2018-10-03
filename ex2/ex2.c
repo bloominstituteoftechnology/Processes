5d236ea222a401b06b39eeb0c129e7c537fdc6f6
Jasper <joshtietz11@gmail.com>
2018-10-03 16:31:19 -0500

    Joshua Tietz -- processes day 1

--- ex2/ex2.c
+++ ex2/ex2.c
@@ -8,7 +8,24 @@
 
 int main(void)
 {
-    // Your code here 
-    
+    FILE* fp;
+    fp = fopen("text.txt", "w"); // Open text.txt, 'w' sets text.txt to an empty file we can write in
+
+    int rc = fork();
+    if (rc < 0) {
+        fprintf(stderr, "fork failed\n");
+        exit(1);
+    } else if (rc == 0) {
+        printf("child process here\n");
+        char *child_str = "This is child string!\n";
+        fwrite(child_str,  // child_str is a pointer to the array of elements to be written
+        sizeof(char),     // sizeof(char) is the size in bytes of each element to be written
+        strlen(child_str), // strlen(child_str) is the number of elements, each one with a size of size bytes from sizeof(char)
+        fp);                //fp is the pointer to a file object that we are executing/writing in, in this case it is fp: aka text.txt
+    } else {
+        printf("parent process here\n");
+        char *parent_str = "This is parent string!\n";
+        fwrite(parent_str, sizeof(char), strlen(parent_str), fp);
+    }
+    fclose(fp);
     return 0;
-}
