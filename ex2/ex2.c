ed75eef5208190b5b50d35a159bcd199a2d2dd0b
Katie Gorbell <katiegorbell@gmail.com>
2018-10-03 18:25:12 -0500

    ex2 completed

--- ex2/ex2.c
+++ ex2/ex2.c
@@ -8,7 +8,24 @@
 
 int main(void)
 {
-    // Your code here 
-    
+    FILE *fp;
+    fp = fopen("./text.txt", "wb");
+    printf("The file has been opened...\n");
+
+    int rc = fork();
+    if (rc < 0) {
+        fprintf(stderr, "fork failed\n");
+        exit(1);
+    } else if (rc == 0) {
+        printf("Child: pid %d\n", getpid());
+        char str[] = "The child hays hi.\n";
+        fwrite(str, 4, sizeof(str), fp);
+    } else {
+        printf("Parent: pid %d\n", getpid());
+        char str[] = "The parent hays hi.\n";
+        fwrite(str, 4, sizeof(str), fp);
+    }
+
+    fclose(fp);
     return 0;
 }
