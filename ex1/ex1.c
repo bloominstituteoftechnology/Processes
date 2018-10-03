adb05a83a93cd5cfcc1a41efc188a2825776c4b3
cmok4290 <cmok4290@gmail.com>
2018-10-03 15:32:48 -0400

    edited ex1.c

--- ex1/ex1.c
+++ ex1/ex1.c
@@ -1,6 +1,6 @@
 // Write a program that calls `fork()`. Before calling `fork()`, have the main process access a variable
-// (e.g., x) and set its value to something (e.g., 100). What value is the variable in the child process?
-// What happens to the variable when both the child and parent change the value of x?
+// (e.g., x) and set its value to something (e.g., 100). What value is the variable in the child process? The initial value when the variable was declared.
+// What happens to the variable when both the child and parent change the value of x? The variable holds different values in separate processes.
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -8,7 +8,28 @@
 
 int main(void)
 {
-    // Your code here
+    printf("Parent, pid: %d\n", getpid());
+    int rv = fork();
+    int val;
 
+    if(rv < 0)
+    {
+        printf(stderr, "fork failed\n");
+        exit(1);
+    }
+    else if(rv == 0)
+    {
+        printf("Child, pid: %d, val: %d\n", getpid(), val);
+        val = 50;
+        printf("Child, pid: %d, val: %d\n", getpid(), val);
+    }
+    else
+    {
+        int wc = waitpid(rv, NULL, 0);
+        printf("Parent, pid: %d, val: %d\n", getpid(), val);
+        val = 100;
+        printf("Parent, pid: %d, val: %d\n", getpid(), val);
+    }
+    
     return 0;
 }
