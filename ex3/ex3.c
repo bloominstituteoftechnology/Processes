778ab62e7afbd096ea54e016b865bff84bb463d6
snowcoding <vimalshah77@gmail.com>
2018-10-03 14:28:35 -0700

    Added wait() to allow child to finish first before parent

--- ex3/ex3.c
+++ ex3/ex3.c
@@ -9,7 +9,24 @@
 
 int main(void)
 {
-    // Your code here
+    printf("Before forking pid: %d\n", (int) getpid());
+
+    pid_t pid = fork();
+
+    if (pid < 0) //fork failed
+    {
+        exit(1);
+    } 
+    else if (pid == 0) //child process
+    {
+        printf("AFTER forking CHILD says Hello pid: %d\n", (int) getpid());
+
+    }
+    else {
+        //wait for child to complete
+        waitpid(pid, NULL, 0); 
+        printf("AFTER forking PARENT says Goodbye pid: %d\n", (int) getpid());
+    }
 
     return 0;
 }
