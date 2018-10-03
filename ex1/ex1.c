380ea1576d743f6aafef82d7ff3aa7cb575d3520
Austin Merando <ajmerando1@yahoo.com>
2018-10-03 15:48:17 -0700

    ex3 done

--- ex1/ex1.c
+++ ex1/ex1.c
@@ -1,6 +1,7 @@
 // Write a program that calls `fork()`. Before calling `fork()`, have the main process access a variable
 // (e.g., x) and set its value to something (e.g., 100). What value is the variable in the child process?
 // What happens to the variable when both the child and parent change the value of x?
+//Answer: It seems like the variable will always end up as the parents value at the end of the program
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -8,7 +9,26 @@
 
 int main(void)
 {
-    // Your code here
+	printf("I'm the parent process, and my pid is %d!\n", getpid());
 
-    return 0;
+	int x = 100;
+	pid_t pid = fork();
+
+	if (pid == 0) {
+		printf("I'm the child process, and my pid is %d!\n", getpid());
+		x += 1;
+		printf("x:%d for child\n", x);
+		exit(0);
+	}
+	else {
+		x += 10;
+		printf("x:%d for parent\n", x);
+	}
+	printf("I'm the parent again, and I just spawned child %d!\n", pid);
+
+	printf("x:%d out of if\n", x);
+
+	printf("WAT: pid: %d\n", getpid());
+
+	return 0;
 }
