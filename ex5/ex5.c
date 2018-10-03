66e739c1219ee70417b907eeddf52d5c41cd55ca
nchen0 <nicky.chen0@gmail.com>
2018-10-03 16:00:36 -0700

    ex5

--- ex5/ex5.c
+++ ex5/ex5.c
@@ -1,7 +1,7 @@
 // Write a program that forks a child and creates a shared pipe
-// between the parent and child processes. Have the child write 
-// the three messages to the parent and have the parent print out 
-// the messages. 
+// between the parent and child processes. Have the child write
+// the three messages to the parent and have the parent print out
+// the messages.
 
 #include <stdio.h>
 #include <unistd.h>
@@ -10,13 +10,37 @@
 
 #define MSGSIZE 16
 
-char* msg1 = "hello world #1";
-char* msg2 = "hello world #2";
-char* msg3 = "hello world #3";
+char *msg1 = "hello child #1\n";
+char *msg2 = "hello child #2\n";
+char *msg3 = "hello child #3\n";
 
 int main(void)
 {
+    char buffer[MSGSIZE]; // A buffer for incoming data
+    int p[2];             // array
+    if (pipe(p) < 0)
+    {
+        fprintf(stderr, "pipe failed\n");
+        exit(1);
+    }
     // Your code here
-    
+    int rc = fork();
+    if (rc == 0)
+    {
+        write(p[1], msg1, MSGSIZE);
+        write(p[1], msg2, MSGSIZE);
+        write(p[1], msg3, MSGSIZE);
+        printf("Child process checking in.\n");
+    }
+    if (rc > 0)
+    {
+        // Parent process
+        for (int i = 0; i < 3; i++)
+        {
+            read(p[0], buffer, MSGSIZE);
+            printf("% s\n", buffer);
+        }
+    }
+
     return 0;
 }
--- example.c
+++ example.c
@@ -2,23 +2,34 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-int main(int argc, char *argv[])
+#define MSGSIZE 16 // define a constant message size for each message
+
+char *msg1 = "hello world #1";
+char *msg2 = "hello world #2";
+char *msg3 = "hello world #3";
+
+int main()
 {
-    printf("hello world (pid: %d)\n", (int)getpid());
-    int rc = fork();
-    // ------------------------------------------------ child process starts executing here
-    if (rc < 0)
-    { // fork failed; exit
-        fprintf(stderr, "fork failed\n");
+    char inbuf[MSGSIZE]; // a buffer that will hold the incoming data that is being written
+    int p[2];            // a two-element array to hold the read and write file descriptors that are used by the pipe
+
+    // establish our pipe, passing it the p array so that it gets populated by the read and write file descriptors
+    if (pipe(p) < 0)
+    {
+        fprintf(stderr, "pipe failed\n");
         exit(1);
     }
-    else if (rc == 0)
-    { // child process satisfies this branch
-        printf("hello, child here (pid: %d) \n", (int)getpid());
-    }
-    else
+
+    // write 16 bytes of data to the write file descriptor
+    write(p[1], msg1, MSGSIZE);
+    write(p[1], msg2, MSGSIZE);
+    write(p[1], msg3, MSGSIZE);
+
+    for (int i = 0; i < 3; i++)
     {
-        printf("hello, parent here (pid: %d) of child %d\n", (int)getpid(), rc);
+        // read 16 bytes of data from the read file descriptor
+        read(p[0], inbuf, MSGSIZE);
+        printf("% s\n", inbuf);
     }
 
     return 0;
