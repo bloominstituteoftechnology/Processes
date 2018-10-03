f1e739611d06e834c87c12978262ca1d0ad674e5
Jameson Brown <jameson@Jamesons-MacBook-Pro.local>
2018-10-03 13:56:11 -0600

    Completed ex3 module

--- ex3/ex3.c
+++ ex3/ex3.c
@@ -9,7 +9,13 @@
 
 int main(void)
 {
-    // Your code here
+    int rv = fork();
+    if (rv == 0) {
+        printf("hello\n");
+    } else {
+        wait(NULL);
+        printf("goodbye\n");
+    }
 
     return 0;
 }
