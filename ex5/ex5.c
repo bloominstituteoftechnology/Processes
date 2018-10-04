205707dea8ed3cac890dd4b3f2d32d78584824a6
Austin Merando <ajmerando1@yahoo.com>
2018-10-04 15:53:11 -0700

    ex5 done

--- ex5/ex5.c
+++ ex5/ex5.c
@@ -13,10 +13,25 @@
 char* msg1 = "hello world #1";
 char* msg2 = "hello world #2";
 char* msg3 = "hello world #3";
-
 int main(void)
 {
-    // Your code here
-    
-    return 0;
+	char buf[MSGSIZE];
+
+	pid_t pid = fork();
+	int fd[2];
+
+	pipe(fd);
+
+	if (pid == 0) {
+			write(fd[1], msg1, MSGSIZE);
+			write(fd[1], msg2, MSGSIZE);
+			write(fd[1], msg3, MSGSIZE);
+	}
+	for (int i = 0; i < 3; i++) {
+		// read 16 bytes of data from the read file descriptor 
+		read(fd[0], buf, MSGSIZE);
+		printf("%s\n", buf);
+	}
+
+	return 0;
 }
