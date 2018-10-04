fb0ad6c90847dee8f83ecd82919328a34b0f9494
m4rkh0ng <m4rkh0ng@gmail.com>
2018-10-04 16:34:02 -0400

    finishes ex5

--- ex5/ex5.c
+++ ex5/ex5.c
@@ -17,6 +17,39 @@ char* msg3 = "hello world #3";
 int main(void)
 {
     // Your code here
-    
+    char inbuf[MSGSIZE];
+    int p[2];
+
+    pipe(p);
+    int rc = fork();
+
+    if(rc == 0) // child /successful fork
+    {
+        printf("child writing to fork\n");
+        write(p[1], msg1, MSGSIZE);
+        write(p[1], msg2, MSGSIZE);
+        write(p[1], msg3, MSGSIZE);
+    }
+    else if(rc > 0) // parent?
+    {
+        int waitcall = waitpid(rc, NULL, 0);
+        printf("parent reading from child\n");
+        // close(p[1]);
+        for (int i = 0; i < 3; i++) {
+            read(p[0], inbuf, MSGSIZE);
+            printf("%s\n", inbuf);
+        }
+        printf("parent is finished\n");
+    }
+    else if (pipe(p) < 0)
+    {
+        printf(stderr, "pipe failed\n");
+        exit(2);
+    }
+    else if(rc < 0) // no fork
+    {
+        printf(stderr, "fork failed\n");
+        exit(1);
+    }
     return 0;
 }
