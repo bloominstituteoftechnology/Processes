93bbcc7f63d321250e8dc90f14ab0e71e21d589c
Simon <Simon_Freelance@hotmail.com>
2018-10-04 16:05:24 -0400

    Added more explanation and refactored the code

--- ex2/ex2.c
+++ ex2/ex2.c
@@ -3,32 +3,61 @@
 // returned by `fopen()`? What happens when they are written to the file concurrently?
 
 // The parent first can update the file then the child updates after the parent.
-//
+// They both can access the file descriptor since they were both able to perform an operation to that specific file
+// which in this case is text.txt
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
 
-int main(void)
-{
-    // Your code here
+
+
+int main(void) {
     FILE *fp;
+
     fp = fopen("text.txt", "w+");
-    printf("Before forking pid: %d\n", getpid());
-    fprintf(fp, "%s %s %s %s %d\n", "File", "write", "before", "fork", getpid());
+    fprintf(fp, "%s %d\n", "Before forking", getpid());
+    if(!fp) {
+        perror("File opening failed");
+        return EXIT_FAILURE;
+    }
     int pid = fork();
-    if (pid < 0) //fork failed
-    {
+    fprintf(fp, "%s %d\n", "After forking", getpid());
+
+    if (pid < 0) {
         exit(1);
     }
-    else if (pid == 0) //child process
-    {
-        printf("AFTER forking CHILD pid: %d\n", getpid());
-        fprintf(fp, "%s %s %d\n", "Child-File", "write", getpid());
+    else if (pid == 0) {
+        fprintf(fp, "%s %d\n", "Child file", getpid());
     }
-    else
-    {
-        printf("AFTER forking PARENT pid: %d\n", getpid());
-        fprintf(fp, "%s %s %d\n", "Parent-File", "write", getpid());
+    else {
+        fprintf(fp, "%s %d\n", "Parent file", getpid());
     }
     fclose(fp);
+    return 0;
 }
+
+// Code that I worked with Vim
+// int main(void)
+// {
+//     // Your code here
+//     FILE *fp;
+//     fp = fopen("text.txt", "w+");
+//     printf("Before forking pid: %d\n", getpid());
+//     fprintf(fp, "%s %s %s %s %d\n", "File", "write", "before", "fork", getpid());
+//     int pid = fork();
+//     if (pid < 0) //fork failed
+//     {
+//         exit(1);
+//     }
+//     else if (pid == 0) //child process
+//     {
+//         printf("AFTER forking CHILD pid: %d\n", getpid());
+//         fprintf(fp, "%s %s %d\n", "Child-File", "write", getpid());
+//     }
+//     else
+//     {
+//         printf("AFTER forking PARENT pid: %d\n", getpid());
+//         fprintf(fp, "%s %s %d\n", "Parent-File", "write", getpid());
+//     }
+//     fclose(fp);
+// }
