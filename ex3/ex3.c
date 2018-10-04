9002e4dca388ee56fa810f99ec84ba1e6799e3df
JacobLayton <jlayjlay3@gmail.com>
2018-10-03 23:30:09 -0700

    Ex3 complete

--- ex3/ex3.c
+++ ex3/ex3.c
@@ -9,7 +9,17 @@
 
 int main(void)
 {
-    // Your code here
+    int rc = fork();
+
+    if (rc < 0) {
+        fprintf(stderr, "fork failed\n");
+        exit(1);
+    } else if (rc == 0) {
+        printf("hello child\n");
+    } else {
+        waitpid(rc, NULL, 0);
+        printf("goodbye parent\n");
+    }
 
     return 0;
 }
