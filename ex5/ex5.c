a51be67d9cc9bbf1587b8840a7f861ff332550b1
cmok4290 <cmok4290@gmail.com>
2018-10-04 16:42:22 -0400

    edited ex5.c

--- ex5/ex5.c
+++ ex5/ex5.c
@@ -16,7 +16,43 @@ char* msg3 = "hello world #3";
 
 int main(void)
 {
-    // Your code here
+    int    fd[2], nbytes, cpid = fork();
+    char   buffer[MSGSIZE];
+
+    if(pipe(fd) < 0)
+    {
+       perror("pipe");
+       exit(1);
+    }
     
+    pipe(fd);
+
+    if(cpid < 0)
+    {
+        perror("fork");
+        exit(1);
+    }
+
+    if(cpid == 0)
+    {
+        printf("Child writing...\n");
+        write(fd[1], msg1, MSGSIZE);
+        write(fd[1], msg2, MSGSIZE);
+        write(fd[1], msg3, MSGSIZE);
+        printf("Finished writing.\n");
+    }
+    else
+    {
+        int wc = waitpid(cpid, NULL, 0);
+        close(fd[1]);
+        printf("Parent reading...\n");
+        nbytes = read(fd[0], buffer, MSGSIZE);
+        while(nbytes > 0)
+        {
+            printf("%s\n", buffer);
+        }
+        printf("Finished reading.\n");
+    }
+
     return 0;
 }
