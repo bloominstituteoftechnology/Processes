71c088908f446b6ec36d16cb900cbc36284dd3d4
Thomas Greenhalgh <thomasgreenhalgh@gmail.com>
2018-10-04 16:27:31 -0700

    finishes mvp

--- ex2/ex2.c
+++ ex2/ex2.c
@@ -11,7 +11,24 @@ int main(void)
     // Your code here
     char *file = "text.txt";
     char *type = "a";
-    double *f = fopen(file, type);
-    // printf("f is %d\n", *f);
+    FILE *f = fopen(file, type);
+
+    int rc = fork();
+    if (rc < 0)
+    { // fork failed; exit
+        fprintf(stderr, "fork failed\n");
+        exit(1);
+    }
+    else if (rc == 0)
+    { // child process satisfies this branch
+        printf("AFTER forking CHILD pid: %d\n", (int)getpid());
+        fprintf(f, "%s %s %d\n", "Child-File", "write", (int)getpid());
+    }
+    else
+    {
+        printf("AFTER forking PARENT pid: %d\n", (int)getpid());
+        fprintf(f, "%s %s %d\n", "Parent-File", "write", (int)getpid());
+    }
+    fclose(f);
     return 0;
 }
