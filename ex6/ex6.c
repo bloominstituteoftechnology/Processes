9d1a6d918ff7c69d255fd148673842c132703001
snowcoding <vimalshah77@gmail.com>
2018-10-04 15:52:00 -0700

    Average the empty write to stdout

--- ex6/ex6.c
+++ ex6/ex6.c
@@ -12,15 +12,51 @@ and `clock_gettime()` should work just fine.
 */
 
 #include <stdio.h>
+#include <stdint.h>
 #include <unistd.h>
 #include <time.h>
 
 #define number_iter 1000000
 #define BILLION 1000000000L
 
+
 int main()
 {
-    // Your code here
+	
+    struct timespec start, end; //Define the timespec start and end
+    uint64_t diff; //Initialize the difference
     
+    int numiter = number_iter; //Initialize the number of iterations
+    uint64_t sum = 0;  //Initialize the sum
+    float avg = 0; //Init the average
+    
+    int avgIter = 10; // Set the number of averages we want
+
+    FILE *fp;
+    fp = fopen ("AvgEmptyWrites.csv", "w+");
+
+    while (avgIter-- > 0){
+
+        while (--numiter > 0){
+            /* measure monotonic time */
+            clock_gettime(CLOCK_MONOTONIC, &start);	/* mark start time */
+            printf("");
+            clock_gettime(CLOCK_MONOTONIC, &end);	/* mark the end time */
+
+            diff = BILLION * (end.tv_sec - start.tv_sec) + end.tv_nsec - start.tv_nsec;
+            sum += diff;
+        }
+
+        avg = sum/number_iter; //Calculate the avg
+
+        fprintf(fp, "%f\n", avg);
+        // printf("elapsed time = %llu nanoseconds\n", (long long unsigned int) diff);
+        // fclose(fp);
+
+        printf("1M Sample Average Run %d: %f ns\n", avgIter, avg);
+
+        sum = 0;
+        numiter = number_iter;
+    }
     return 0;
 }
