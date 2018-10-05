3e1245acd2f9f9bb0aec726d692ae5f1a65aeffb
JacobLayton <jlayjlay3@gmail.com>
2018-10-05 00:33:44 -0700

    Ex5 complete

--- ex5/ex5.c
+++ ex5/ex5.c
@@ -16,7 +16,30 @@ char* msg3 = "hello world #3";
 
 int main(void)
 {
-    // Your code here
+    char inbuf[MSGSIZE];
+    int p[2];
+
+    if (pipe(p) < 0) {
+        fprintf(stderr, "pipe failed\n");
+        exit(1);
+    }
+
+    int rc = fork();
+    if (rc < 0) {
+        fprintf(stderr, "fork failed\n");
+        exit(1);
+    } else if (rc == 0) {
+        printf("hello child\n");
+        write(p[1], msg1, MSGSIZE);
+        write(p[1], msg2, MSGSIZE);
+        write(p[1], msg3, MSGSIZE);
+    } else {
+        for (int i = 0; i < 3; i++) { 
+        read(p[0], inbuf, MSGSIZE);
+        printf("% s\n", inbuf);
+        printf("goodbye parent\n");
+    }
+    }
     
     return 0;
 }
