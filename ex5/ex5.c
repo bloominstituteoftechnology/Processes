d79b562b4465262d90b8d4cf5209f6d984dd5c90
darthMinimus <irishseanvaldivia@gmail.com>
2018-10-05 23:43:04 -0500

    Got all done but ex6 and stretch.

--- ex5/ex5.c
+++ ex5/ex5.c
@@ -17,6 +17,32 @@ char* msg3 = "hello world #3";
 int main(void)
 {
     // Your code here
+    char buffer[MSGSIZE];
+    int pipes[2];
     
+    if(pipe(pipes) < 0){
+        printf("No data to pipe.");
+        exit(1);
+    }
+
+    int f = fork();
+
+    if(f < 0){
+        printf("Fork failed!");
+        exit(2);
+    } else if(f == 0){
+        printf("Child fork\n");
+        write(pipes[1], msg1, MSGSIZE);
+        write(pipes[1], msg2, MSGSIZE);
+        write(pipes[1], msg3, MSGSIZE);
+    } else {
+        wait(NULL);
+        close(pipes[1]);
+        printf("Parent fork\n");
+
+        while(read(pipes[0], buffer, MSGSIZE) > 0){
+            printf("Pipe from parent %s\n", buffer);
+        }
+    }
     return 0;
 }
