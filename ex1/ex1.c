ab9072fe06930237bd3883850ed0413c11ef1020
Rachelle De Jesus Pestanas <rachelle.pestanas@gmail.com>
2018-10-10 18:07:31 -0500

    initial commit: completed ex1

--- ex1/ex1.c
+++ ex1/ex1.c
@@ -8,7 +8,27 @@
 
 int main(void)
 {
-    // Your code here
+    int x = 100;
+    printf(" Value of x is %d\n ", x);
 
+    int newP = fork();
+    printf("parent is calling for a child value %d\n", newP);
+    // Your code here
+    if (newP == 0)
+    {
+        printf(" if pid == 0 , value of x is %d\n", x);
+        printf(" if pid == 0 , newP is %d\n", newP);
+        x = 94;
+        printf("after value is = 94, x is %d\n", x);
+        printf("after value is = 94, newP is %d\n", newP);
+    }
+    else
+    {
+        printf(" else x is %d\n", x);
+        printf(" else newP is %d\n", newP);
+        x = 43;
+        printf(" after value is = 43, x is %d\n", x);
+        printf(" after value is = 43, newP is %d\n", newP);
+    }
     return 0;
 }
