276de5784a29c84d3d4e3f1fd90a24fdecae0546
Rachelle De Jesus Pestanas <rachelle.pestanas@gmail.com>
2018-10-11 17:41:49 -0500

    completed ex2

--- ex2/ex2.c
+++ ex2/ex2.c
@@ -8,7 +8,25 @@
 
 int main(void)
 {
-    // Your code here 
+    FILE *file;
+    file = fopen("text.txt", "r+"); 
     
+    printf("Parent process, pid is %d\n", getpid());
+    int pid = fork();
+    
+     if (pid < 0)
+    {
+        fprintf(stderr, "fork failed\n");
+        exit(1);
+    }
+    else if (pid == 0)
+    {
+        fprintf(file, "%s: %d\n", "Child process", getpid());
+    }
+    else
+    {
+        fprintf(file, "%s\n", "Parent process again");
+    }
+    fclose(file);
     return 0;
 }
