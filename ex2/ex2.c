c33214011bd7123d0f5b13a8814ab660334f702b
Randy Calderon <randycweb@gmail.com>
2018-10-12 02:47:25 -0700

    Completed ex2

--- ex2/ex2.c
+++ ex2/ex2.c
@@ -2,6 +2,9 @@
 // and then calls `fork()` to create a new process. Can both the child and parent access the file descriptor
 // returned by `fopen()`? What happens when they are written to the file concurrently?
 
+// Yes they can both access the file descriptor
+// The text file writes both entries no problem one after another in new lines to the text file
+
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
@@ -13,21 +16,23 @@ int main(void)
     FILE *fd;
 
     fd = fopen("text.txt", "w+");
-    
-    fprintf(fd, "%s %d", "Before", getpid());
+
+    fprintf(fd, "%s %d\n", "Before the fork\n", getpid());
 
     pid_t process = fork();
 
     if (process == 0)
     {
-        printf("The file opened child\n");
+        fprintf(fd, "%s %d\n", "The file opened child\n", getpid());
         fclose(fd);
     }
     else
     {
-        printf("The file opened parent\n");
+        fprintf(fd, "%s %d\n", "The file opened parent\n", getpid());
         fclose(fd);
     }
+
+    fclose(fd);
 
     return 0;
 }
