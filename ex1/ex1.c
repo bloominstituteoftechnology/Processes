559495790434aefbeaf376fcac2a346e314fe72a
Sukhada Gholba <sukhadagholba@gmail.com>
2018-11-07 13:24:19 -0800

    used fork() system call to observe changes to the varibale x by parent and child process

--- ex1/ex1.c
+++ ex1/ex1.c
@@ -9,6 +9,20 @@
 int main(void)
 {
     // Your code here
+    int x =100;
+    pid_t pid;
+	
+    pid=fork();
+
+    if(pid==0){
+	x=80;    
+    	printf("I'm the child! and x = %d\n", x);  //child changes the value of x and prints it out
+    }
+    else{
+	x=99;    
+    	printf("I'm the parent! and x = %d\n", x); //parent changes the value of x and prints it out
+    }
+     
 
     return 0;
 }
