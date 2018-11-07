1ecf0ba645d37eeec67d10e02c4919fe79c5cc3f
Charlie Winslow <cwinslow22@yahoo.com>
2018-11-07 15:49:19 -0600

    Charlie Winslow
    ex1 completed

--- ex1/ex1.c
+++ ex1/ex1.c
@@ -8,7 +8,22 @@
 
 int main(void)
 {
-    // Your code here
+    int x = 100;
+    printf("hello world (pid: %d)\n", (int) getpid());
+    int rc = fork();
+    // ------------------------------------------------ child process starts executing here
+    if (rc < 0) {    // fork failed; exit
+        fprintf(stderr, "fork failed\n");
+        exit(1);
+    } else if (rc == 0) {    // child process satisfies this branch
+        printf("hello, child here (pid: %d), x = %d \n", (int) getpid(), x);
+        x=99;
+        printf("hello, child here again (pid: %d), x = %d \n", (int) getpid(), x);
+    } else {
+        printf("hello, parent here (pid: %d) of child %d, x = %d \n", (int) getpid(), rc, x);
+        x=101;
+        printf("hello, parent here again (pid: %d) of child %d, x = %d \n", (int) getpid(), rc, x);
+    }
 
     return 0;
 }
