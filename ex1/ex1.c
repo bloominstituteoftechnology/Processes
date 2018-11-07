45db32eea8f8b60228046fb6a8bcb1be82d5a4a7
tulf <timzulf@gmail.com>
2018-11-07 18:26:49 -0500

    Tim Zulf- WIP { ex1: stick a fork in it. }

--- ex1/ex1.c
+++ ex1/ex1.c
@@ -8,7 +8,18 @@
 
 int main(void)
 {
-    // Your code here
-
+  // first set x to 100 I guess
+  int x = 100;
+  int rc = fork();
+  if (rc == 0){
+    printf(" the child is %d\n", x);
+    //change value of x and see what happnes:
+    x = 20;
+    printf("Tada it's %d now\n", x);
+  }
+  else{
+      printf(" Parnet is %d\n", x);
+  }
+  // leave return outside of if else branch
     return 0;
 }
