b365de7ec6262fbc92ec141adf3df4d83600fb27
mkerbleski <mkerbles@gmail.com>
2018-11-07 15:15:18 -0700

    complete ex3.c

--- ex3/ex3.c
+++ ex3/ex3.c
@@ -10,6 +10,20 @@
 int main(void)
 {
     // Your code here
+    int process = getpid();
+    printf("process %d\n", process);
+    int parent = getppid();
+    printf("parent %d\n", parent);
+    char * hello = "hello";
+    int rc = fork();
+    int wc = waitpid(rc, NULL, 0); //waits for fork on line 15
+    if(rc < 0){
+        printf("fork failed\n");
+    } else if(rc == 0){
+        printf("%s\n", hello);
+    } else {
+        printf("goodbye\n");
+    }
 
     return 0;
 }
