4dc5006350dbd36851de0e0c2daa3913e11f69d4
Noah Amar <ne.amar@outlook.com>
2018-11-07 16:03:51 -0600

    mvp--ex1, ex2, ex3

--- ex2/ex2.c
+++ ex2/ex2.c
@@ -8,7 +8,32 @@
 
 int main(void)
 {
+    pid_t pid;
+    pid = fork();
     // Your code here 
-    
+	FILE* fp; 
+	// Open file in Read mode 
+	fp = fopen("text.txt", "w"); 
+
+	// data in file: geeksforgeeks 
+
+	// for (int i=0; i<10; i++){
+    //     fprintf(fp, "Noah\n", i);
+    // }
+ 
+
+    // return 0;
+
+    if (pid < 0) {    // fork failed; exit
+        fprintf(stderr, "fork failed\n");
+        exit(1);
+    } else if (pid == 0) {    // child process satisfies this branch
+        
+       fprintf(fp, "car\n");
+    } else {
+        
+        fprintf(fp, "dog\n");
+    }
+    fclose(fp); // Close File after Reading 
     return 0;
 }
