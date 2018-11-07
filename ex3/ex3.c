428e8d8c93b322bdc62bd2c3dbb6ce055f46ca46
francistse23 <wtse.ucsd@gmail.com>
2018-11-07 12:57:49 -0800

    Ex3 completed. Hello always prints before Goodbye.

--- ex3/ex3.c
+++ ex3/ex3.c
@@ -9,7 +9,13 @@
 
 int main(void)
 {
-    // Your code here
+    int rc = fork();
+    if (rc == 0) {
+        printf("hello\n");
+    } else {
+        wait(NULL);
+        printf("goodbye\n");
+    }
 
     return 0;
 }
