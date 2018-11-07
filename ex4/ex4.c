33a5fb3dc9742da6bdd5b2f857f647ad19674c9c
sca4cs <shannon.atkinson.ca@gmail.com>
2018-11-07 18:07:51 -0500

    exec

--- ex4/ex4.c
+++ ex4/ex4.c
@@ -4,13 +4,33 @@
 // are so many variants of the same basic call?
 
 #include <stdio.h>
-#include <unistd.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <sys/wait.h>
+#include <string.h>
 
 int main(void)
 {
-    // Your code here    
+    int rc = fork();
+    
+    if (rc < 0) {
+        fprintf(stderr, "fork failed\n");
+        exit(1);
+    } else if (rc == 0) {
+        // char *args[]={"/bin/ls",NULL}; 
+        // execvp(args[0],args);
+
+        // char *args[]={"/bin/ls",NULL};
+        // execv(args[0],args);
+
+        //execl("/bin/ls", "/bin/ls", NULL);
+
+        execle("/bin/ls", "/bin/ls", NULL);
+
+        printf("everything after exec() is ignored as this process is replaced with another process\n");
+    } else {
+        int wc = waitpid(rc, NULL, 0);
+    }
 
     return 0;
 }
---
+++ ex4/exec.c
@@ -0,0 +1,9 @@
+#include<stdio.h> 
+#include<unistd.h> 
+  
+int main(void) 
+{ 
+    printf("I am exec.c called by execvp()\n"); 
+      
+    return 0; 
+} 
