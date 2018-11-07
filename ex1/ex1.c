f5a2d2f0fd34fe39367321b28b0357453d636494
Munsu <dark_specineff2002@yahoo.com>
2018-11-07 15:15:34 -0800

    still stuck on ex1

--- ex1/ex1.c
+++ ex1/ex1.c
@@ -9,5 +9,25 @@
 int main(void)
 {
     // Your code here
-    zzz return 0;
+    int x = 100;
+    printf("hello world (pid: %d)\n", (int)getpid());
+    int rc = fork();
+    // ------------------------------------------------ child process starts executing here
+    if (rc < 0)
+    { // fork failed; exit
+        fprintf(stderr, "fork failed\n");
+        exit(1);
+    }
+    else if (rc == 0)
+    { // child process satisfies this branch
+        x = 200;
+        printf("hello, child here (pid: %d) \n", (int)getpid());
+    }
+    else
+    {
+        x = 500;
+        printf("hello, parent here (pid: %d) of child %d\n", (int)getpid(), rc);
+    }
+
+    return 0;
 }
