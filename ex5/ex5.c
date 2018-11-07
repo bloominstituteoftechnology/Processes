8e1d67373257bfc2f83e12d7b11681bfb4436c53
Sukhada Gholba <sukhadagholba@gmail.com>
2018-11-07 15:46:05 -0800

    completed ex5

--- ex5/ex5.c
+++ ex5/ex5.c
@@ -17,6 +17,32 @@ char* msg3 = "hello world #3";
 int main(void)
 {
     // Your code here
-    
+    char buffer[MSGSIZE];    
+    int p[2]; 
+    int pipe_var = pipe(p);
+
+    int f = fork();    
+
+    if (pipe_var < 0) {
+        fprintf(stderr, "pipe failed\n");
+        exit(1);
+    }
+
+    if(f<0){
+    	fprintf(stderr, "fork failed\n");
+        exit(1);
+    }
+    else if(f==0){
+   	write(p[1], msg1, MSGSIZE);
+    	write(p[1], msg2, MSGSIZE);
+    	write(p[1], msg3, MSGSIZE);
+    }
+    else{
+
+    	for (int i = 0; i < 3; i++) {
+        	read(p[0], buffer, MSGSIZE);
+        	printf("%s\n", buffer);
+    	}	    
+    }
     return 0;
 }
