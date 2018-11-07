8cad46657de33c84688aa418c04fdf34a74b4b3b
sca4cs <shannon.atkinson.ca@gmail.com>
2018-11-07 15:55:13 -0500

    fork

--- ex1/ex1.c
+++ ex1/ex1.c
@@ -8,7 +8,25 @@
 
 int main(void)
 {
-    // Your code here
+    int x = 100;
+
+    printf("hello world (pid: %d); x = %d \n", (int) getpid(), x);
+    int rc = fork();
+    // ------------------------------------------------ child process starts executing here
+    if (rc < 0) {    // fork failed; exit
+        fprintf(stderr, "fork failed\n");
+        exit(1);
+    } else if (rc == 0) {    // child process satisfies this branch
+        x = 99;
+        printf("hello, child here (pid: %d); x = %d \n", (int) getpid(), x);
+    } else {
+        x = 101;
+        printf("hello, parent here (pid: %d); x = %d\n", (int) getpid(), x);
+    }
+
+    printf("hello world again (pid: %d); x = %d \n", (int) getpid(), x);
 
     return 0;
 }
+
+// The parent process and the child process both get their own copy of variable x which can be independently updated.
