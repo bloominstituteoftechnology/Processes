840a7728ee9f2410902eb0d9edadc1ff934e6f43
codejoncode <jonathanjholloway@gmail.com>
2018-11-07 15:06:59 -0600

    did the first ex1.c  excercised answer the questions for the child and parent  x variable starts off as it is intially before the fork.  even after the parent changes x. The child doesn't notice the change and starts as if x was orginally initialize to its value then makes its own change. This to is not reconignized most likely by the parent.

--- ex1/ex1.c
+++ ex1/ex1.c
@@ -1,6 +1,21 @@
 // Write a program that calls `fork()`. Before calling `fork()`, have the main process access a variable
 // (e.g., x) and set its value to something (e.g., 100). What value is the variable in the child process?
 // What happens to the variable when both the child and parent change the value of x?
+/*
+to access a variable  is to read or assign the information inside the variable. to access a variable you simply type its name. 
+*/
+
+//What happens is that each variable is it changes for that position x is changed for the child and x is changed for the parent.  however x doesn't really change   example 
+
+/*
+here  is one of the outputs. 
+x is : 100
+x is : 722
+x is : 100
+x is : 122
+
+so this is the before the change and after   722 is the parent 122 is the child.    intially it will be 100 but for the child it is also intially set at 100 even after the parent change.  the child doesn't recongize this change at all. 
+*/
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -8,7 +23,21 @@
 
 int main(void)
 {
-    // Your code here
+    int x = 100; 
+    int rc = fork();
+    
+    if(rc == 0){
+      printf("x is : %d\n", x); 
+      x = 122;
+      printf("x is : %d\n", x); 
+    } else{
+      printf("x is : %d\n", x); 
+      x = 722; 
+      printf("x is : %d\n", x);  
+    }
+
+
+
 
     return 0;
 }
