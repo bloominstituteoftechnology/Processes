9dae86dd5a3f7976a7ae19649d0904f006d0ef1a
James Phillips <bri79319@gmail.com>
2018-11-07 17:09:10 -0500

    ex1 done

--- ex1/ex1.c
+++ ex1/ex1.c
@@ -5,10 +5,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/wait.h>
 
 int main(void)
 {
     // Your code here
+    int x = 100;
 
-    return 0;
+    int rc = fork();
+
+    if (rc == 0) // Child
+    {
+        printf("Child Process\n");
+        printf("Value of x is: %d\n", x);
+        x = 200;
+        printf("New Value of x is: %d\n", x);
+    }
+
+    else if (rc > 0) // Parent
+    {
+        wait(NULL);
+        printf("Parent Process\n");
+        printf("Value of x is: %d\n", x);
+        x = 300;
+        printf("New Value of x is: %d\n", x);
+    }
+
+    else // Error
+    {
+        fprintf(stderr, "Error Creating Process");
+    }
 }
