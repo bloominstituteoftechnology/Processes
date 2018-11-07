ab38d0069772bd67d901e9bab6203d3b82914635
Sukhada Gholba <sukhadagholba@gmail.com>
2018-11-07 15:05:46 -0800

    completed ex4

--- ex4/ex4.c
+++ ex4/ex4.c
@@ -10,7 +10,27 @@
 
 int main(void)
 {
-    // Your code here    
+    // Your code here
+    	printf("hello world (pid: %d)\n", (int) getpid());
 
-    return 0;
+    	pid_t pid;    
+	pid=fork();
+
+	if(pid<0){             // fork failed; exit
+        fprintf(stderr, "fork failed\n");    //stderr is standard error stream and is the default destination for error messages and other diagnostic warnings
+        exit(1);
+		
+	}
+	else if(pid==0){
+		printf("This is child process pid %d\n", getpid());
+        	execl("/bin/ls", "ls", NULL);   
+		printf("Return not expected. Must be an execl() error");
+
+	}
+	else{
+		wait(NULL);
+        	printf("This is parent process pid %d\n", getpid());
+	}
+
+    	return 0;
 }
