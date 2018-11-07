c05740fb0e7b3c75058514f057d1cd8762c3e4fe
fron12 <sohnf90@yahoo.com>
2018-11-07 15:26:20 -0800

    ex1 attempt

--- ex1/ex1.c
+++ ex1/ex1.c
@@ -9,6 +9,23 @@
 int main(void)
 {
     // Your code here
- 
+    int x = 100;
+    int first = fork();
+    printf("Hello world (x: %d)\n", x);
+
+    if(first < 0){
+        fprintf(stderr, "fork failed\n");
+        exit(1);
+    }
+    else if(first == 0) {
+        printf("Hello, this is child (x: %d) \n", x);
+        x = 12;
+        printf("Hello, this is updated child (x: %d) \n",  x);
+    }
+    else {
+        printf("Hello, this is parent (x: %d) of child %d\n", x, first);
+        x = 45;
+        printf("Hello, this is updated parent (x: %d) of child %d\n", x, first);
+    }
     return 0;
 }
