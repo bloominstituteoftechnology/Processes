6f44ee5aa17396350d071cd8625076cac0664224
William <kwontaewoongah@gmail.com>
2018-11-07 15:18:12 -0800

    refactored ex2 and also answered quetions for ex1 and ex2

--- ex2/ex2.c
+++ ex2/ex2.c
@@ -1,43 +1,24 @@
 // Write a program that opens the text.txt  file (with the `fopen()` system call) located in this directory 
 // and then calls `fork()` to create a new process. Can both the child and parent access the file descriptor 
 // returned by `fopen()`? What happens when they are written to the file concurrently?
-
+//the child and parent can both access the file descriptor returned by fopen()
+//when they are written to the file concurrently the text appears non-deterministically as either the text from the parent or the child 
+//may appear first.
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
 
 int main(void)
 {
-    // Your code here 
-    FILE *fp=fopen("./text.txt","w+");
-    int c;
+    FILE *fp=fopen("./text.txt","w");
     int forked=fork();
     if (forked==0){
         char str[] = "This is Lambda!\n";
         fwrite(str , 1 , sizeof(str) , fp );
-        fclose(fp);
-        FILE *child_fp=fopen("./text.txt","r");
-        do {
-            c = fgetc(fp);
-            if ( feof(fp) ) {
-                break ;
-            }
-        printf("%c", c);
-        } while(1);
-        fclose(child_fp);
     } else{
         char str[] = "This is Sparta!\n";
         fwrite(str , 1 , sizeof(str) , fp );
-        fclose(fp);
-        FILE *parent_fp=fopen("./text.txt","r");
-        do {
-            c = fgetc(fp);
-            if ( feof(fp) ) {
-                break ;
-            }
-        printf("%c", c);
-        } while(1);
-        fclose(parent_fp);
     }
+    fclose(fp);
     return 0;
 }
