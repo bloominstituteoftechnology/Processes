779e44e050668cf55009ed84c67949e69ac04187
Rachel DiCesare <racheldicesare@missrachelthedeveloper.com>
2018-11-08 15:34:55 -0500

    completed ex5

--- ex5/ex5.c
+++ ex5/ex5.c
@@ -17,6 +17,19 @@ char* msg3 = "hello world #3";
 int main(void)
 {
     // Your code here
-    
-    return 0;
+  char inbuf[MSGSIZE];
+  int p[2], i;
+
+  if (pipe(p) < 0)
+     exit(1);
+
+     write(p[1], msg1, MSGSIZE);
+     write(p[1], msg2, MSGSIZE);
+     write(p[1], msg3, MSGSIZE);
+
+     for (i = 0; i < 3; i++) {
+         read (p[0], inbuf, MSGSIZE);
+         printf("%s\n", inbuf);
+     }
+     return 0;
 }
