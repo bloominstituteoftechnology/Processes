8671ff8a65d8ba71f67fb12c7a12ab6b6131cf44
AI2EJAY <JHammett1314@gmail.com>
2018-11-08 17:46:06 -0600

    6 Not working yet but some template down for it

--- ex4/ex4.c
+++ ex4/ex4.c
@@ -1,8 +1,7 @@
 // Write a program that calls `fork()` and then calls some form of `exec()`
 // to run the program `/bin/ls`. Try a few variants of `exec()`, such as 
 // `execl()`, `execle()`, `execv()`, and others. Why do you think there 
-// are so many variants of the same basic call?
-
+// are so many variants of the same basic call? different types of pointers to make the call with
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
@@ -11,7 +10,30 @@
 int main(void)
 {
     int f = fork();
-    // Your code here    
 
+    if (f < 0) 
+    {
+        fprintf(stderr, "fork failed\n");
+    
+    } else if (f == 0) 
+    {
+        printf("I'm the kid (pid: %d)\n", (int) getpid());
+        char *myArgs[2];
+
+        myArgs[0] = "/bin/ls";
+        myArgs[1] = NULL;
+
+        // execv(myArgs[0], myArgs);
+        execvp(myArgs[0], myArgs);
+        fprintf(stderr, "execvp failed\n");
+        // you can write an error fprintf under here, because if execvp is successful
+        // no code under execvp should run
+        // but yeah it's workin shouldn't it show more what my 1: bash terminal shows though or is there a way I coudl get that? cause that means what it just ran the file we were already in? or idk?
+    } else 
+    {
+        int holdup = waitpid(f, NULL, 0);
+        printf("Parent here (pid: %d) of child %d\n", (int) getpid(), f);
+    }
+  
     return 0;
 }
--- ex6/ex6.c
+++ ex6/ex6.c
@@ -18,9 +18,39 @@ and `clock_gettime()` should work just fine.
 #define number_iter 1000000
 #define BILLION 1000000000L
 
-int main()
+int localpid(void) {
+	static int a[9] = { 0 };
+	return a[0];
+}
+
+int main(int argc, char **argv)
 {
+    
+	__uint64_t diff;
+	struct timespec start, end;
+	int i;
+
+	/* measure monotonic time */
+	clock_gettime(CLOCK_MONOTONIC, &start);	/* mark start time */
+	sleep(1);	/* do stuff */
+	clock_gettime(CLOCK_MONOTONIC, &end);	/* mark the end time */
+
+	diff = BILLION * (end.tv_sec - start.tv_sec) + end.tv_nsec - start.tv_nsec;
+	printf("elapsed time = %llu nanoseconds\n", (long long unsigned int) diff);
+
+	/* now re-do this and measure CPU time */
+	/* the time spent sleeping will not count (but there is a bit of overhead */
+	clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &start);	/* mark start time */
+	sleep(1);	/* do stuff */
+	clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &end);		/* mark the end time */
+
+	diff = BILLION * (end.tv_sec - start.tv_sec) + end.tv_nsec - start.tv_nsec;
+	printf("elapsed process CPU time = %llu nanoseconds\n", (long long unsigned int) diff);
+
+	return 0;
+
+    // int clockid_t clock_gettime(CLOCK_REALTIME, struct timespec *tp);
     // Your code here
     
-    return 0;
+    // return 0;
 }
