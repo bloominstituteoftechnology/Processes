73ff568ea394affa5ff0dcfabf47ee31fdda3593
Katherine Gilligan <katia.gilligan88@gmail.com>
2018-11-08 14:34:09 -0700

    Finished ex5!

--- ex5/ex5.c
+++ ex5/ex5.c
@@ -16,7 +16,32 @@ char* msg3 = "hello world #3";
 
 int main(void)
 {
-    // Your code here
-    
+    char inbuf[MSGSIZE];
+    int p[2]; 
+
+    if (pipe(p) < 0) {
+        fprintf(stderr, "pipe failed\n");
+        exit(1);
+    }
+
+    int rc = fork();
+
+    if (rc < 0) {
+        fprintf(stderr, "fork failed\n");
+        exit(1); 
+    }else if(rc == 0){
+        printf("Child fork writing to pipe\n"); 
+        write(p[1], msg1, MSGSIZE);
+        write(p[1], msg2, MSGSIZE);
+        write(p[1], msg3, MSGSIZE);
+    }else{
+        int wc = waitpid(rc, NULL, 0); // child runs before parent to write 
+        printf("Parent fork reading from pipe\n"); 
+        for (int i = 0; i < 3; i++) {
+            read(p[0], inbuf, MSGSIZE);
+            printf("% s\n", inbuf);
+        }
+    }
+
     return 0;
 }
