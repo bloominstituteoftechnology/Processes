46839b30fd286864f8b25289852239691be21cbf
Josue Peralta <jperal77@gmail.com>
2018-11-08 16:09:50 -0600

    completed exercise 5

--- ex5/ex5.c
+++ ex5/ex5.c
@@ -1,7 +1,7 @@
-// Write a program that forks a child and creates a shared pipe
-// between the parent and child processes. Have the child write 
-// the three messages to the parent and have the parent print out 
-// the messages. 
+// Write a program that rcs a rc and creates a shared pipe
+// between the parent and rc processes. Have the rc write
+// the three messages to the parent and have the parent print out
+// the messages.
 
 #include <stdio.h>
 #include <unistd.h>
@@ -10,13 +10,49 @@
 
 #define MSGSIZE 16
 
-char* msg1 = "hello world #1";
-char* msg2 = "hello world #2";
-char* msg3 = "hello world #3";
+char *msg1 = "hello world #1";
+char *msg2 = "hello world #2";
+char *msg3 = "hello world #3";
 
 int main(void)
 {
     // Your code here
-    
+    char inbuf[MSGSIZE]; // a buffer that will hold the incoming data that is being written
+    int p[2];            // a two-element array to hold the read and write file descriptors that are used by the pipe
+
+    // establish our pipe, passing it the p array so that it gets populated by the read and write file descriptors
+    if (pipe(p) < 0)
+    {
+        fprintf(stderr, "pipe failed\n");
+        exit(1);
+    }
+
+    int rc = fork();
+
+    if (rc < 0)
+    {
+        fprintf(stderr, "forking failed\n");
+        exit(1);
+    }
+    else if (rc == 0)
+    {
+        printf("Parent writing to pipe\n");
+        // write 16 bytes of data to the write file descriptor
+        write(p[1], msg1, MSGSIZE);
+        write(p[1], msg2, MSGSIZE);
+        write(p[1], msg3, MSGSIZE);
+    }
+    else
+    {
+        int wc = waitpid(rc, NULL, 0);
+
+        printf("Child reading from pipe\n");
+        for (int i = 0; i < 3; i++)
+        {
+            // read 16 bytes of data from the read file descriptor
+            read(p[0], inbuf, MSGSIZE);
+            printf("%s\n", inbuf);
+        }
+    }
     return 0;
 }
