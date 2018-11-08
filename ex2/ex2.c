eb994cf41007971f208fabb0ef46ce7a7ee23542
Sukhada Gholba <sukhadagholba@gmail.com>
2018-11-08 09:15:49 -0800

    updated answers

--- ex2/ex2.c
+++ ex2/ex2.c
@@ -2,6 +2,9 @@
 // and then calls `fork()` to create a new process. Can both the child and parent access the file descriptor 
 // returned by `fopen()`? What happens when they are written to the file concurrently?
 
+//Answer-Yes, child and parent can both access the file descriptor. Both process are able to write to the text file. The parent seem to write first and then the child.
+
+
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
@@ -15,7 +18,11 @@ int main(void)
 
 	pid_t pid;
     	pid = fork();	
-
+	
+	if(pid<0){
+        	fprintf(stderr, "fork failed\n");
+        	exit(1);
+    	}
 	if(pid==0){
     		fprintf(fp, "This is child process\n"); //this sentence is written to the text.txt file by accessing the file descriptor returned by fopen
 	}
