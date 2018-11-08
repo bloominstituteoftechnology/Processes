186befba096b5d1da5df41303ea00d67cde6f816
Carey <careycbaldwin@gmail.com>
2018-11-08 14:52:54 -0500

    first attempt at exercise 4, seems to work with execv

---
+++ practice/thursday.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+// int main(void)
+// {
+//     fork();
+//     fork();
+//     fork();
+//     fork();
+
+//     printf("rabbits\n");
+
+//     return 0;
+// }
+
+
+int main(void)
+{
+    char commandLine[8192];
+
+    printf("ls$ ");
+
+    scanf(" %s")
+}
