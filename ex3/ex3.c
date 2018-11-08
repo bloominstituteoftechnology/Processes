8f3bcf89c6acaaf8afdb3e4fbc60150055bd9261
Carey <careycbaldwin@gmail.com>
2018-11-08 12:18:51 -0500

    completed exercise 3

--- ex3/ex3.c
+++ ex3/ex3.c
@@ -7,9 +7,33 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
+//https://www.geeksforgeeks.org/wait-system-call-c/
+
+// int main()
+// {
+//     pid_t cpid;
+//     if (fork() == 0)
+//         exit(0);  //end child process
+//     else
+//         cpid = wait(NULL);
+//     printf("Parent pid = %d\n", getpid());
+//     printf("Child pid = %d\n", cpid);
+//     return 0;
+// }
+
+
 int main(void)
 {
-    // Your code here
+    int rc = fork();
 
+    if(rc<0) {
+        fprintf(stderr, "fork failed\n");
+        exit(1);
+    } else if (rc == 0){
+        printf("hello, pid = %d\n", getpid());
+    }else {
+        wait(NULL);
+        printf("goodbye, pid = %d\n", getpid());
+    }
     return 0;
 }
