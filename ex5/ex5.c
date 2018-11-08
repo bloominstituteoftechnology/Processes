bc64f6378d2251fef92ffc4bd9daff966e99d200
fron12 <sohnf90@yahoo.com>
2018-11-08 15:55:28 -0800

    ex5 attempt

--- ex5/ex5.c
+++ ex5/ex5.c
@@ -18,5 +18,35 @@ int main(void)
 {
     // Your code here
     
+    char inbuf[MSGSIZE]; // buffer that will hold the incoming data that is being written
+    int p[2]; // a two-element array to  hold the read and write file descriptors that are used by the pipe
+
+    if(pipe(p) < 0) {
+        fprintf(stderr, "pipe failed\n");
+        exit(1);
+    }
+
+    int fifth = fork();
+
+    if(fifth< 0) {
+        fprintf(stderr, "fork failed\n");
+        exit(1);
+    }
+
+    if(fifth == 0) {
+        printf("Hello, child here. (pid: %d) \n", (int) getpid());
+        write(p[1], msg1, MSGSIZE);
+        write(p[1], msg2, MSGSIZE);
+        write(p[1], msg3, MSGSIZE);
+    }
+    else {
+        waitpid(fifth, NULL, 0);
+        printf("Hello, parent here. (pid: %d) \n", (int) getpid());
+        for(int i = 0; i < 3; i++) {
+            read(p[0], inbuf, MSGSIZE);
+            printf("%s\n", inbuf);
+        }
+    }
+
     return 0;
 }
