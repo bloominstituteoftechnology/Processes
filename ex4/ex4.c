72bf2d3c48154403a756798420850595c5e78dac
Csillag61 <csillagok@gmail.com>
2019-01-09 17:38:29 -0500

    ex4 prints the /bin/ls

--- ex4/ex4.c
+++ ex4/ex4.c
@@ -2,15 +2,37 @@
 // to run the program `/bin/ls`. Try a few variants of `exec()`, such as 
 // `execl()`, `execle()`, `execv()`, and others. Why do you think there 
 // are so many variants of the same basic call?
-
+//
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <sys/wait.h>
+#include <string.h>
 
 int main(void)
 {
-    // Your code here    
+   int rc = fork(); //starting the child process
+   if (rc < 0) {
+       fprintf(stderr, "fork failed\n"); //error handling
+       exit(1);
+
+   } else if ( rc == 0){
+       printf("Child running\n");
+       char *myargs[2];
+       myargs[0] = strdup("/bin/ls");//douplicate the strng
+       myargs[1] = NULL;
+
+
+       execvp(myargs[0], myargs);
+   } else {
+       waitpid(rc, NULL, 0);//Wait for a specific child process to end
+       printf("Child done\n");
+       char *myargs[2];
+       myargs[0]= strdup("/bin/ls");//douplicate the strng
+       myargs[1] = NULL;
+       execv(myargs[0], myargs);
+
+   }
 
     return 0;
 }
