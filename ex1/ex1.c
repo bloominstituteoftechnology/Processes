37c712471de85b28dcad5e6c29d9f9641489b962
Nicocchi <nicocchilover@gmail.com>
2019-01-09 15:38:46 -0500

    ex1 and ex2 complete

--- ex1/ex1.c
+++ ex1/ex1.c
@@ -1,6 +1,10 @@
 // Write a program that calls `fork()`. Before calling `fork()`, have the main process access a variable
 // (e.g., x) and set its value to something (e.g., 100). What value is the variable in the child process?
 // What happens to the variable when both the child and parent change the value of x?
+//
+// The value of the variable is 100. When changing the value of the variable in the Parent
+// and child, it changes to the value of the what is defined and doesn't interfere with the
+// other value changes of the same variable.
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -8,7 +12,17 @@
 
 int main(void)
 {
-    // Your code here
+    int x = 100;
+
+    pid_t pid = fork();
+
+    if (pid == 0) {
+      x = 10;
+      printf("Child! X is %d\n", x);
+    } else {
+      x = 20;
+      printf("Parent! X is %d\n", x);
+    }
 
     return 0;
 }
--- ex2/ex2.c
+++ ex2/ex2.c
@@ -1,6 +1,11 @@
-// Write a program that opens the text.txt  file (with the `fopen()` system call) located in this directory 
-// and then calls `fork()` to create a new process. Can both the child and parent access the file descriptor 
+// Write a program that opens the text.txt  file (with the `fopen()` system call) located in this directory
+// and then calls `fork()` to create a new process. Can both the child and parent access the file descriptor
 // returned by `fopen()`? What happens when they are written to the file concurrently?
+//
+// The child does not have access to the file.
+// When writing to the files concurrently, the child takes precedence over the parent.
+// I assume this is because the child process is called last, so whatever is being written from
+// the child to the file will overwrite what was written from the parent to the file.
 
 #include <stdio.h>
 #include <unistd.h>
@@ -8,7 +13,54 @@
 
 int main(void)
 {
-    // Your code here 
-    
+    FILE *fp;
+
+    fp = fopen("text.txt", "r");
+
+    pid_t pid = fork();
+
+    // Check if file exists, otherwise, output error
+  if (fp == NULL) {
+		printf("Failed To Open File\n");
+		return 0;
+	}
+
+  if (pid == 0) {
+    printf("Child\n");
+    int c;
+
+    while(1) {
+      c = fgetc(fp);
+      if (feof(fp)) {
+        break;
+      }
+      printf("%c", c);
+    }
+    fclose(fp);
+
+    fp = fopen("text.txt", "w+");
+    fprintf(fp, "%s %s %s %d", "We", "are", "in", 2021);
+    fclose(fp);
+
+  } else {
+    printf("Parent\n");
+    int c;
+
+    while(1) {
+      c = fgetc(fp);
+      if (feof(fp)) {
+        break;
+      }
+      printf("%c", c);
+    }
+    fclose(fp);
+
+    fp = fopen("text.txt", "w+");
+    fprintf(fp, "%s %s %s %d", "We", "are", "in", 2020);
+    fclose(fp);
+  }
+
+
+
     return 0;
 }
