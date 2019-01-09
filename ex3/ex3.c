571c1bf6c5d60646ae23451c5e93e2106db560e4
Timothy Hoang <timh1203@gmail.com>
2019-01-09 15:05:26 -0600

    Add exercise 3

--- ex3/ex3.c
+++ ex3/ex3.c
@@ -2,6 +2,8 @@
 // the parent process should print "goodbye". You should ensure that the child
 // process always prints first.
 
+// - [Dig Deeper: SO's print parent process at end](https://stackoverflow.com/questions/46458684/c-how-to-print-parent-process-at-the-end-of-child)
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -10,6 +12,22 @@
 int main(void)
 {
     // Your code here
+
+    // starting process
+    int rc = fork();
+    
+    // child process starts execution here
+    if (rc < 0) { // fork failed; exit
+      fprintf(stderr, "fork failed\n");
+      exit(1);
+    }
+    else if (rc == 0) { // child process satisfies this branch
+      printf("hello \n");
+    }
+    else {
+      wait(NULL);
+      printf("goodbye \n");
+    }
 
     return 0;
 }
