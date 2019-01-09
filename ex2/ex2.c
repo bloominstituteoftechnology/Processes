7c885389e39677a1651685e50bc0e8767ce5f9d5
Timothy Hoang <timh1203@gmail.com>
2019-01-09 14:56:56 -0600

    Add exercise 2

--- ex2/ex2.c
+++ ex2/ex2.c
@@ -2,13 +2,48 @@
 // and then calls `fork()` to create a new process. Can both the child and parent access the file descriptor 
 // returned by `fopen()`? What happens when they are written to the file concurrently?
 
+// 1. Can both the child and parent access the file descriptor returned by `fopen()`? 
+// Yes, both child and parent had access since you can write to fp.
+// 2. What happens when they are written to the file concurrently?
+// They both get saved.
+
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
 
 int main(void)
 {
-    // Your code here 
-    
-    return 0;
+  // Your code here 
+  FILE *fp;
+  // char str[50];
+  fp = fopen("text.txt", "w");
+  printf( "===Opening the file in write mode===\n");
+
+  if (fp == NULL)
+  {
+    printf("Error opening file!\n");
+    exit(1);
+  }
+
+  int rc = fork();
+  if (rc < 0) { // fork failed; exit
+    fprintf(stderr, "fork failed\n");
+    exit(1);
+  }
+  else if (rc == 0) { // child process satisfies this branch
+    // printf("Enter a name (Child) \n");
+    // scanf("%s", str);
+    fprintf(fp, "%s \n", "child");
+    printf("===Finished writing to file (Child)===\n");
+  }
+  else {
+    // printf("Enter a name (Parent) \n");
+    // scanf("%s", str);
+    fprintf(fp, "%s \n", "parent");
+    printf("===Finished writing to file (Parent)===\n");
+  }
+
+  printf("===Closing the file===\n");
+  fclose(fp);
+  return 0;
 }
