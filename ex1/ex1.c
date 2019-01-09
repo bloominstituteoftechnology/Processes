23715a88b51c46f6123857b70ba2b1c4505c6499
James Cassidy <cassidyjamesw@gmail.com>
2019-01-09 18:13:49 -0500

    ex1

--- ex1/ex1.c
+++ ex1/ex1.c
@@ -9,6 +9,28 @@
 int main(void)
 {
     // Your code here
+    int x = 100;
+    printf("x value before calling fork: %d \n", x);
+
+    int rc = fork();
+
+    if (rc < 0)
+    {
+        fprintf(stderr, "fork failed\n");
+        exit(1);
+    }
+    else if (rc == 0)
+    {
+        x = 200;
+        printf("hello, child here (pid: %d) \n", (int)getpid());
+        printf("x value in child when changed: %d \n", x);
+    }
+    else
+    {
+        x = 300;
+        printf("hello, parent here (pid: %d) of child %d \n", (int)getpid(), rc);
+        printf("x value in parent when changed: %d \n", x);
+    }
 
     return 0;
 }
