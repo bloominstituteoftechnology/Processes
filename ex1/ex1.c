08f2deca213033a46c9e1b0da0adb75347cc264d
Lucas Beemer <lucasbeemer@gmail.com>
2019-01-09 15:58:15 -0800

    intial commit, pull

--- ex1/ex1.c
+++ ex1/ex1.c
@@ -1,5 +1,8 @@
 // Write a program that calls `fork()`. Before calling `fork()`, have the main process access a variable
-// (e.g., x) and set its value to something (e.g., 100). What value is the variable in the child process?
+// (e.g., x) and set its value to something (e.g., 100). 
+
+// What value is the variable in the child process?
+
 // What happens to the variable when both the child and parent change the value of x?
 
 #include <stdio.h>
