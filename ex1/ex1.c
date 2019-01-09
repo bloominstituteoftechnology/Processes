335fefff6ab23747f86ac41b48148215dc79de03
Cameron Ray <cameronray@Camerons-MacBook-Pro.local>
2019-01-09 15:28:54 -0800

    ex1 complete

--- ex1/ex1.c
+++ ex1/ex1.c
@@ -9,6 +9,21 @@
 int main(void)
 {
     // Your code here
+    int x = 100;
+
+    printf("Before fork, pid is %d\n", (int) getpid());
+
+    int rc = fork();
+
+    if (rc == 0) {
+        printf("Child: pid is %d and x is %d\n", (int) getpid(), x);
+        x++;
+        printf("Child after increment: pid is %d and x is %d\n", (int) getpid(), x);
+    } else if (rc > 0) {
+        printf("Parent: pid is %d and x is %d\n", (int) getpid(), x);
+        x++;
+        printf("Parent after increment: pid is %d and x is %d\n", (int) getpid(), x);
+    }
 
     return 0;
 }
