92ef9b0081aa6b6f19b9b7b07819025988b2f487
Jonathan T. Miles <jonathan.t.miles@gmail.com>
2019-01-09 14:23:21 -0800

    finished messing with exec() calls

--- ex4/ex4.c
+++ ex4/ex4.c
@@ -1,7 +1,9 @@
 // Write a program that calls `fork()` and then calls some form of `exec()`
-// to run the program `/bin/ls`. Try a few variants of `exec()`, such as 
-// `execl()`, `execle()`, `execv()`, and others. Why do you think there 
+// to run the program `/bin/ls`. Try a few variants of `exec()`, such as
+// `execl()`, `execle()`, `execv()`, and others. Why do you think there
 // are so many variants of the same basic call?
+
+// > there are a few different ways to execute files; probably some are more conventional for some languages or environments than others.
 
 #include <stdio.h>
 #include <unistd.h>
@@ -10,7 +12,17 @@
 
 int main(void)
 {
-    // Your code here    
+    // Your code here
+    fork();
+
+    // char *environ = "environment key";
+
+    char *args[2];
+    args[0] = "ls";
+    args[1] = NULL;
+
+    execv("/bin/ls", args);
+
 
     return 0;
 }
