0973e063edf7ebf144dbc7412443ddcf2deb2cdd
Csillag61 <csillagok@gmail.com>
2019-01-09 17:32:17 -0500

    child and parent with fork

--- ex3/ex3.c
+++ ex3/ex3.c
@@ -2,14 +2,32 @@
 // the parent process should print "goodbye". You should ensure that the child
 // process always prints first.
 
+
 #include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
+#include <stdlib.h>
 #include <sys/wait.h>
+#include <string.h>
+
+
 
 int main(void)
+
 {
     // Your code here
+    int rc = fork();
 
+    if (rc < 0) {
+        fprintf(stderr, "fork failed\n");
+        exit(1);
+
+    }else if ( rc == 0){
+        printf("Hello!\n");
+
+    }else {
+        wait(NULL);
+        printf("Bye, bye!\n");
+    }
     return 0;
+    
 }
