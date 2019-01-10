5db43785f524fd2cc3a86f0892f393cfe6239051
iAmAdamReid <adam@adamreid.me>
2019-01-10 15:21:13 -0800

    Finished ex5, ex6

--- ex5/ex5.c
+++ ex5/ex5.c
@@ -17,6 +17,30 @@ char* msg3 = "hello world #3";
 int main(void)
 {
     // Your code here
-    
+    char inbuf[MSGSIZE]; // our buffer that holds incoming data to be written
+    int p[2]; // a two-element array that holds read and read write descriptors
+
+    if(pipe(p) < 0){
+        fprintf(stderr, "Pipe initialization failed.\n");
+        exit(1);
+    } else {
+        int forked = fork();
+        if(forked < 0){
+            fprintf(stderr, "Fork attempt failed.\n");
+            exit(1);
+        } else if(forked == 0){
+            // if fork creates child process, write messages inside pipe during child process
+            printf("Child process writing...\n");
+            write(p[1], msg1, MSGSIZE); // we use p[1] for each since they are write messages. p[0] is our read buffer.
+            write(p[1], msg2, MSGSIZE);
+            write(p[1], msg3, MSGSIZE);
+        } else {
+            for(int i = 0; i < 3; i++){ // begin the read process once child process completes
+                read(p[0], inbuf, MSGSIZE);
+                printf("%s\n", inbuf);
+            }
+        }
+    }
+
     return 0;
 }
--- ex6/ex6.c
+++ ex6/ex6.c
@@ -11,6 +11,8 @@ turns out to only be the case for OSX versions < 10.12. Anything later than that
 and `clock_gettime()` should work just fine. 
 */
 
+// This resource expands on what the timespec struct is doing: https://www.gnu.org/savannah-checkouts/gnu/libc/manual/html_node/Elapsed-Time.html
+
 #include <stdio.h>
 #include <unistd.h>
 #include <time.h>
@@ -21,6 +23,24 @@ and `clock_gettime()` should work just fine.
 int main()
 {
     // Your code here
+
+    uint64_t diff;
+
+    struct timespec start, end;
+
+    long long unsigned int elapsed_time = 0; // initialize time to 0
+
+    for(int i = 0; i < number_iter; i++){ // find total time needed over 1 million iterations
+        clock_gettime(CLOCK_MONOTONIC, &start); // write start time to start
+        write(0, "", 0); // write an empty call
+        clock_gettime(CLOCK_MONOTONIC, &end); // write end time to end
+        diff = BILLION * (end.tv_sec - start.tv_sec) + end.tv_nsec - start.tv_nsec; // use billion to refactor into nanoseconds
+        elapsed_time += diff;
+    }
+
+    long long unsigned int average_time = elapsed_time/number_iter; // find the average time for each iteration
+
+    printf("Average time = %llu nanoseconds\n", average_time);
     
     return 0;
 }
