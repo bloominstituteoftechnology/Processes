16163b59f8b53ce9eff157be130d11b70d68c7e8
Andrew Liao <andrewoliao@gmail.com>
2019-01-10 17:46:15 -0500

    Complete ex3

--- ex3/ex3.c
+++ ex3/ex3.c
@@ -9,7 +9,17 @@
 
 int main(void)
 {
-    // Your code here
+    int rc = fork();
+
+    if (rc < -1){
+    	exit(1);
+    } else if (rc == 0){
+    	printf("Hello\n");
+    } else {
+    	waitpid(rc, NULL, 0);
+    	printf("Goodbye\n");
+    }
+
 
     return 0;
 }
