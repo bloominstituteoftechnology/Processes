466612410a71113a83fe9198856fcdf7c74c7a81
Anthony Greb <agreb255@gmail.com>
2019-01-10 17:50:23 -0500

    ex4 working

--- ex4/ex4.c
+++ ex4/ex4.c
@@ -7,11 +7,24 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <sys/wait.h>
+#include <string.h>
 
 int main(void)
 {
     int rc = fork();
-    
+    if (rc < 0) {
+        fprintf(stderr, "Fork Failed\n");
+        exit(1);
+    } else if (rc == 0) {
+        printf("Child process (pid: %d)\n", (int) getpid());
+        char *myargs[2];
+        myargs[0] = strdup("/bin/ls");
+        myargs[1] = NULL;
+        execvp(myargs[0], myargs);
+    } else {
+        waitpid(rc, NULL, 0);
+        printf("Parent process (pid: %d) of child process %d\n", (int) getpid(), rc);
+    }
 
     return 0;
 }
