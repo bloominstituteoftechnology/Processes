e0de2c0ae241c2112aafd12bc1e674caa3b668be
Nicocchi <nicocchilover@gmail.com>
2019-01-10 11:38:55 -0500

    updated ex2

--- ex2/ex2.c
+++ ex2/ex2.c
@@ -2,10 +2,8 @@
 // and then calls `fork()` to create a new process. Can both the child and parent access the file descriptor
 // returned by `fopen()`? What happens when they are written to the file concurrently?
 //
-// The child does not have access to the file.
-// When writing to the files concurrently, the child takes precedence over the parent.
-// I assume this is because the child process is called last, so whatever is being written from
-// the child to the file will overwrite what was written from the parent to the file.
+// The child does have access to the file and both the parent and child do
+// write to the file without overwritting.
 
 #include <stdio.h>
 #include <unistd.h>
@@ -14,52 +12,27 @@
 
 int main(void)
 {
-    FILE *fp;
+  FILE *fp = fopen("text.txt", "w");
+  pid_t rc = fork();
 
-    fp = fopen("text.txt", "r");
-
-    pid_t pid = fork();
-
-    Check if file exists, otherwise, output error
-  if (fp == NULL) {
-		printf("Failed To Open File\n");
-		return 0;
-	}
-
-  if (pid == 0) {
-    printf("Child\n");
-    int c;
-
-    while(1) {
-      c = fgetc(fp);
-      if (feof(fp)) {
-        break;
-      }
-      printf("%c", c);
-    }
-    fclose(fp);
-
-    fp = fopen("text.txt", "w+");
-    fprintf(fp, "%s %s %s %d", "We", "are", "in", 2021);
-    fclose(fp);
-
-  } else {
-    printf("Parent\n");
-    int c;
-
-    while(1) {
-      c = fgetc(fp);
-      if (feof(fp)) {
-        break;
-      }
-      printf("%c", c);
-    }
-    fclose(fp);
-
-    fp = fopen("text.txt", "w+");
-    fprintf(fp, "%s %s %s %d", "We", "are", "in", 2020);
-    fclose(fp);
+  if(rc < 0)
+  {
+    fprintf(stderr, "fork failed\n");
+    exit(1);
   }
+  else if(rc == 0)
+  {
+    fputs("Hello from child.\n", fp);
+    // fprintf(fp, "%s %s %s %s", "Hello", "from", "child.");
+
+  }
+  else
+  {
+    fputs("Hello from parent.\n", fp);
+  }
+  fclose(fp);
+
+  return 0;
 
 
 
