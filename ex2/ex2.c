2ab6f194485081c4f8479d6aac1d04086ac44839
Luisan327 <luisangelsc@gmail.com>
2019-01-10 21:11:52 -0600

    ex2.c

--- ex2/ex2.c
+++ ex2/ex2.c
@@ -8,7 +8,26 @@
 
 int main(void)
 {
-    // Your code here 
-    
+    FILE *file;
+
+    file = fopen("text.txt", "r+");
+
+    int rc = fork();
+
+    if (rc < 0) {
+        fprintf(stderr, "fork did not succeed\n");
+        exit(1);
+    } else if (rc == 0) {
+        printf("hello, I am child (pid: %d)\n", (int) getpid());
+        fprintf(file, "CHILD: I'm a rebel leaving my mark\n");
+        //run something on child process
+
+    } else {
+        printf("hello, I am parent of %d (pid: %d)\n", rc, (int) getpid());
+        fprintf(file, "I'm a parent, picking up after my child -_-\n");
+        //run something on parent process
+    }
+    fclose(file);
+
     return 0;
 }
