c5cfab8aa67dd3a688b825b6f7a467090a1790eb
Luisan327 <luisangelsc@gmail.com>
2019-01-10 21:50:19 -0600

    ex4.c

--- ex4/ex4.c
+++ ex4/ex4.c
@@ -8,9 +8,26 @@
 #include <stdlib.h>
 #include <sys/wait.h>
 
-int main(void)
+int main(int argc, char *argv[])
 {
-    // Your code here    
+    int rc = fork();
+    if (rc < 0) {
+        fprintf(stderr, "fork did not succeed\n");
+        exit(1);
+    } else if (rc == 0) {
+        printf("CHILD (pid: %d) \n", (int) getpid());
+        char *myargs[2];    // allocate an array of chars to hold 3 bytes
+        // `strdup` duplicates the given input string
+        myargs[0] = "/bin/ls";      // pass the name of the program we want to run as the first array element 
+        myargs[1] = NULL;              // end of the array
+        //execvp(myargs[0], myargs);
+        execlp("/bin/ls", "ls", (char *) 0);
+
+        printf("this will not be seen, I still don't know why\n");
+    } else {
+        int wc = waitpid(rc, NULL, 0);    // `waitpid` call added here
+        printf("PARENT (pid: %d) of child %d\n", (int) getpid(), rc);
+    }
 
     return 0;
 }
