4a5da2bcdf62b492b80346298ff54d73cf85889b
Andrew Liao <andrewoliao@gmail.com>
2019-01-10 19:13:24 -0500

    Complete ex5

--- ex5/ex5.c
+++ ex5/ex5.c
@@ -16,7 +16,28 @@ char* msg3 = "hello world #3";
 
 int main(void)
 {
-    // Your code here
+		int p[2];
+		char buff[256];
+		
+		if(pipe(p) < 0){
+			exit(1);
+		}
+		int rc = fork();
+
+		if (rc < 0){
+			exit(1);
+		} else if (rc == 0){
+			write(p[1], msg1, MSGSIZE);
+    	write(p[1], msg2, MSGSIZE);
+    	write(p[1], msg3, MSGSIZE);
+		} else {
+			
+			for (int i = 0; i < 3; i++){
+				read(p[0], buff, MSGSIZE);
+				printf("%s\n", buff);
+			}
+		}
+
     
     return 0;
 }
