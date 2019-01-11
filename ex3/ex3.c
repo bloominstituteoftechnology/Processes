d6dcb56208af65d871e255e6b8e2a41e116bca40
Luisan327 <luisangelsc@gmail.com>
2019-01-10 21:18:01 -0600

    ex3.c

--- ex3/ex3.c
+++ ex3/ex3.c
@@ -10,6 +10,20 @@
 int main(void)
 {
     // Your code here
+    int rc = fork();
+
+    if (rc < 0) {
+        fprintf(stderr, "fork did not succeed\n");
+        exit(1);
+    } else if (rc == 0) {
+        printf("hello\n");
+        //run something on child process
+    } else {
+        //run something on parent process
+        int wc = waitpid(rc, NULL, 0);
+        printf("goodbye\n");
+
+    }
 
     return 0;
 }
