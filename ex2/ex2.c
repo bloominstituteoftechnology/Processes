d5cea5468d8c4c30f6ddae40470738977a2af089
cubsfan46350 <cubsfan.46350@msn.com>
2019-01-15 18:53:28 -0600

    completed example 2

--- ex2/ex2.c
+++ ex2/ex2.c
@@ -9,6 +9,31 @@
 int main(void)
 {
     // Your code here 
+    FILE *t_file;
+    int x_file = fork();
+    char filename[100], c;
+
+    if (x_file < 0) {
+        printf("The Fork Failed\n");
+    }
+    else {
+        if (x_file == 0) {
+            printf("I am the child process\n");
+        }
+        printf("Opening Text File\n");
+        t_file = fopen("./text.txt", "r");
+        if (t_file == NULL) {
+            printf("Cannot open file \n");
+            exit(0);
+        }
+    }
+    c = fgetc(t_file);
+    while (c != EOF) {
+        printf("%c", c);
+        c = fgetc(t_file);
+    }
     
+    printf("Closing File\n");
+    fclose(t_file);
     return 0;
 }
