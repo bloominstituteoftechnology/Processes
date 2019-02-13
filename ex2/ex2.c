6804dbb2609f35c6f84b11399c7b4446b0e078be
Vuk Radosavljevic <vukrado1@gmail.com>
2019-02-13 16:55:51 -0600

    ex2

--- ex2/ex2.c
+++ ex2/ex2.c
@@ -6,9 +6,40 @@
 #include <unistd.h>
 #include <stdlib.h>
 
+
+
+#define MAXCHAR 1000
+
 int main(void)
 {
     // Your code here 
+    int rc = fork();
     
+    FILE *fp;
+    char *fileName = "text.txt";
+    char str[MAXCHAR];
+
+    fp = fopen(fileName, "r");
+    
+
+    if (rc < 0) {    // fork failed; exit
+        fprintf(stderr, "fork failed\n");
+        exit(1);
+    } else if (rc == 0) {    // child process satisfies this branch
+        printf("hello, child here (pid: %d) \n", (int) getpid());
+    } else {
+        printf("hello, parent here (pid: %d) of child %d\n", (int) getpid(), rc);
+    }
+
+    if (fp == NULL) {
+        printf("Could not open file %s", fileName);
+        return 1;
+    }
+
+    while (fgets(str, MAXCHAR, fp) != NULL) {
+        printf("%s \n", str);
+    
+    fclose(fp);
     return 0;
+    }
 }
