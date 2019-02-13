0933069a089fc2c0a386cd9fb1605e7aac184786
Drew <drewsup123c@gmail.com>
2019-02-13 14:21:51 -0700

    prettified code

--- ex1/ex1.c
+++ ex1/ex1.c
@@ -13,7 +13,7 @@ int main(void)
     int x = 100;
     printf("%d/n", x);
     int rc = fork();
-        if (rc < 0) {    // fork failed; exit
+    if (rc < 0) {    // fork failed; exit
         fprintf(stderr, "fork failed\n");
         exit(1);
     } else if (rc == 0) {    // child process satisfies this branch
