5360ab998384b8e6065ae089e4402e975c4d9c82
lndubose <lndubose13@gmail.com>
2019-02-13 17:12:25 -0600

    Manipulate code for second exercise

--- ex2/ex2.c
+++ ex2/ex2.c
@@ -3,9 +3,9 @@
 // returned by `fopen()`? What happens when they are written to the file concurrently?
 
 /*
-It seems to me that they can. And they will be the same even if you call within their scopes. 
-I also printed out the pointer and they are the same as well. What actually suprised me the most
-was how it printed to the terminal.
+Ok interesting stuff. I would like to talk to Sean more about this tomorrow. So I found that they have the same
+file descriptor and pointer. I also found that you can overwrite the file depending where the fork is in the file.
+I am not really sure what the hope was to see for the second question.
 */
 
 #include <stdio.h>
@@ -16,15 +16,16 @@ int main(void)
 {
     // Your code here
     FILE *fp;
-    fp = fopen("text.txt", "r");
+    // int rc = fork(); Having fork here will overwrite th file
+    fp = fopen("text.txt", "w");
     if (fp == NULL)
     {
         printf("Could not open the file for text.txt");
         exit(1);
     }
     int fileNum;
-    // fileNum = fileno(fp);
-    // printf("Pointer: %d\n", fileNum);
+    fileNum = fileno(fp);
+    printf("Pointer: %d\n", fileNum);
 
     int rc = fork();
     if (rc < 0)
@@ -34,15 +35,16 @@ int main(void)
     }
     else if (rc == 0)
     {
-        fileNum = fileno(fp);
+        char *txt = "Child.";
+        fprintf(fp, "Look it is %s", txt);
+
         printf("Child descriptor: %d\n", fileNum);
-        printf("Child pointer: %p\n", fp);
     }
     else
     {
-        fileNum = fileno(fp);
+        char *txt = "Parent.";
+        fprintf(fp, "Look it is %s", txt);
         printf("Parent descriptor: %d\n", fileNum);
-        printf("Parent pointer: %p\n", fp);
     }
 
     fclose(fp);
