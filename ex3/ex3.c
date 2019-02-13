9d21195ecf24db50e4bb30574b6a4e84b34afa59
lajawanti <myemail>
2019-02-13 17:41:49 -0500

    ex3.c

--- ex3/ex3.c
+++ ex3/ex3.c
@@ -11,5 +11,20 @@ int main(void)
 {
     // Your code here
 
+    //printf("..%d \n", (int)getpid());
+
+    int s = fork();
+
+    if(s == 0){
+         printf("hello   %d \n", (int)getpid());
+    } else{
+         wait(NULL);
+         printf("goodbye  %d \n", (int)getpid());
+    }
+
+
     return 0;
 }
+    
+    
+    
