8f8a987c66e97b1e3d04397306385fa40e95c9a5
Conner Alegre <cga0@pm.me>
2019-02-13 14:29:01 -0700

    Complete ex4.c

--- ex4/ex4.c
+++ ex4/ex4.c
@@ -10,7 +10,21 @@
 
 int main(void)
 {
-    // Your code here    
-
+    int child_process = fork();
+    if (child_process < 0)
+    {
+        fprintf(stderr, "Failure to fork!\n");
+        exit(1);
+    } 
+    else if (child_process == 0)
+    {
+        // char *argv[] = {"ls", NULL};
+        // execvp("/bin/ls", argv);
+        execl("/bin/ls", "ls", (char *)0);
+    }
+    else
+    {
+        int wc = waitpid(child_process, NULL, 0);
+    }
     return 0;
 }
