eae5110dee16798e47e82885e05ce6881cb985f0
Katie Cleary <katiecleary@airmaster.lan>
2019-02-13 15:39:13 -0500

    initial push

--- ex1/ex1.c
+++ ex1/ex1.c
@@ -1,4 +1,5 @@
-// Write a program that calls `fork()`. Before calling `fork()`, have the main process access a variable
+// Write a program that calls `fork()`. Before calling `fork()`, 
+// have the main process access a variable
 // (e.g., x) and set its value to something (e.g., 100). What value is the variable in the child process?
 // What happens to the variable when both the child and parent change the value of x?
 
