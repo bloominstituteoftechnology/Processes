c5c52293e05052cde7646b2ec954460ed020cac2
Hunter <huntersmith_15@hotmail.com>
2019-02-13 14:05:27 -0700

    ex4 complete

--- ex4/ex4.c
+++ ex4/ex4.c
@@ -7,10 +7,20 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <sys/wait.h>
+#include <string.h>
 
 int main(void)
 {
     // Your code here    
-
+    int child = fork();
+    if (child == 0) {
+        char *arg[2];
+        arg[0] = strdup("/bin/ls");
+        arg[1] = NULL;
+        execv(arg[0], arg);
+    } else {
+        waitpid(child, NULL, 0);
+        printf("PArent running here\n");
+    }
     return 0;
 }
