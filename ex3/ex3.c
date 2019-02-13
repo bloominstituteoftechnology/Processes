f329a4c22359e8a58f53d6a226c6e86f1203ebc7
stdemicheli <stdemicheli@gmail.com>
2019-02-14 00:23:25 +0100

    Completes ex3.

--- ex3/ex3.c
+++ ex3/ex3.c
@@ -9,7 +9,20 @@
 
 int main(void)
 {
-    // Your code here
-
+    
+    int rc = fork();
+    
+    if (rc < 0) {
+        fprintf(stderr, "fork failed\n");
+        exit(1);
+    } else if (rc == 0) {
+        printf("hello, child here (pid: %d) \n", (int) getpid());
+        printf("hello\n");
+    } else {
+        int wc = waitpid(rc, NULL, 0);
+        printf("hello, parent here (pid: %d) of child %d\n", (int) getpid(), rc);
+        printf("goodbye\n");
+    }
+    
     return 0;
 }
