4f4ba693ddca1eedc7e3f1104d9f9933e3634b4d
Drew <drewsup123c@gmail.com>
2019-02-13 14:40:02 -0700

    finished ex2

--- ex2/ex2.c
+++ ex2/ex2.c
@@ -8,7 +8,26 @@
 
 int main(void)
 {
-    // Your code here 
+    FILE *file = fopen("./text.txt", "w");
+    // the w in this will reset the file everytime (at least that is what documentation says)
+    if(file == NULL){
+        printf("The file was not opened!");
+    }
+    // Above if statement will print if file was not opened 
+
+    int rc = fork();
+    if (rc < 0) {    // fork failed; exit
+        fprintf(stderr, "fork failed\n");
+        exit(1);
+    } else if (rc == 0) {    // child process satisfies this branch
+        printf("hello, child here (pid: %d) \n", (int) getpid());
+        fprintf(file, "Hello world!!!");
+    } else {
+        printf("hello, parent here (pid: %d) of child %d\n", (int) getpid(), rc);
+        fprintf(file, "Hello world!!!");
+    }
     
     return 0;
 }
+
+// two hello worlds appear in the file one when rc == 0 and another in the else statement
