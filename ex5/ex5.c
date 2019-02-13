e7248117646d6b323197d008fa4d0d35117bb5ba
Ryan Walker <rytwalker@gmail.com>
2019-02-13 17:40:04 -0500

    ex5 firstpass

--- ex5/ex5.c
+++ ex5/ex5.c
@@ -1,7 +1,7 @@
 // Write a program that forks a child and creates a shared pipe
-// between the parent and child processes. Have the child write 
-// the three messages to the parent and have the parent print out 
-// the messages. 
+// between the parent and child processes. Have the child write
+// the three messages to the parent and have the parent print out
+// the messages.
 
 #include <stdio.h>
 #include <unistd.h>
@@ -10,13 +10,34 @@
 
 #define MSGSIZE 16
 
-char* msg1 = "hello world #1";
-char* msg2 = "hello world #2";
-char* msg3 = "hello world #3";
+char *msg1 = "hello world #1";
+char *msg2 = "hello world #2";
+char *msg3 = "hello world #3";
 
 int main(void)
 {
     // Your code here
-    
+    int child = fork();
+    char inbuf[MSGSIZE];
+    int p[2];
+    pipe(p);
+    if (child == 0)
+    {
+        write(p[1], msg1, MSGSIZE);
+        write(p[1], msg2, MSGSIZE);
+        write(p[1], msg3, MSGSIZE);
+        printf("messages written...\n");
+    }
+    else
+    {
+        waitpid(child, NULL, 0);
+        for (int i = 0; i < 3; i++)
+        {
+            // read 16 bytes of data from the read file descriptor
+            read(p[0], inbuf, MSGSIZE);
+            printf("%s\n", inbuf);
+        }
+    }
+
     return 0;
 }
