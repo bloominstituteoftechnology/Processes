26657b20b26d1550d9c174fc378ad09a8ad5ad78
Eva <evalee904@gmail.com>
2019-02-14 15:16:42 -0600

    ex4

--- ex4/ex4.c
+++ ex4/ex4.c
@@ -1,6 +1,6 @@
 // Write a program that calls `fork()` and then calls some form of `exec()`
-// to run the program `/bin/ls`. Try a few variants of `exec()`, such as 
-// `execl()`, `execle()`, `execv()`, and others. Why do you think there 
+// to run the program `/bin/ls`. Try a few variants of `exec()`, such as
+// `execl()`, `execle()`, `execv()`, and others. Why do you think there
 // are so many variants of the same basic call?
 
 #include <stdio.h>
@@ -10,7 +10,26 @@
 
 int main(void)
 {
-    // Your code here    
+    int child = fork();
+
+    if (child < 0)
+    {
+        printf("\nFork failed\n");
+    }
+    else if (child == 0)
+    {
+        char *args[2];
+        args[0] = "/bin/ls"; // list files and dirs
+        args[1] = NULL;
+
+        execvp(args[0], args);
+        printf("\nCan't be seen\n");
+    }
+    else
+    {
+        int wc = waitpid(child, NULL, 0);
+        printf("\nParent\n");
+    }
 
     return 0;
 }
