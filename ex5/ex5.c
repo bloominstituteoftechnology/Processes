03b07a4d5be91da781a813333e36d01ee2cc2faf
Vuk Radosavljevic <vukrado1@gmail.com>
2019-02-14 17:12:08 -0600

    ex5

--- ex5/ex5.c
+++ ex5/ex5.c
@@ -17,6 +17,31 @@ char* msg3 = "hello world #3";
 int main(void)
 {
     // Your code here
-    
+    char inbuf[MSGSIZE];
+    int p[2];
+
+    if (pipe(p) < 0) {
+        fprintf(stderr, "pipe failed\n");
+        exit(1);
+    }
+
+
+    pid_t rc = fork();
+
+    if (rc < 0) {    // fork failed; exit
+        fprintf(stderr, "fork failed\n");
+        exit(1);
+    } else if (rc > 0) {
+        for (int i = 0; i < 3; i++) {
+            read(p[0], inbuf, MSGSIZE);
+            printf("%s\n", inbuf);
+        }
+    } else {
+        write(p[1], msg1, MSGSIZE);
+        write(p[1], msg2, MSGSIZE);
+        write(p[1], msg3, MSGSIZE);
+    }
+
+
     return 0;
 }
