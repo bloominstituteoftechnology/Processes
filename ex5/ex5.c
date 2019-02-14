62ee8afafea818f1c34cd8257c9d518f1ab975ab
stdemicheli <stdemicheli@gmail.com>
2019-02-14 23:20:12 +0100

    Completes ex4, ex5.

--- ex5/ex5.c
+++ ex5/ex5.c
@@ -16,7 +16,29 @@ char* msg3 = "hello world #3";
 
 int main(void)
 {
-    // Your code here
     
+    char inbuf[16];
+    int p[2];
+    
+    if (pipe(p) < 0) {
+        fprintf(stderr, "pipe failed\n");
+        exit(1);
+    }
+    
+    int rc = fork();
+    
+    
+    if (rc < 0) {
+        fprintf(stderr, "fork failed\n");
+        exit(1);
+    } else if (rc == 0) {
+        //int wc = waitpid(rc, NULL, 0);
+        printf("hello, child here (pid: %d) \n", (int) getpid());
+        write(p[1], "Hi there", 16);
+    } else {
+        printf("hello, parent here (pid: %d) of child %d\n", (int) getpid(), rc);
+        read(p[0], inbuf, 16);
+        printf("%s\n", inbuf);
+    }
     return 0;
 }
