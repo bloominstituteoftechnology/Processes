bd8315844e18abc6f6c40976c4e78a60912fd032
Kat Fries <katherynejf@gmail.com>
2019-02-20 18:31:10 -0500

    ex4 done. I was only able to get files to print out with execvp

--- ex4/ex4.c
+++ ex4/ex4.c
@@ -10,7 +10,45 @@
 
 int main(void)
 {
-    // Your code here    
+    pid_t pid;   
+    pid = fork();  
+    char *argv[3] = {"/bin/ls", ".", NULL}; // execvp( "find", argv );
 
+    if (pid == 0) {
+        execvp( "find", argv );
+        //  ./ex4
+        //  ./ex4.c
+
+    } else {
+        printf("Parent says hello\n");
+    }
     return 0;
 }
+
+/*
+https://ece.uwaterloo.ca/~dwharder/icsrts/Tutorials/fork_exec/
+"The exec Family of Functions
+There is a family of exec() functions, all of which have slightly different characteristics:
+
+       int execl ( const char *path, const char *arg, ... );
+       int execlp( const char *file, const char *arg, ... );
+       int execle( const char *path, const char *arg, ..., char *const envp[] );
+       int execv ( const char *path, char *const argv[] );
+       int execvp( const char *file, char *const argv[] );
+       int execve( const char *file, char *const argv[], char *const envp[] );
+Each system call is the word exec followed by either l or v and then possibly followed by either e or p.
+
+The first three have are of the form execl and accept a variable number of arguments. In order to use this feature, you must load the <stdarg.h> header file. Please see the example stdarg.c.
+
+The latter three are of the form execv in which case the arguments are passed using an array of pointers to strings where the last entry is NULL. For example, you might have:
+        char *argv[] = {"Hello ", "world!", NULL};
+
+If the name ends in either l or v, the program name must be given in full.
+
+If the name is appended by a p, it will search for the file using the current environment variable PATH, which usually includes /bin/, /usr/bin/, etc.
+
+Finally, if the name is appended by a e, one can include an array of strings indicating environment variables, each of the form "ENVVAR=value" and the array being null terminated. For example,
+       char *envp[] = {"USER=dwharder", "HOME=/home/dwharder", NULL};
+"
+
+*/
