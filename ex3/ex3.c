6540ddd690d1759ca945d6c3cc815eab0498a8e5
Evan Carlstrom <evan.carlstrom@gmail.com>
2019-02-20 17:21:09 -0500

    reviewing some ex3 changes to learn more about the effects of waitpid()

--- ex3/ex3.c
+++ ex3/ex3.c
@@ -11,15 +11,19 @@ int main(void)
 {
     // Your code here
     pid_t pid = fork();
+    waitpid(pid, NULL, 0);
 
     if(pid == 0) {
         printf("%s", "hello\n");
-        wait(NULL);
+        // waitpid(pid, NULL, 0);
     }
     else {
         printf("%s", "goodbye\n");
-        waitpid(pid, NULL, 1);
+        // waitpid(pid, NULL, 0);
+        
     }
 
     return 0;
 }
+
+/* currently testing out the results of implementing waitpid() in various spots in the code */
