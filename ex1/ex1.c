ddf125696143c15e4fea996782d0f6efa1a6b75b
Kat Fries <katherynejf@gmail.com>
2019-02-20 16:48:26 -0500

    re: ex1 changed variable within parent and removed wait

--- ex1/ex1.c
+++ ex1/ex1.c
@@ -1,12 +1,16 @@
 // Write a program that calls `fork()`. Before calling `fork()`, have the main process access a variable
 // (e.g., x) and set its value to something (e.g., 100). What value is the variable in the child process?
 // What happens to the variable when both the child and parent change the value of x?
-// init is 1
+
+
+
+
+// init is PID 1
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>             // required for fork()
-#include <sys.wait.h>           // required for wait()
+// #include <sys.wait.h>           // required for wait()
                                 // when sys.wait wasn't added it could run in different orders
 int main(void) {
 
@@ -16,13 +20,24 @@ int main(void) {
 
     if (pid == 0) {
         x = 7;
-        printf("%d\n", x);
+        printf("%d\n", x);      // x = 7
     } else {
-        printf("%d\n", x);
-        wait(NULL); // if this is commentted result will be different.
-        printf("%d\n", x);
+        printf("%d\n", x);      // x = 100
+        x = 10;
+        // wait(NULL); // if this is commentted result will be different.
+        printf("%d\n", x);      // x = 10
     }
+/*
+Order run:
+100
+10
+7
 
+But without wait it could be:
+7
+100
+10
+*/
 
   return 0;
 }
