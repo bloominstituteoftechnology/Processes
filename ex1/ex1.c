05f5f16dc7570828b3c6bb602a3eeef3e700a9ef
AM_Myrick <amyrick.820@gmail.com>
2019-02-20 15:09:12 -0500

    answers fork question

--- ex1/ex1.c
+++ ex1/ex1.c
@@ -2,6 +2,9 @@
 // (e.g., x) and set its value to something (e.g., 100). What value is the variable in the child process?
 // What happens to the variable when both the child and parent change the value of x?
 
+// In the program below, the parent process decrements and the child process increments. Because they are two separate processes running the 
+// code independently, x = 99 for the parent and x = 101 for the child.
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
