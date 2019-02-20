f5fbc886c8247a2533acca94ba854794a54418f0
Evan Carlstrom <evan.carlstrom@gmail.com>
2019-02-20 15:16:09 -0500

    ex1 code and questions complete

--- ex1/ex1.c
+++ ex1/ex1.c
@@ -9,6 +9,19 @@
 int main(void)
 {
     // Your code here
-    // initial commit
+    int x = 100;
+    int pid = fork();
+
+    if(pid == 0) {
+        printf("Hello, I'm a child process!\n");
+    }
+    else {
+        printf("Hello, I'm a parent process!\n");
+    }
+    printf("%d %d", 100, x);
+
     return 0;
 }
+
+/* After initialization, the variable in the child process would be the same as its value in the parent process. When the parent and child change the value of x, we can access both values of x within their respective forks.
+If we were to do something like (x * 2) in the parent fork and (x / 2) in the child fork, we could work with both 200 and 50. */
