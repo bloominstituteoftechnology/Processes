e04e66095a1191cd622122b7d22162c977dddee7
luiscmartinez <mrluismartinezzz@gmail.com>
2019-02-20 13:13:21 -0800

    completed ex1 & ex2

--- ex1/ex1.c
+++ ex1/ex1.c
@@ -9,6 +9,24 @@
 int main(void)
 {
   // Your code here
+  int x = 800;
+  printf("Initial value %d\n", x);
+
+  pid_t pid = fork();
+
+  if (pid < 0)
+  {
+    exit(1);
+  }
+  if (pid == 0)
+  {
+    printf("value of x child process, (x:%d) (pid:%d)\n", x, getpid());
+  }
+  else
+  {
+    x = 100;
+    printf("value of x parent process %d\n", x);
+  }
 
   return 0;
 }
