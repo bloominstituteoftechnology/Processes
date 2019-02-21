0e59526341950ec7e99e09f6b80254ad5b7ac203
Barbara Hunter <commandandcode@gmail.com>
2019-02-21 17:21:45 -0500

    ex 3 done

--- ex3/ex3.c
+++ ex3/ex3.c
@@ -10,6 +10,24 @@
 int main(void)
 {
     // Your code here
+    int rc = fork();
+
+    if (rc < 0)
+    {
+        printf(stderr, "Forking failed \n");
+    }
+    else if (rc == 0)
+    {
+
+        printf("Hello there, if rc==0, this is our child processID is: (pid: %d) \n", (int)getpid());
+        printf("Goodbye\n");
+    }
+    else
+    {
+        int wc = waitpid(rc, NULL, 0);
+        printf("Hello there, if rc==0, this is our Parent processID is: (pid: %d) \n", (int)getpid());
+        printf("Hello\n");
+    }
 
     return 0;
 }
