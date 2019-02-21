098350d2de21bc35ac8a48751943e312c7a153dc
IAN CAMERON <iancameron@IANs-MacBook-Pro.local>
2019-02-21 16:48:50 -0700

    finishes pipe

--- ex5/ex5.c
+++ ex5/ex5.c
@@ -18,12 +18,37 @@ int main(void)
 {
     printf("parent %d\n", (int) getpid());
     char inbuffer[MSGSIZE];
-    int p;
+    int p[2];
 
-    if (pipe(p) > 0) 
+    if (pipe(p) < 0) 
     {
         fprintf(stderr, "Pipe failure\n");
         exit(1);
+    }
+    int rc = fork();
+    if (rc < 0) 
+    {
+        fprintf(stderr, "Fork failure\n");
+        exit(2);
+    }
+    else if (rc == 0)
+    {
+        printf("Child writing to pipe\n");
+        write(p[1], msg1, MSGSIZE);
+        write(p[1], msg2, MSGSIZE);
+        write(p[1], msg3, MSGSIZE);
+    }
+    else 
+    {
+        close(p[1]); //close write branch
+        wait(NULL); //not necessary b/c processes can run at same time but doesnt hurt
+        //ssize_t read(int fd, void *buf, size_t count);
+        for (int i = 0; i < 3; i++) 
+        {
+            read(p[0], inbuffer, MSGSIZE);
+            printf("%s\n", inbuffer);
+        }
+        close(p[0]);  //close read
     }
     
     return 0;
