86c5673f9da2917c1c12ff82b04101dcac7abdf4
Evan Carlstrom <evan.carlstrom@gmail.com>
2019-02-21 16:10:37 -0500

    set up basic ex6 code, just need to add writing logic now

--- ex6/ex6.c
+++ ex6/ex6.c
@@ -12,6 +12,7 @@ and `clock_gettime()` should work just fine.
 */
 
 #include <stdio.h>
+#include <stdint.h>
 #include <unistd.h>
 #include <time.h>
 
@@ -21,6 +22,17 @@ and `clock_gettime()` should work just fine.
 int main()
 {
     // Your code here
+    uint64_t diff;
+    struct timespec start, end;
+    int i;
+
+    /* basic structure */
+    clock_gettime(CLOCK_MONOTONIC, &start);
+    sleep(3); /* this line will be replaced by the writing, just using sleep as a placeholder */
+    clock_gettime(CLOCK_MONOTONIC, &end);
+
+    diff = BILLION * (end.tv_sec - start.tv_sec) + end.tv_nsec - start.tv_nsec;
+    printf("Time elapsed: %llu nanoseconds\n", (long long unsigned int) diff);
     
     return 0;
 }
