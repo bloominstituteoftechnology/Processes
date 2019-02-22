b093f4574c2e0f193d957222113a5ddfc9b5b072
SeanOne <basement365@gmail.com>
2019-02-21 18:30:14 -0800

    finished up ex2 | I am not sure the output but I am able to read/write

--- ex2/ex2.c
+++ ex2/ex2.c
@@ -9,6 +9,29 @@
 int main(void)
 {
     // Your code here 
-    
+    FILE *ptr_file;
+    char buf[1000];
+    int rc = fork();
+
+    ptr_file = fopen("./text.txt", "r+");
+    if(!ptr_file) {
+        return 1;
+    }
+
+    while (fgets(buf,1000, ptr_file) != NULL)
+        if(rc < 0) {
+            fprintf(stderr, "unable to fork\n");
+            exit(1);
+        } else if (rc == 0) { // this is the child fork
+            fputs("This has been added from the child process\n", ptr_file);   
+            printf("This is from the child: %s\n", buf);
+            
+        } else { // this is the parent
+            fputs("This has been added from the parent process\n", ptr_file);
+            printf("This is from the Parent: %s\n", buf);    
+        }
+
+    fclose(ptr_file);
+
     return 0;
 }
