ccf657140adbea9380f47dfb4eb678e37239e59a
Barbara Hunter <commandandcode@gmail.com>
2019-03-20 19:20:41 -0400

    ex 3

--- ex3/ex3.c
+++ ex3/ex3.c
@@ -10,6 +10,23 @@
 int main(void)
 {
     // Your code here
+    printf("Before forking pid: %d\n", (int)getpid());
+
+    pid_t pid = fork();
+
+    if (pid < 0)
+    {
+        exit(1);
+    }
+    else if (pid == 0)
+    {
+        printf("AFTER forking CHILD says Hello pid: %d\n", (int)getpid());
+    }
+    else
+    {
+        waitpid(pid, NULL, 0);
+        printf("AFTER forking PARENT says Goodbye pid: %d\n", (int)getpid());
+    }
 
     return 0;
 }
