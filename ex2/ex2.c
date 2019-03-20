a6b1777976e9765353e2402c7070cea34d046a0c
Axolotl <samuelmachat@gmail.com>
2019-03-20 18:01:55 -0400

    ex2

--- ex2/ex2.c
+++ ex2/ex2.c
@@ -1,6 +1,7 @@
-// Write a program that opens the text.txt file (with the `fopen()` library call) located in this directory 
-// and then calls `fork()` to create a new process. Can both the child and parent access the file descriptor 
-// returned by `fopen()`? What happens when they are written to the file concurrently?
+// Write a program that opens the text.txt file (with the `fopen()` library
+// call) located in this directory and then calls `fork()` to create a new
+// process. Can both the child and parent access the file descriptor returned
+// by `fopen()`? What happens when they are written to the file concurrently?
 
 #include <stdio.h>
 #include <unistd.h>
@@ -8,7 +9,14 @@
 
 int main(void)
 {
-    // Your code here 
-    
+    FILE *fp;
+    fp = fopen("text.txt", "w+");
+
+    fork();
+
+    fprintf(fp, "text");
+
+    fclose(fp);
+
     return 0;
 }
