3d0aa4440197a2ecf39425a8da077dc1aa3dd90d
Axolotl <samuelmachat@gmail.com>
2019-03-20 18:18:48 -0400

    ex3

--- ex3/ex3.c
+++ ex3/ex3.c
@@ -1,6 +1,6 @@
-// Write another program using `fork()`. The child process should print "hello";
-// the parent process should print "goodbye". You should ensure that the child
-// process always prints first.
+// Write another program using `fork()`. The child process should print
+// "hello"; the parent process should print "goodbye". You should ensure
+// that the child process always prints first.
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -9,7 +9,17 @@
 
 int main(void)
 {
-    // Your code here
+    int rc = fork();
+
+    if (rc == 0)
+    {
+        printf("hello\n");
+    }
+    else
+    {
+        wait(NULL);
+        printf("goodbye\n");
+    }
 
     return 0;
 }
