59f687bb13a79ac532a7d7f1d1a324cddafefc6f
Clantigua2 <clantigua954@gmail.com>
2019-03-21 18:26:21 -0400

    completed ex5

--- ex5/ex5.c
+++ ex5/ex5.c
@@ -10,13 +10,26 @@
 
 #define MSGSIZE 16
 
-char* msg1 = "hello world #1";
-char* msg2 = "hello world #2";
-char* msg3 = "hello world #3";
+char* msg1 = "hello world #1\n";
+char* msg2 = "hello world #2\n";
+char* msg3 = "hello world #3\n";
 
 int main(void)
 {
     // Your code here
+    pid_t pid = fork();
+    char* messages[sizeof MSGSIZE * 3];
+        messages[0] = msg1;
+        messages[1] = msg2;
+        messages[2] = msg3;
+    
+    if (pid == 0) {
+            for(int i = 0; i < 2; i++) {
+                write(STDOUT_FILENO, messages[i], 128 );
+            }
+    } else {
+        printf("parent stuff");
+    }
     
     return 0;
 }
