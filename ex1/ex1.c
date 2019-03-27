225f561051072f8aceb6837cb5b6bd127ecf3279
Hargobind Atwal <ethaipath@gmail.com>
2019-03-27 13:10:18 -0700

    exploring fork

--- ex1/ex1.c
+++ ex1/ex1.c
@@ -5,10 +5,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 int main(void)
 {
     // Your code here
+    int *x = malloc(sizeof(int));
+    pid_t pid = fork();
+    (*x)++;
+    if (pid == 0)
+    {
+        printf("%d Child proccess 1 %d\n", *x, pid);
+        (*x)++;
+        printf("%d Child proccess 2 %d\n", *x, pid);
 
+        execlp("ls", "ls", NULL);
+        perror("exec");
+
+        (*x)++;
+        printf("%d Child proccess 3 %d\n", *x, pid);
+        // exit(9);
+    }
+    else
+    {
+        printf("%d Parent processes 1 %d\n", *x, pid);
+        (*x)++;
+        printf("%d Parent processes 2 %d\n", *x, pid);
+        wait(0);
+        (*x)++;
+        printf("%d Parent processes 3 %d\n", *x, pid);
+    }
     return 0;
 }
