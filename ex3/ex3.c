5ffda38088b079be7fcc4fee7e7aaded18f3450a
Wanda <doubleuevans@gmail.com>
2019-05-01 16:39:36 -0500

    ex3 completed, child process prints first

--- ex3/ex3.c
+++ ex3/ex3.c
@@ -10,6 +10,17 @@
 int main(void)
 {
     // Your code here
+    int ex3_fork = fork();
+
+    if (ex3_fork < 0) {
+        printf("fork failed\n");
+        exit(1);
+    } else if (ex3_fork == 0){ //child
+        printf("hello!");
+    } else { // parent
+        wait(NULL);
+        printf("goodbye!");
+    }
 
     return 0;
 }
