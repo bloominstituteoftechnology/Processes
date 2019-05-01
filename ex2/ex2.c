4121548e970e0dc9f33ca4621fe9b4e971725c3f
Wanda <doubleuevans@gmail.com>
2019-05-01 16:33:27 -0500

    returning textfile for ex2.

--- ex2/ex2.c
+++ ex2/ex2.c
@@ -2,6 +2,8 @@
 // and then calls `fork()` to create a new process. Can both the child and parent access the file descriptor 
 // returned by `fopen()`? What happens when they are written to the file concurrently?
 
+// It doesnt seem that the both the child and parent can access the textfile.
+
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
@@ -13,17 +15,38 @@ int main(void)
     FILE *fp;
     printf("Opening the file text.txt to read\n");
 
-    fp = fopen("text.txt", "r");
+    fp = fopen("text.txt", "r\n");    
 
-    if (fp == NULL)
+    // if (fp == NULL)
+    // {
+    //     printf("Could not open text.txt");
+    //     return 1;
+    // } else {
+    //     while ((c = getc(fp)) != EOF)
+    //         //putchar(c);
+    //         printf("%c", c);
+    //         // printf("\n");
+    //     fclose(fp);
+    // }
+
+    // so it prints parent fork, then child fork, but prints the
+    // textfile from parent fork?
+    int text_fork = fork();
+    if (text_fork < 0)
     {
-        printf("Could not open text.txt");
-        return 1;
-    } else {
+        printf("fork failed\n");
+        exit(1);
+    } else if (text_fork == 0)
+    {
+        printf("child fork \n");
         while ((c = getc(fp)) != EOF)
-            //putchar(c);
             printf("%c", c);
-            // printf("\n");
+        fclose(fp);
+    } else
+    {
+        printf("parent fork \n");
+        while ((c = getc(fp)) != EOF)
+            printf("%c", c);
         fclose(fp);
     }
     
