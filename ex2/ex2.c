af0d5db75a1d78dbfddea7c6b140155719cfca9b
John O'Rourke <officialjohnorourke@gmail.com>
2019-05-29 16:57:43 -0400

    Completes ex2 and answers ex1 & 2 questions

--- ex2/ex2.c
+++ ex2/ex2.c
@@ -1,14 +1,26 @@
 // Write a program that opens the text.txt file (with the `fopen()` library call) located in this directory 
 // and then calls `fork()` to create a new process. Can both the child and parent access the file descriptor 
 // returned by `fopen()`? What happens when they are written to the file concurrently?
+// Yes; they both seem to repeat, where you'd only expect the code after the fork to complete.
 
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
 
-int main(void)
-{
-    // Your code here 
-    
+int main(void) {
+    FILE *fp = fopen("text.txt", "w+");
+
+    fprintf(fp, "Hello from parent");
+
+    int f = fork();
+    if (f < 0) {
+        fprintf(stderr, "Fork failed.\n");
+        exit(1);
+    }
+
+    fprintf(fp, "Hello from child");
+
+    fclose(fp);
+
     return 0;
 }
