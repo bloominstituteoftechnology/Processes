1d0cd8562e420203548a9911e37ab79b52030ddb
John O'Rourke <officialjohnorourke@gmail.com>
2019-05-29 17:49:23 -0400

    Completes ex5

--- ex5/ex5.c
+++ ex5/ex5.c
@@ -14,9 +14,31 @@ char* msg1 = "hello world #1";
 char* msg2 = "hello world #2";
 char* msg3 = "hello world #3";
 
-int main(void)
-{
-    // Your code here
+int main(void) {
+    char buffer[MSGSIZE];
+    int fd[2];
+
+    if (pipe(fd) == -1) {
+        fprintf(stderr, "PIPE ERROR!\n");
+        exit(-1);
+    }
+
+    int rc = fork();
+    
+    if (rc < 0) {
+        fprintf(stderr, "FORK ERROR!\n");
+        exit(1);
+    } else if (rc == 0) {
+        write(fd[1], msg1, MSGSIZE);
+        write(fd[1], msg2, MSGSIZE);
+        write(fd[1], msg3, MSGSIZE);
+    } else {
+        wait(&rc);
+        for (int i = 0; i < 3; i++) {
+            read(fd[0], buffer, MSGSIZE);
+            printf("%s\n", buffer);
+        }
+    }
     
     return 0;
 }
