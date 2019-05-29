c340cc519e0b6338d556882ff961bbce2cd6e8c7
John O'Rourke <officialjohnorourke@gmail.com>
2019-05-29 17:16:17 -0400

    Completes ex4

--- ex4/ex4.c
+++ ex4/ex4.c
@@ -2,15 +2,20 @@
 // to run the program `/bin/ls`. Try a few variants of `exec()`, such as 
 // `execl()`, `execle()`, `execv()`, and others. Why do you think there 
 // are so many variants of the same basic call?
+// Because of different use cases; it's for the sake of having more specifications available.
 
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <sys/wait.h>
 
-int main(void)
-{
-    // Your code here    
+int main(void) {
+    fork();
+
+    char *args[] = {"/bin/ls", NULL};
+    execvp(args[0], args);
+    // execv(args[0], args);
+
 
     return 0;
 }
