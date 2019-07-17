5fd3dd2889854b772d20d7f1235cca0e9f80556d
nateyoungren <nathanaelyoungren@gmail.com>
2019-07-17 16:43:09 -0700

    Exec, excercise 4

--- ex4/ex4.c
+++ ex4/ex4.c
@@ -7,10 +7,26 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <sys/wait.h>
+#include <string.h>
 
-int main(void)
+int main(int argc, char *argv[])
 {
-    // Your code here    
+    // Your code here  
+    int rc = fork();
+    
+
+    if (rc < 0) {
+    	printf("Fork failed...\n");
+    } else if (rc == 0) {
+
+    	char *my_args[3];
+
+    	my_args[0] = strdup("/bin/ls");
+    	my_args[1] = strdup("ex4.c");
+    	my_args[1] = NULL;
+
+    	execvp(my_args[0], my_args);
+    }
 
     return 0;
 }
